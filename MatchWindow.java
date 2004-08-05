a2f9425caaef0ff010aaa6fa03e2d6e0bc2d78c8
Maxym Mykhalchuk <mihmax@gmail.com>
2004-08-05 21:51:32 +0000

    [1004219] Going to segment without matches may throw Exception - fixed
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@29 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- MatchWindow.java
+++ MatchWindow.java
@@ -2,7 +2,7 @@
 //  
 //  MatchWindow.java - 
 //  
-//  Copyright (C) 2004, Keith Godfrey
+//  Copyright (C) 2004, Keith Godfrey 
 //  
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by

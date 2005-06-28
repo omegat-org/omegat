c6f97686051f2d94314c62db08aa066fb3e1cac5
Maxym Mykhalchuk <mihmax@gmail.com>
2005-06-28 10:13:32 +0000

    HTML filter refactoring: minor
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@210 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/util/PatternConsts.java
+++ src/org/omegat/util/PatternConsts.java
@@ -28,7 +28,7 @@ import java.util.regex.Pattern;
  *
  * @author Maxym Mykhalchuk
  */
-class PatternConsts
+public class PatternConsts
 {
 	/** compiled pattern to extract the encoding from XML file, if any */
 	public static final Pattern XML_ENCODING = Pattern.compile(

ab002953d0e14cf63956bba70a192d9b76f391fe
Maxym Mykhalchuk <mihmax@gmail.com>
2005-12-19 15:14:26 +0000

    fixing 1) wrong segmentation: It looks like OmegaT segments at: <text:user-defined text:name="License info for source files"> This has to be fixed so that OmegaT considers <text:user-defined ...> as an inline tag.
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@284 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/filters2/xml/openoffice/OOFilter.java
+++ src/org/omegat/filters2/xml/openoffice/OOFilter.java
@@ -156,6 +156,7 @@ public class OOFilter extends XMLAbstractFilter
         defineFormatTag("text:tab-stop/", "t/");                                // NOI18N
         defineFormatTag("text:line-break", "br");                               // NOI18N
         defineFormatTag("text:line-break/", "br/");                             // NOI18N
+        defineFormatTag("text:user-defined", "ud");                             // NOI18N
         
         // not working in 1.4.5
         // defineVerbatumTag("text:note-citation");                             // NOI18N

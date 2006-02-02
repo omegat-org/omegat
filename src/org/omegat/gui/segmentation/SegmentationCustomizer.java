5a817ce4946558eb23e994ba98ba22fea4381e68
Henry Pijffers <henry.pijffers@gmail.com>
2006-02-02 22:24:16 +0000

    Segmentation setup dialog now closes on Escape
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@363 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/segmentation/SegmentationCustomizer.java
+++ src/org/omegat/gui/segmentation/SegmentationCustomizer.java
@@ -24,12 +24,18 @@ package org.omegat.gui.segmentation;
 import java.awt.Component;
 import java.awt.Dialog;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;     // HP
+import java.awt.event.KeyEvent;        // HP
 import java.awt.Frame;
 import java.beans.ExceptionListener;
 import java.text.MessageFormat;
+import javax.swing.AbstractAction;     // HP
+import javax.swing.Action;             // HP
+import javax.swing.JComponent;         // HP
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;          // HP
 import javax.swing.ListSelectionModel;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.CellEditorListener;
@@ -64,6 +70,21 @@ public class SegmentationCustomizer extends JDialog
     
     private void constructor()
     {
+        // HP
+        //  Handle escape key to close the window
+        KeyStroke escape = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false);
+        Action escapeAction = new AbstractAction()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                dispose();
+            }
+        };
+        getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).
+        put(escape, "ESCAPE");                                                  // NOI18N
+        getRootPane().getActionMap().put("ESCAPE", escapeAction);               // NOI18N
+        // END HP
+    
         initComponents();
         
         getRootPane().setDefaultButton(okButton);

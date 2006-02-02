b8e910e5bb2b3dbd6abc4c62d50a6e4b7f56c9b0
Henry Pijffers <henry.pijffers@gmail.com>
2006-02-02 22:09:30 +0000

    Editing Field Options/Workflow Options dialog now closes on Escape
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@359 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/dialogs/WorkflowOptionsDialog.java
+++ src/org/omegat/gui/dialogs/WorkflowOptionsDialog.java
@@ -21,9 +21,16 @@
 
 package org.omegat.gui.dialogs;
 
+import java.awt.event.ActionEvent;     // HP
+import java.awt.event.KeyEvent;        // HP
 import java.awt.Frame;
+import javax.swing.AbstractAction;     // HP
+import javax.swing.Action;             // HP
+import javax.swing.JComponent;         // HP
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.KeyStroke;          // HP
+
 import org.omegat.util.OStrings;
 import org.omegat.util.Preferences;
 
@@ -42,6 +49,22 @@ public class WorkflowOptionsDialog extends JDialog
     public WorkflowOptionsDialog(Frame parent)
     {
         super(parent, true);
+        
+        // HP
+        //  Handle escape key to close the window
+        KeyStroke escape = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false);
+        Action escapeAction = new AbstractAction()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                dispose();
+            }
+        };
+        getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).
+        put(escape, "ESCAPE");                                                  // NOI18N
+        getRootPane().getActionMap().put("ESCAPE", escapeAction);               // NOI18N
+        // END HP
+        
         initComponents();
 
         getRootPane().setDefaultButton(okButton);

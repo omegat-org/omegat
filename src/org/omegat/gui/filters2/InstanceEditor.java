e4108083d10c2c6b16da4140bf049eac78e4611d
Henry Pijffers <henry.pijffers@gmail.com>
2006-02-02 22:21:54 +0000

    Edit Pattern/Instance Editor dialog now closes on Escape
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@362 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/filters2/InstanceEditor.java
+++ src/org/omegat/gui/filters2/InstanceEditor.java
@@ -22,10 +22,16 @@
 package org.omegat.gui.filters2;
 
 import java.awt.Dialog;
+import java.awt.event.ActionEvent;     // HP
+import java.awt.event.KeyEvent;        // HP
 import java.util.Vector;
+import javax.swing.AbstractAction;     // HP
+import javax.swing.Action;             // HP
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComponent;         // HP
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.KeyStroke;          // HP
 import javax.swing.border.TitledBorder;
 import org.omegat.filters2.AbstractFilter;
 
@@ -62,6 +68,21 @@ public class InstanceEditor extends JDialog
             hintTextArea.setText(hint);
         else
             hintTextArea.setVisible(false);
+            
+        // HP
+        //  Handle escape key to close the window
+        KeyStroke escape = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false);
+        Action escapeAction = new AbstractAction()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                dispose();
+            }
+        };
+        getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).
+        put(escape, "ESCAPE");                                                  // NOI18N
+        getRootPane().getActionMap().put("ESCAPE", escapeAction);               // NOI18N
+        // END HP
         
         pack();
     }

17accc42fed2d9694514f4c100e1724833cdd204
Maxym Mykhalchuk <mihmax@gmail.com>
2006-02-18 12:11:07 +0000

    Separated filter plugin handling code from FilterMaster into a separate PluginUtils class
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@400 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/filters2/master/FilterMaster.java
+++ src/org/omegat/filters2/master/FilterMaster.java
@@ -91,7 +91,7 @@ public class FilterMaster
      */
     private FilterMaster()
     {
-        loadPlugins();
+        PluginUtils.loadPlugins();
         if( configFile.exists() )
             loadConfig();
         else
@@ -231,67 +231,6 @@ public class FilterMaster
         }
     }
     
-    private List old_plugins = null;
-    private ClassLoader plugins_cl = null;
-    /** Returns the classloader of the filter plugins. */
-    private ClassLoader getPluginsClassloader()
-    {
-        if( !plugins.equals(old_plugins) )
-        {
-            List jars = new ArrayList(plugins.size());
-            for(int i=0; i<plugins.size(); i++)
-            {
-                List pfilters = (List)plugins.get(i);
-                URL jarurl = (URL)pfilters.get(0);
-                jars.add(jarurl);
-            }
-            plugins_cl = new URLClassLoader((URL[])jars.toArray(new URL[plugins.size()]));
-            old_plugins = new ArrayList();
-            old_plugins.addAll(plugins);
-        }
-        return plugins_cl;
-    }
-    
-    
-    /** Utility Method to instantiate a filter */
-    private AbstractFilter instantiateFilter(OneFilter filter)
-            throws TranslationException
-    {
-        AbstractFilter filterObject = null;
-        try
-        {
-            ClassLoader cl = getPluginsClassloader();
-            Class filterClass;
-            if( filter.isFromPlugin() )
-                filterClass = cl.loadClass(filter.getClassName());
-            else
-                filterClass = Class.forName(filter.getClassName());
-            Constructor filterConstructor = filterClass.getConstructor((Class[])null);
-            filterObject = (AbstractFilter)filterConstructor.newInstance((Object[])null);
-        }
-        catch( ClassNotFoundException cnfe )
-        {
-            throw new TranslationException(cnfe.toString());
-        }
-        catch( NoSuchMethodException nsme )
-        {
-            throw new TranslationException(nsme.toString());
-        }
-        catch( InstantiationException ie )
-        {
-            throw new TranslationException(ie.toString());
-        }
-        catch ( IllegalAccessException iae )
-        {
-            throw new TranslationException(iae.toString());
-        }
-        catch( InvocationTargetException ite )
-        {
-            throw new TranslationException(ite.getCause().toString());
-        }
-        return filterObject;
-    }
-    
     /**
      * OmegaT core calls this method to load a source file.
      *
@@ -306,7 +245,6 @@ public class FilterMaster
         if( lookup==null )
             return false;
         
-        //if()
         setMemorizing(true);
         
         AbstractFilter filterObject = lookup.filterObject;
@@ -476,7 +414,7 @@ public class FilterMaster
                 if( matchesMask(name, instance.getSourceFilenameMask()) )
                 {
                     AbstractFilter filterObject;
-                    filterObject = instantiateFilter(filter);
+                    filterObject = PluginUtils.instantiateFilter(filter);
                     
                     BufferedReader reader = filterObject.createReader(file, instance.getSourceEncoding());
                     
@@ -528,139 +466,6 @@ public class FilterMaster
     }
     
     //////////////////////////////////////////////////////////////////////////
-    // Plugins
-    //////////////////////////////////////////////////////////////////////////
-    
-    /**
-     * The list of plugins.
-     * <p>
-     * The format is simple:
-     * <ul>
-     * <li>each element of this list is a List itself
-     * <li>the first (0th) element of each sublist is an URL of JAR file
-     * <li>further elements are names of filter classes
-     * </ul>
-     */
-    private List plugins;
-    
-    /**
-     * Loads filter plugins.
-     * <p>
-     * Filter plugins should be situated in &lt;OmegaT-install-dir&gt;/plugins,
-     * and be packed as JAR files with manifest stating
-     * <pre> OmegaT-Plugin: true </pre>
-     * and then for each filter
-     * <pre> Name: the.package.name.TheFilterName
-     * OmegaT-Filter: true</pre> for each filter in a plugin
-     * (plugin may have more than one filter).
-     */
-    private void loadPlugins()
-    {
-        plugins = new ArrayList();
-        File pluginsDir = new File("plugins/");                                 // NOI18N
-        if( pluginsDir.exists() && pluginsDir.isDirectory() )
-            loadPluginsFrom(pluginsDir);
-    }
-    
-    /**
-     * Loads filter plugins from a single directory.
-     */
-    private void loadPluginsFrom(File dir)
-    {
-        File[] filters = dir.listFiles(new FileFilter()
-        // File filter that accepts JAR files or directories
-        {
-            public boolean accept(File file)
-            {
-                return
-                        (
-                        file.isFile() &&
-                        file.getName().toLowerCase().endsWith(".jar")       // NOI18N
-                        )
-                        ||
-                        (
-                        file.isDirectory() &&
-                        !file.getName().equals(".") &&                      // NOI18N
-                        !file.getName().equals("..")                        // NOI18N
-                        );
-            }
-        });
-        for(int i=0; i<filters.length; i++)
-        {
-            if( filters[i].isFile() )
-            {
-                try
-                {
-                    URL jar = filters[i].toURL();
-                    loadOnePlugin(jar);
-                }
-                catch( MalformedURLException mue )
-                {
-                    // nothing is really wrong
-                    // strange exception
-                    StaticUtils.log("Couldn't access local file system " +      // NOI18N
-                            "to get '"+filters[i]+"' !");                       // NOI18N
-                    mue.printStackTrace(StaticUtils.getLogStream());
-                }
-                catch( IOException ioe )
-                {
-                    // nothing is really wrong
-                    // we just couldn't load one JAR
-                    StaticUtils.log("Couldn't load plugin JAR '"+               // NOI18N
-                            filters[i]+"' !");                                  // NOI18N
-                }
-            }
-            else
-                loadPluginsFrom(filters[i]);
-        }
-    }
-    
-    /**
-     * Loads plugins from a single JAR.
-     *
-     * @param jar name of the plugin JAR file to load filters from.
-     */
-    private void loadOnePlugin(URL jar) throws IOException
-    {
-        JarFile filter_jar = new JarFile(jar.getFile());
-        Manifest manifest = filter_jar.getManifest();
-        
-        Attributes mainattribs = manifest.getMainAttributes();
-        if( mainattribs.getValue("OmegaT-Plugin")==null )                       // NOI18N
-            return; // it's not OmegaT plugin
-        
-        List filterList = new ArrayList();
-        filterList.add(jar);
-        
-        Map entries = manifest.getEntries();
-        String[] keys = (String[])entries.keySet().toArray(new String[]{});
-        for(int i=0; i<keys.length; i++)
-        {
-            Attributes attrs = (Attributes)entries.get(keys[i]);
-            String name = attrs.getValue("Name");                               // NOI18N
-            String isfilter = attrs.getValue("OmegaT-Filter");                  // NOI18N
-            if( isfilter!=null && isfilter.equals("true") )                     // NOI18N
-            {
-                filterList.add(keys[i]);
-                try
-                {
-                    URLClassLoader.newInstance(new URL[] {jar}).loadClass(keys[i]);
-                }
-                catch( ClassNotFoundException e )
-                {
-                    // we just don't load the plugin
-                    throw new IOException(e.getLocalizedMessage());
-                }
-            }
-        }
-        
-        if( filterList.size()>1 )
-        {
-            plugins.add(filterList);
-        }
-    }
-    
-    //////////////////////////////////////////////////////////////////////////
     // Filters
     //////////////////////////////////////////////////////////////////////////
     
@@ -677,7 +482,7 @@ public class FilterMaster
     public void revertFiltersConfigToDefaults()
     {
         setupBuiltinFilters();
-        loadPlugins();
+        PluginUtils.loadPlugins();
         loadFilterClassesFromPlugins();
         saveConfig();
     }
@@ -756,7 +561,8 @@ public class FilterMaster
      */
     private void checkIfAllFilterPluginsAreAvailable()
     {
-        ClassLoader cl = getPluginsClassloader();
+        ClassLoader cl = PluginUtils.getPluginsClassloader();
+        List plugins = PluginUtils.getPlugins();
         
         int k=0;
         while( k<filters.filtersSize() )
@@ -833,7 +639,9 @@ public class FilterMaster
      */
     private void loadFilterClassesFromPlugins()
     {
-        ClassLoader cl = getPluginsClassloader();
+        ClassLoader cl = PluginUtils.getPluginsClassloader();
+        List plugins = PluginUtils.getPlugins();
+        
         for(int i=0; i<plugins.size(); i++)
         {
             List filterList = (List)plugins.get(i);

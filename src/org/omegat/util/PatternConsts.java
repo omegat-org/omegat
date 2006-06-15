9f73968f0df627c8c828c26dc8a14fdbcedc838e
Maxym Mykhalchuk <mihmax@gmail.com>
2006-06-15 20:55:01 +0000

    Some new patterns for DocBook filter
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@505 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/util/PatternConsts.java
+++ src/org/omegat/util/PatternConsts.java
@@ -33,43 +33,66 @@ import java.util.regex.Pattern;
  */
 public class PatternConsts
 {
-	/** compiled pattern to extract the encoding from XML file, if any */
-	public static final Pattern XML_ENCODING = Pattern.compile(
-		"<\\?xml.*?encoding\\s*=\\s*\"(\\S+?)\".*?\\?>");                       // NOI18N
-	
-	/** compiled pattern to extract the encoding from XML file, if any */
-	public static final Pattern XML_HEADER = Pattern.compile(
-		"(<\\?xml.*?\\?>)");                                                    // NOI18N
-	
-	/** compiled pattern to extract the encoding from HTML file, if any */
-	public static final Pattern HTML_ENCODING = Pattern.compile(
-		"<meta.*?content\\s*=\\s*[\"']\\s*text/html\\s*;\\s*charset\\s*=\\s*(\\S+?)[\"'].*?/?\\s*>",  // NOI18N
-		Pattern.CASE_INSENSITIVE);
-	
-	/** compiled pattern to look for HTML file HEAD declaration */
-	public static final Pattern HTML_HEAD = Pattern.compile(
-		"<head.*?>",                                                            // NOI18N
-		Pattern.CASE_INSENSITIVE);
-	
-	/** compiled pattern to look for HTML file HTML declaration */
-	public static final Pattern HTML_HTML = Pattern.compile(
-		"<html.*?>",                                                            // NOI18N
-		Pattern.CASE_INSENSITIVE);
+    /**
+     * Compiled pattern to extract the encoding from XML file, if any.
+     * Found encoding is stored in group #1.
+     */
+    public static final Pattern XML_ENCODING = Pattern.compile(
+            "<\\?xml.*?encoding\\s*=\\s*\"(\\S+?)\".*?\\?>");                       // NOI18N
     
-    /** 
-     * Pattern that matches full string containing in full and only 
+    /** compiled pattern to match XML header */
+    public static final Pattern XML_HEADER = Pattern.compile(
+            "(<\\?xml.*?\\?>)");                                                    // NOI18N
+    
+    /**
+     * Compiled pattern to extract the DOCTYPE declaration from XML file, if any.
+     * Groups:
+     * <br>
+     * #1 - DOCTYPE name
+     * <br>
+     * #3 - PUBLIC DOCTYPE URL
+     * <br>
+     * #5 - SYSTEM DOCTYPE URL
+     */
+    public static final Pattern XML_DOCTYPE = Pattern.compile(
+            "<\\!DOCTYPE\\s+(\\w+)\\s+(PUBLIC\\s+\"(-//.*)\"\\s+)?");//(SYSTEM\\s+)?\"(.*?)\"\\s+>");                       // NOI18N
+    
+    /**
+     * Compiled pattern to extract the root tag from XML file, if any.
+     * Group #1 should contain the root tag.
+     */
+    public static final Pattern XML_ROOTTAG = Pattern.compile(
+            "<(\\w+)");                       // NOI18N
+    
+    /** compiled pattern to extract the encoding from HTML file, if any */
+    public static final Pattern HTML_ENCODING = Pattern.compile(
+            "<meta.*?content\\s*=\\s*[\"']\\s*text/html\\s*;\\s*charset\\s*=\\s*(\\S+?)[\"'].*?/?\\s*>",  // NOI18N
+            Pattern.CASE_INSENSITIVE);
+    
+    /** compiled pattern to look for HTML file HEAD declaration */
+    public static final Pattern HTML_HEAD = Pattern.compile(
+            "<head.*?>",                                                            // NOI18N
+            Pattern.CASE_INSENSITIVE);
+    
+    /** compiled pattern to look for HTML file HTML declaration */
+    public static final Pattern HTML_HTML = Pattern.compile(
+            "<html.*?>",                                                            // NOI18N
+            Pattern.CASE_INSENSITIVE);
+    
+    /**
+     * Pattern that matches full string containing in full and only
      * omegat-specific tag (without leading &lt; and trailing &gt;).
      */
     public static final Pattern OMEGAT_TAG_ONLY = Pattern.compile(
             "^\\/?[a-zA-Z]+[0-9]+\\/?$");                                       // NOI18N
-
-    /** 
-     * Pattern that matches omegat-specific tags 
+    
+    /**
+     * Pattern that matches omegat-specific tags
      * (with leading &lt; and trailing &gt; in any place of a string).
      */
     public static final Pattern OMEGAT_TAG = Pattern.compile(
             "<\\/?[a-zA-Z]+[0-9]+\\/?>");                                       // NOI18N
-
+    
     /** Pattern that detects space-only regular expressions. */
     public static final Pattern SPACY_REGEX = Pattern.compile(
             "((\\s|\\\\n|\\\\t|\\\\s)(\\+|\\*)?)+");                            // NOI18N

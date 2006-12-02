b4f3ea253b651c53d7cc49dd8e546f17288fb8cf
Henry Pijffers <henry.pijffers@gmail.com>
2006-12-02 23:12:35 +0000

    Moved all logging functionality to org.omegat.util.Log
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@854 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/util/StaticUtils.java
+++ src/org/omegat/util/StaticUtils.java
@@ -24,12 +24,8 @@
 
 package org.omegat.util;
 
-import java.awt.GraphicsEnvironment;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.awt.GraphicsEnvironment;
 import java.text.BreakIterator;
 import java.text.MessageFormat;
 import java.util.List;
@@ -38,7 +34,6 @@ import java.util.ArrayList;
 import java.util.Locale;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.Random;
 
 
 /**
@@ -51,11 +46,6 @@ import java.util.Random;
  */
 public class StaticUtils
 {
-	/**
-	 * Name of the log file
-	 */
-	private final static String FILE_LOG = "log.txt";                           // NOI18N
-	
 	/**
 	 * Configuration directory on Windows platforms
 	 */
@@ -403,305 +393,6 @@ public class StaticUtils
         return res.toString();
     }
 
-    public static String getLogLocation() {
-        return getConfigDir() + FILE_LOG;
-    }
-
-    private static SessionPrintStream log = null;
-    /**
-     * Returns a log stream.
-     */
-    public static PrintStream getLogStream() {
-        if (log == null) {
-            try {
-                // create a new session print stream for the log file
-                log = new SessionPrintStream( // encapsulated to output session ID
-                    new PrintStream(
-                        new FileOutputStream(getConfigDir() + FILE_LOG, true), 
-                        true, "UTF-8")); // NOI18N
-            }
-            catch(Exception e) {
-                // in case we cannot create a log file on dist,
-                // redirect to system out
-               log = new SessionPrintStream(System.out);
-            }
-
-            // get the session ID from the log session print stream
-            String sessionID = log.getSessionID();
-
-            // also encapsulate the system out in a session print stream
-            // make sure they use the same session ID
-            SessionPrintStream sessionOut = new SessionPrintStream(System.out);
-            sessionOut.setSessionID(sessionID);
-            System.setOut(sessionOut);
-        }
-
-        return log;
-    }
-
-    /**
-     * Logs what otherwise would go to System.out
-     */
-    public static void log(String s)
-    {
-        // Write to log
-        try
-        {
-            PrintStream fout = getLogStream();
-            fout.println(s);
-            //fout.close();
-            fout.flush(); // don't close it, otherwise the session ID is lost
-        }
-        catch( Exception e )
-        {
-            // doing nothing
-        }
-
-        // Write to System.out
-        System.out.println(s);
-    }
-
-    /**
-      * Logs a message, retrieved from the resource bundle.
-      *
-      * @param key The key of the message in the resource bundle.
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    public static void logRB(String key) {
-        logRB(key, null);
-    }
-
-    /**
-      * Logs a message, retrieved from the resource bundle.
-      *
-      * @param key        The key of the message in the resource bundle.
-      * @param parameters Parameters for the message. These are inserted by
-      *                   using StaticUtils.format.
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    public static void logRB(String key, Object[] parameters) {
-        // Retrieve the message
-        String message = OStrings.getString(key);
-
-        // Format the message, if there are parameters
-        if (parameters != null)
-            message = format(message, parameters);
-
-        // Write the message to the log
-        log(message);
-    }
-
-    /**
-      * Writes an exception or error message plus stack trace to the log.
-      *
-      * @param throwable The exception or error to log
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    public static void log(Throwable throwable) {
-        // Log the throwable class
-        log(throwable.getClass().getName() + ":");
-
-        // Log the message, if any
-        String message = throwable.getMessage();
-        if ((message != null) && (message.length() > 0))
-            log(message);
-
-        // Log the stack trace
-        StringWriter stackTrace = new StringWriter();
-        throwable.printStackTrace(new PrintWriter(stackTrace));
-        log(stackTrace.toString());
-    }
-
-    /**
-      * Writes a warning message to the log (to be retrieved from the
-      * resource bundle), preceded by a lie containing the warning ID.
-      *
-      * While the warning message can be localised, the warning ID should not,
-      * so developers can determine what warning was given by looking at the
-      * warning ID, instead of trying to interpret localised messages.
-      *
-      * @param id The key of the warning message in the resource bundle
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    public static void logWarningRB(String key) {
-        // Retrieve the waring message
-        String message = OStrings.getString(key);
-
-        // Log it
-        logWarning(key, message, null);
-    }
-
-    /**
-      * Writes a warning message to the log (to be retrieved from the
-      * resource bundle), preceded by a line containing the warning ID.
-      *
-      * While the warning message can be localised, the warning ID should not,
-      * so developers can determine what warning was given by looking at the
-      * warning ID, instead of trying to interpret localised messages.
-      *
-      * @param key        The key of the warning message in the resource bundle
-      * @param parameters Parameters for the warning message. These are
-      *                   inserted by using StaticUtils.format.
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    public static void logWarningRB(String key, Object[] parameters) {
-        // Retrieve the warning message
-        String message = OStrings.getString(key);
-
-        // Log it
-        logWarning(key, message, parameters);
-    }
-
-    /**
-      * Writes the specified warning message to the log, preceded by a line
-      * containing the warning ID.
-      *
-      * While the warning message can be localised, the warning ID should not,
-      * so developers can determine what warning was given by looking at the
-      * warning ID, instead of trying to interpret localised messages.
-      *
-      * @param id      An identification string for the warning
-      * @param message The actual warning message
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    public static void logWarning(String id, String message) {
-        logWarning(id, message, null);
-    }
-
-    /**
-      * Writes the specified warning message to the log, preceded by a line
-      * containing the warning ID.
-      *
-      * While the warning message can be localised, the warning ID should not,
-      * so developers can determine what warning was given by looking at the
-      * warning ID, instead of trying to interpret localised messages.
-      *
-      * @param id         An identification string for the warning
-      * @param message    The actual warning message
-      * @param parameters Parameters for the warning message. These are
-      *                   inserted by using StaticUtils.format.
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    public static void logWarning(String id, String message, Object[] parameters) {
-        logIdentifiableMessage(
-            format(OStrings.getString("SU_LOG_WARNING_ID"), new Object[] {id}),
-            message,
-            parameters);
-    }
-
-    /**
-      * Writes an error message to the log (to be retrieved from the
-      * resource bundle), preceded by a line containing the error ID.
-      *
-      * While the error message can be localised, the error ID should not,
-      * so developers can determine what error was given by looking at the
-      * error ID, instead of trying to interpret localised messages.
-      *
-      * @param key The key of the error message in the resource bundle
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    public static void logErrorRB(String key) {
-        // Retrieve and log the error message
-        String message = OStrings.getString(key);
-
-        // Log it
-        logError(key, message, null);
-    }
-
-    /**
-      * Writes an error message to the log (to be retrieved from the
-      * resource bundle), preceded by a line containing the error ID.
-      *
-      * While the error message can be localised, the error ID should not,
-      * so developers can determine what error was given by looking at the
-      * error ID, instead of trying to interpret localised messages.
-      *
-      * @param id         The key of the error message in the resource bundle
-      * @param parameters Parameters for the error message. These are
-      *                   inserted by using StaticUtils.format.
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    public static void logErrorRB(String key, Object[] parameters) {
-        // Retrieve the error message
-        String message = OStrings.getString(key);
-
-        // Log it
-        logError(key, message, parameters);
-    }
-
-    /**
-      * Writes the specified error message to the log, preceded by a line
-      * containing the error ID.
-      *
-      * While the error message can be localised, the error ID should not,
-      * so developers can determine what error was given by looking at the
-      * error ID, instead of trying to interpret localised messages.
-      *
-      * @param id      An identification string for the error
-      * @param message The actual error message
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    public static void logError(String id, String message) {
-        logError(id, message, null);
-    }
-
-    /**
-      * Writes the specified error message to the log, preceded by a line
-      * containing the error ID.
-      *
-      * While the error message can be localised, the error ID should not,
-      * so developers can determine what error was given by looking at the
-      * error ID, instead of trying to interpret localised messages.
-      *
-      * @param id         An identification string for the error
-      * @param message    The actual error message
-      * @param parameters Parameters for the error message. These are
-      *                   inserted by using StaticUtils.format.
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    public static void logError(String id, String message, Object[] parameters) {
-        logIdentifiableMessage(
-            format(OStrings.getString("SU_LOG_ERROR_ID"), new Object[] {id}),
-            message,
-            parameters);
-    }
-
-    /**
-      * Writes the specified message to the log, preced by a line containing
-      * an identification for the message.
-      *
-      * While the message can be localised, the ID should not, so developers
-      * can determine what message was given by looking at the error ID,
-      * instead of trying to interpret localised messages.
-      *
-      * @param idLine     The identification line for the message
-      * @param message    The actual message
-      * @param parameters Parameters for the message. These are
-      *                   inserted by using StaticUtils.format.
-      */
-    protected static void logIdentifiableMessage(String idLine, String message, Object[] parameters) {
-        // First write the ID line to the log
-        log(idLine);
-
-        // Format the message, if there are parameters
-        if (parameters != null)
-            message = format(message, parameters);
-
-        // Write the message to the log
-        log(message);
-    }
-
     /**
      * Extracts an element of a class path.
      *
@@ -822,8 +513,8 @@ public class StaticUtils
             // log the exception, only do this after the config dir
             // has been set to the current working dir, otherwise
             // the log method will probably fail
-            logErrorRB("SU_USERHOME_PROP_ACCESS_ERROR");
-            log(e.toString());
+            Log.logErrorRB("SU_USERHOME_PROP_ACCESS_ERROR");
+            Log.log(e.toString());
 
             return m_configDir;
         }
@@ -899,7 +590,7 @@ public class StaticUtils
                     // if the dir could not be created,
                     // set the config dir to the current working dir
                     if (!created) {
-                        logErrorRB("SU_CONFIG_DIR_CREATE_ERROR");
+                        Log.logErrorRB("SU_CONFIG_DIR_CREATE_ERROR");
                         m_configDir = new File(".").getAbsolutePath();          // NOI18N
                     }
                 }
@@ -911,8 +602,8 @@ public class StaticUtils
                 m_configDir = new File(".").getAbsolutePath();                  // NOI18N
                 
                 // log the exception, but only after the config dir has been reset
-                logErrorRB("SU_CONFIG_DIR_CREATE_ERROR");
-                log(e.toString());
+                Log.logErrorRB("SU_CONFIG_DIR_CREATE_ERROR");
+                Log.log(e.toString());
             }
         }
         
@@ -1097,193 +788,5 @@ public class StaticUtils
         str = str.replaceAll("'", "''");
         return MessageFormat.format(str, arguments);
     }
-
-    /**
-      * Print stream that writes a session ID before each line of output
-      *
-      * @author Henry Pijffers (henry.pijffers@saxnot.com)
-      */
-    private static class SessionPrintStream extends PrintStream {
-
-        /**
-          * Print stream to write all output to.
-          */
-        //private PrintStream out;
-
-        /**
-          * Session ID
-          */
-        String sessionID;
-
-        /**
-          * Indicates whether the last character output was a newline.
-          */
-        private boolean lastIsNewline = false;
-
-        /**
-          * Constructs a new SessionPrintStream
-          *
-          * @param out The print stream to write all output to
-          */
-        public SessionPrintStream(PrintStream out) {
-            super(out);
-
-            // get a positive random number
-            Random generator = new Random();
-            generator.setSeed(System.currentTimeMillis()); // use current time as seed
-            int random = Math.abs(generator.nextInt());
-
-            // convert the number to string, 5 chars max, pad with zero's if necessary
-            sessionID = String.valueOf(random);
-            if (sessionID.length() > 5)
-                sessionID = sessionID.substring(0, 5);
-            else if (sessionID.length() < 5)
-                for (int i = 5; i > sessionID.length(); i++)
-                    sessionID = "0" + sessionID;
-        }
-
-        /**
-          * Retrieves the session ID.
-          *
-          * @return The session ID of this SessionPrintStream
-          */
-        public String getSessionID() {
-            return sessionID;
-        }
-
-        /**
-          * Overrides the generated session ID.
-          *
-          * @param sessionID The session ID to use
-          */
-        public void setSessionID(String sessionID) {
-            this.sessionID = sessionID;
-        }
-
-        /**
-          * Writes the session ID to the output stream when at the start of a new line.
-          */
-        void printSessionID() {
-            printSessionID(false);
-        }
-
-        /**
-          * Writes the session ID to the output stream when at the start of a new line.
-          *
-          * @param forceWrite When true, the session ID is always writen,
-          *                   even if not at the start of a new line.
-          */
-        void printSessionID(boolean forceWrite) {
-            if (forceWrite || lastIsNewline)
-                super.print(sessionID + ": ");
-        }
-
-        public void print(boolean b) {
-            print(String.valueOf(b));
-        }
-
-        public void print(char c) {
-            print(String.valueOf(c));
-        }
-
-        public void print(char[] s) {
-            for (int i = 0; i < s.length; i++)
-                print(s[i]);
-        }
-
-        public void print(double d) {
-            print(String.valueOf(d));
-        }
-
-        public void print(float f) {
-            print(String.valueOf(f));
-        }
-
-        public void print(int i) {
-            print(String.valueOf(i));
-        }
-
-        public void print(long l) {
-            print(String.valueOf(l));
-        }
-
-        public void print(Object o) {
-            print(String.valueOf(o));
-        }
-
-        public void print(String s) {
-            if (s == null)
-                s = "null";
-            byte[] bytes = s.getBytes();
-            for (int i = 0; i < bytes.length; i++)
-                write((int)bytes[i]);
-        }
-
-        public void println() {
-            printSessionID();
-            super.println();
-            lastIsNewline = true;
-        }
-
-        public void println(boolean b) {
-            print(b);
-            println();
-        }
-
-        public void println(char c) {
-            print(c);
-            println();
-        }
-
-        public void println(char[] s) {
-            print(s);
-            println();
-        }
-
-        public void println(double d) {
-            print(d);
-            println();
-        }
-
-        public void println(float f) {
-            print(f);
-            println();
-        }
-
-        public void println(int i) {
-            print(i);
-            println();
-        }
-
-        public void println(long l) {
-            print(l);
-            println();
-        }
-
-        public void println(Object o) {
-            print(o);
-            println();
-        }
-
-        public void println(String s) {
-            print(s);
-            println();
-        }
-
-        /*
-        public void write(byte[] bytes, int off, int len) {
-            len = Math.min(off + len, bytes.length);
-            for (int i = off; i < len; i++)
-                write((int)bytes[i]);
-        }
-        */
-
-        public void write(int b) {
-            printSessionID();
-            super.write(b);
-            lastIsNewline = (((char)b) == '\n');
-        }
-
-    } // SessionPrintStream
 
 } // StaticUtils

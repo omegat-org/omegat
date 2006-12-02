04d694073844aa4924e85cc04673d03b55ba0139
Henry Pijffers <henry.pijffers@gmail.com>
2006-12-02 22:25:40 +0000

    Refactored org.omegat.filters2.master.PlugingUtils
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@845 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/filters2/master/PluginUtils.java
+++ src/org/omegat/filters2/master/PluginUtils.java
@@ -41,7 +41,7 @@ import java.util.jar.Manifest;
 
 import org.omegat.filters2.AbstractFilter;
 import org.omegat.filters2.TranslationException;
-import org.omegat.util.StaticUtils;
+import org.omegat.util.Log;
 
 /**
  * Static utilities for OmegaT filter plugins.
@@ -92,15 +92,14 @@ public final class PluginUtils
                 {
                     // nothing is really wrong
                     // strange exception
-                    StaticUtils.log("Couldn't access local file system " +      // NOI18N
+                    Log.log("Couldn't access local file system " +      // NOI18N
                             "to get '"+filters[i]+"' !");                       // NOI18N
-                    mue.printStackTrace(StaticUtils.getLogStream());
                 }
                 catch( IOException ioe )
                 {
                     // nothing is really wrong
                     // we just couldn't load one JAR
-                    StaticUtils.log("Couldn't load plugin JAR '"+               // NOI18N
+                    Log.log("Couldn't load plugin JAR '"+               // NOI18N
                             filters[i]+"' !");                                  // NOI18N
                 }
             }

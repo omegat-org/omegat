ea7ed173960e8f314736649e0c270e59484c1d38
Didier Briel <d.briel@free.fr>
2007-01-22 09:57:47 +0000

    [ 1589239 ] Add direct support for Office 2007 XML
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@901 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ src/org/omegat/filters3/xml/openxml/OpenXMLXMLFilter.java
@@ -0,0 +1,60 @@
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool 
+          with fuzzy matching, translation memory, keyword search, 
+          glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2000-2006 Keith Godfrey and Maxym Mykhalchuk
+               Home page: http://www.omegat.org/omegat/omegat.html
+               Support center: http://groups.yahoo.com/group/OmegaT/
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+**************************************************************************/
+
+package org.omegat.filters3.xml.openxml;
+
+import org.omegat.filters2.Instance;
+import org.omegat.filters3.xml.XMLFilter;
+import org.omegat.filters3.xml.openxml.*;
+import org.omegat.util.OConsts;
+
+/**
+ * Filter for Open XML XML files that are inside there the Open XML
+ * file (which is actually a ZIP file).
+ *
+ * @author Maxym Mykhalchuk
+ */
+public class OpenXMLXMLFilter extends XMLFilter
+{
+    
+    /** Creates a new instance of OpenXMLXMLFilter */
+    public OpenXMLXMLFilter()
+    {
+        super(new OpenXMLDialect());
+    }
+
+    public Instance[] getDefaultInstances()
+    {
+        return new Instance[]
+        {
+            new Instance("*.xml", OConsts.UTF8, OConsts.UTF8),                  // NOI18N
+        };
+    }
+
+    public String getFileFormatName()
+    {
+        throw new RuntimeException("Not implemented!");                         // NOI18N
+    }
+    
+}

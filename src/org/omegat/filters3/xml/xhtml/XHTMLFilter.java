adf452227d3f76c1c6c25fa1747e345f6a975090
Maxym Mykhalchuk <mihmax@gmail.com>
2007-02-15 19:01:23 +0000

    #1590169 Incorrect XHTML file not parsed at all
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@955 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/filters3/xml/xhtml/XHTMLFilter.java
+++ src/org/omegat/filters3/xml/xhtml/XHTMLFilter.java
@@ -24,10 +24,11 @@
 
 package org.omegat.filters3.xml.xhtml;
 
-import java.util.regex.Pattern;
+import java.io.File;
 
 import org.omegat.filters2.Instance;
 import org.omegat.filters3.xml.XMLFilter;
+import org.omegat.util.Log;
 import org.omegat.util.OStrings;
 
 /**
@@ -44,36 +45,37 @@ public class XHTMLFilter extends XMLFilter
     public XHTMLFilter()
     {
         super(new XHTMLDialect());
+        do_not_send_to_core = false;
     }
-
+    
     /**
      * Human-readable name of the File Format this filter supports.
-     * 
+     *
      * @return File format name
      */
     public String getFileFormatName()
     {
         return OStrings.getString("XHTML_FILTER_NAME");
     }
-
+    
     /**
      * The default list of filter instances that this filter class has.
      * One filter class may have different filter instances, different
      * by source file mask, encoding of the source file etc.
      * <p>
      * Note that the user may change the instances freely.
-     * 
+     *
      * @return Default filter instances
      */
     public Instance[] getDefaultInstances()
     {
-        return new Instance[] 
+        return new Instance[]
         {
             new Instance("*.html", "UTF-8", "UTF-8"),                           // NOI18N
             new Instance("*.xhtml", "UTF-8", "UTF-8"),                          // NOI18N
         };
     }
-
+    
     /**
      * Yes, DocBook may be read in a variety of encodings.
      * @return <code>true</code>
@@ -90,5 +92,46 @@ public class XHTMLFilter extends XMLFilter
     public boolean isTargetEncodingVariable()
     {
         return true;
+    }
+    
+    /**
+     * Whether we're now processing the XHTML file the first time,
+     * and thus we don't need to send translatable content to OmegaT core.
+     */
+    private boolean do_not_send_to_core;
+    
+    /** Checking whether it is a valid XHTML file. */
+    public boolean isFileSupported(File inFile, String inEncoding)
+    {
+        boolean result = super.isFileSupported(inFile, inEncoding);
+        if (result)
+        {
+            try
+            {
+                do_not_send_to_core = true;
+                super.processFile(inFile, inEncoding, null, null);
+            }
+            catch (Exception e)
+            {
+                Log.log("XHTML file "+inFile.getName()+" is not valid.");
+                result = false;
+            }
+            finally
+            {
+                do_not_send_to_core = false;
+            }
+        }
+        return result;
+    }
+    
+    /**
+     * Overrides superimplementation not to send translatable content on XHTML
+     * validity check. */
+    public String translate(String entry)
+    {
+        if (do_not_send_to_core)
+            return entry;
+        else
+            return super.translate(entry);
     }
 }

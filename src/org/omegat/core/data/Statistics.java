c5133f1c2183dba7c64e73238fe1e1a69cd60469
Alex Buloichik <alex73mail@gmail.com>
2008-03-07 10:18:22 +0000

    Add copyright notes for new files
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@1372 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/core/data/Statistics.java
+++ src/org/omegat/core/data/Statistics.java
@@ -1,3 +1,28 @@
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool 
+          with fuzzy matching, translation memory, keyword search, 
+          glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2000-2006 Keith Godfrey, Maxym Mykhalchuk, and Henry Pijffers
+ Portions copyright 2007 Zoltan Bartko - bartkozoltan@bartkozoltan.com
+               Home page: http://www.omegat.org/omegat/omegat.html
+               Support center: http://groups.yahoo.com/group/OmegaT/
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+**************************************************************************/
+
 package org.omegat.core.data;
 
 import java.io.File;
@@ -20,6 +45,11 @@ import org.omegat.util.StaticUtils;
 
 /**
  * Save project statistic into text file.
+ * 
+ * @author Keith Godfrey
+ * @author Henry Pijffers (henry.pijffers@saxnot.com)
+ * @author Maxym Mykhalchuk
+ * @author Bartko Zoltan
  */
 public class Statistics {
     /**

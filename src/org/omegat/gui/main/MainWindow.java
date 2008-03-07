f66aa5e6e6175197212a3077d204dd7e0524b2f2
Alex Buloichik <alex73mail@gmail.com>
2008-03-07 07:02:15 +0000

    MainWindow refactoring: move docking initialization into separate class
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@1365 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/main/MainWindow.java
+++ src/org/omegat/gui/main/MainWindow.java
@@ -53,13 +53,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JTextPane;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 import javax.swing.text.AbstractDocument;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
@@ -96,6 +94,7 @@ import org.omegat.util.RequestPacket;
 import org.omegat.util.StaticUtils;
 import org.omegat.util.Token;
 import org.omegat.util.WikiGet;
+import org.omegat.util.gui.DockingUI;
 import org.omegat.util.gui.OmegaTFileChooser;
 import org.omegat.util.gui.Styles;
 
@@ -103,7 +102,6 @@ import com.vlsolutions.swing.docking.DockingConstants;
 import com.vlsolutions.swing.docking.DockingDesktop;
 import com.vlsolutions.swing.docking.event.DockableStateWillChangeEvent;
 import com.vlsolutions.swing.docking.event.DockableStateWillChangeListener;
-import com.vlsolutions.swing.docking.ui.DockingUISettings;
 
 /**
  * The main window of OmegaT application.
@@ -146,76 +144,10 @@ public class MainWindow extends JFrame implements ActionListener, WindowListener
         m_autoSpellChecking = Preferences.isPreference(Preferences.ALLOW_AUTO_SPELLCHECKING);
     }
     
-    private ImageIcon getIcon(String iconName)
-    {
-        return new ImageIcon( getClass().getResource(
-                "/org/omegat/gui/resources/" +                                  // NOI18N
-                iconName) );        
-    }
-    
     private void initDocking()
     {
-        DockingUISettings.getInstance().installUI();
-        UIManager.put("DockViewTitleBar.minimizeButtonText", OStrings.getString("DOCKING_HINT_MINIMIZE"));  // NOI18N
-        UIManager.put("DockViewTitleBar.maximizeButtonText", OStrings.getString("DOCKING_HINT_MAXIMIZE"));  // NOI18N
-        UIManager.put("DockViewTitleBar.restoreButtonText", OStrings.getString("DOCKING_HINT_RESTORE"));    // NOI18N
-        UIManager.put("DockViewTitleBar.attachButtonText", OStrings.getString("DOCKING_HINT_DOCK"));        // NOI18N
-        UIManager.put("DockViewTitleBar.floatButtonText", OStrings.getString("DOCKING_HINT_UNDOCK"));       // NOI18N
-        UIManager.put("DockViewTitleBar.closeButtonText", new String());                                            // NOI18N
-        UIManager.put("DockTabbedPane.minimizeButtonText", OStrings.getString("DOCKING_HINT_MINIMIZE"));    // NOI18N
-        UIManager.put("DockTabbedPane.maximizeButtonText", OStrings.getString("DOCKING_HINT_MAXIMIZE"));    // NOI18N
-        UIManager.put("DockTabbedPane.restoreButtonText", OStrings.getString("DOCKING_HINT_RESTORE"));      // NOI18N
-        UIManager.put("DockTabbedPane.floatButtonText", OStrings.getString("DOCKING_HINT_UNDOCK"));         // NOI18N
-        UIManager.put("DockTabbedPane.closeButtonText", new String());
-        
-        UIManager.put("DockViewTitleBar.titleFont", new JLabel().getFont());    // NOI18N
-        
-        UIManager.put("DockViewTitleBar.isCloseButtonDisplayed", Boolean.FALSE);// NOI18N
-        
-        UIManager.put("DockViewTitleBar.hide", getIcon("minimize.gif"));                    // NOI18N
-        UIManager.put("DockViewTitleBar.hide.rollover", getIcon("minimize.rollover.gif"));  // NOI18N
-        UIManager.put("DockViewTitleBar.hide.pressed", getIcon("minimize.pressed.gif"));    // NOI18N
-        UIManager.put("DockViewTitleBar.maximize", getIcon("maximize.gif"));                // NOI18N
-        UIManager.put("DockViewTitleBar.maximize.rollover", getIcon("maximize.rollover.gif"));// NOI18N
-        UIManager.put("DockViewTitleBar.maximize.pressed", getIcon("maximize.pressed.gif"));// NOI18N
-        UIManager.put("DockViewTitleBar.restore", getIcon("restore.gif"));                  // NOI18N
-        UIManager.put("DockViewTitleBar.restore.rollover", getIcon("restore.rollover.gif"));// NOI18N
-        UIManager.put("DockViewTitleBar.restore.pressed", getIcon("restore.pressed.gif"));  // NOI18N
-        UIManager.put("DockViewTitleBar.dock", getIcon("restore.gif"));                     // NOI18N
-        UIManager.put("DockViewTitleBar.dock.rollover", getIcon("restore.rollover.gif"));   // NOI18N
-        UIManager.put("DockViewTitleBar.dock.pressed", getIcon("restore.pressed.gif"));     // NOI18N
-        UIManager.put("DockViewTitleBar.float", getIcon("undock.gif"));                     // NOI18N
-        UIManager.put("DockViewTitleBar.float.rollover", getIcon("undock.rollover.gif"));   // NOI18N
-        UIManager.put("DockViewTitleBar.float.pressed", getIcon("undock.pressed.gif"));     // NOI18N
-        UIManager.put("DockViewTitleBar.attach", getIcon("dock.gif"));                      // NOI18N
-        UIManager.put("DockViewTitleBar.attach.rollover", getIcon("dock.rollover.gif"));    // NOI18N
-        UIManager.put("DockViewTitleBar.attach.pressed", getIcon("dock.pressed.gif"));      // NOI18N
-        
-        UIManager.put("DockViewTitleBar.menu.hide", getIcon("minimize.gif"));                      // NOI18N
-        UIManager.put("DockViewTitleBar.menu.maximize", getIcon("maximize.gif"));                  // NOI18N
-        UIManager.put("DockViewTitleBar.menu.restore", getIcon("restore.gif"));                   // NOI18N
-        UIManager.put("DockViewTitleBar.menu.dock", getIcon("restore.gif"));                      // NOI18N
-        UIManager.put("DockViewTitleBar.menu.float", getIcon("undock.gif"));                     // NOI18N
-        UIManager.put("DockViewTitleBar.menu.attach", getIcon("dock.gif"));                    // NOI18N
-        
-        UIManager.put("DockViewTitleBar.menu.close", getIcon("empty.gif"));                     // NOI18N
-        UIManager.put("DockTabbedPane.close", getIcon("empty.gif"));                            // NOI18N
-        UIManager.put("DockTabbedPane.close.rollover", getIcon("empty.gif"));                   // NOI18N
-        UIManager.put("DockTabbedPane.close.pressed", getIcon("empty.gif"));                    // NOI18N
-        UIManager.put("DockTabbedPane.menu.close", getIcon("empty.gif"));                       // NOI18N
-        UIManager.put("DockTabbedPane.menu.hide", getIcon("empty.gif"));                        // NOI18N
-        UIManager.put("DockTabbedPane.menu.maximize", getIcon("empty.gif"));                    // NOI18N
-        UIManager.put("DockTabbedPane.menu.float", getIcon("empty.gif"));                       // NOI18N
-        UIManager.put("DockTabbedPane.menu.closeAll", getIcon("empty.gif"));                    // NOI18N
-        UIManager.put("DockTabbedPane.menu.closeAllOther", getIcon("empty.gif"));               // NOI18N
-        
-        UIManager.put("DockingDesktop.closeActionAccelerator", null);           // NOI18N
-        UIManager.put("DockingDesktop.maximizeActionAccelerator", null);        // NOI18N
-        UIManager.put("DockingDesktop.dockActionAccelerator", null);            // NOI18N
-        UIManager.put("DockingDesktop.floatActionAccelerator", null);           // NOI18N
+        DockingUI.initialize();
 
-        UIManager.put("DragControler.detachCursor", getIcon("undock.gif").getImage());  // NOI18N
-        
         editorScroller = new DockableScrollPane("EDITOR", " ", editor, false);  // NOI18N
         editorScroller.setMinimumSize(new Dimension(100, 100));
         matchesScroller = new DockableScrollPane("MATCHES",                     // NOI18N

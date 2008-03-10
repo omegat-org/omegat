991c4dfebb0559de4550aed649841d2209e142e6
Alex Buloichik <alex73mail@gmail.com>
2008-03-10 17:30:53 +0000

    Add some utils
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@1398 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ src/org/omegat/util/gui/UIThreadsUtil.java
@@ -0,0 +1,70 @@
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool 
+          with fuzzy matching, translation memory, keyword search, 
+          glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2008 Alex Buloichik
+               Home page: http://www.omegat.org/
+               Support center: http://groups.yahoo.com/group/OmegaT/
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ **************************************************************************/
+
+package org.omegat.util.gui;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * Utils for check UI threads and run specific code in UI threads only.
+ * 
+ * @author Alex Buloichik (alex73mail@gmail.com)
+ */
+public class UIThreadsUtil {
+    private static final Logger LOGGER = Logger.getLogger(UIThreadsUtil.class.getName());
+
+    /**
+     * Execute code in swing thread only.
+     */
+    public static void executeInSwingThread(final Runnable code) {
+        if (SwingUtilities.isEventDispatchThread()) {
+            code.run();
+        } else {
+            SwingUtilities.invokeLater(code);
+        }
+    }
+
+    /**
+     * Check if current thread is swing thread, and report error in log if it's
+     * not true.
+     */
+    public static void mustBeSwingThread() {
+        if (!SwingUtilities.isEventDispatchThread()) {
+            LOGGER.log(Level.SEVERE, "This call must be in swing thread", new Exception());
+        }
+    }
+
+    /**
+     * Check if current thread is NOT swing thread, and report error in log if
+     * it's not true.
+     */
+    public static void mustNotBeSwingThread() {
+        if (!SwingUtilities.isEventDispatchThread()) {
+            LOGGER.log(Level.SEVERE, "This call must be in swing thread", new Exception());
+        }
+    }
+}

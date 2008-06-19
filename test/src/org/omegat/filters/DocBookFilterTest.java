345c58ced3c37bf3c0b667befd9b0d88f97bc5ae
Alex Buloichik <alex73mail@gmail.com>
2008-06-19 06:09:28 +0000

    Unit tests: add new DocBook test
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@1759 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- test/src/org/omegat/filters/DocBookFilterTest.java
+++ test/src/org/omegat/filters/DocBookFilterTest.java
@@ -20,18 +20,29 @@
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-**************************************************************************/
+ **************************************************************************/
 
 package org.omegat.filters;
+
+import java.util.List;
 
 import org.omegat.filters3.xml.docbook.DocBookFilter;
 
 public class DocBookFilterTest extends TestFilterBase {
     public void testParse() throws Exception {
-        parse(new DocBookFilter(), "test/data/filters/docBook/file-DocBookFilter.xml");
+        parse(new DocBookFilter(),
+                "test/data/filters/docBook/file-DocBookFilter.xml");
     }
 
     public void testTranslate() throws Exception {
-        translateText(new DocBookFilter(), "test/data/filters/docBook/file-DocBookFilter.xml");
+        translateText(new DocBookFilter(),
+                "test/data/filters/docBook/file-DocBookFilter.xml");
+    }
+
+    public void testParseIntroLinux() throws Exception {
+        List<String> lines = parse(new DocBookFilter(),
+                "test/data/filters/docBook/Intro-Linux/abook.xml");
+        assertTrue("Message not exist, i.e. entities not loaded", lines
+                .contains("Why should I use an editor?"));
     }
 }

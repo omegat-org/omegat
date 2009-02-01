285fdf61fe929c2cacc7bb0b19482a52c651a3a6
Didier Briel <d.briel@free.fr>
2009-02-01 17:38:10 +0000

    Remove unused imports
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@1998 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/core/spellchecker/SpellChecker.java
+++ src/org/omegat/core/spellchecker/SpellChecker.java
@@ -30,8 +30,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;

1a7873146842aa1026778c3b111c574aa68470f7
Didier Briel <d.briel@free.fr>
2009-05-02 20:31:23 +0000

    XLIFF filter: the content of <seg-source> was visible
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@2158 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/filters3/xml/xliff/XLIFFDialect.java
+++ src/org/omegat/filters3/xml/xliff/XLIFFDialect.java
@@ -4,7 +4,7 @@
           glossaries, and translation leveraging into updated projects.
 
  Copyright (C) 2000-2006 Keith Godfrey and Maxym Mykhalchuk
-           (C) 2007 Didier Briel
+           (C) 2007-2009 Didier Briel
  
                Home page: http://www.omegat.org/
                Support center: http://groups.yahoo.com/group/OmegaT/
@@ -63,6 +63,7 @@ public class XLIFFDialect extends DefaultXMLDialect
             "ept",                                                              // NOI18N
             "it",                                                               // NOI18N
             "context",                                                          // NOI18N
+            "seg-source",                                                       // NOI18N
         });
     }
 }

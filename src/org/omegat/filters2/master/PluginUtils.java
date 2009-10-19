d236066168aabe91f31376edbe80a1b5774f3c76
Alex Buloichik <alex73mail@gmail.com>
2009-10-19 08:31:31 +0000

    Plugins changes: new code for plugins loading
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@2440 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/filters2/master/PluginUtils.java
+++ src/org/omegat/filters2/master/PluginUtils.java
@@ -26,13 +26,16 @@ package org.omegat.filters2.master;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.Attributes;
@@ -243,6 +246,99 @@ public final class PluginUtils
     /** Class Loader for filter plugins. */
     private static ClassLoader plugins_cl = null;
     
+    
+    /**
+     * Loads all plugins from main classloader and from /plugins/ dir. We should
+     * load all jars from /plugins/ dir first, because some plugin can use more
+     * than one jar.
+     */
+    public static void loadPlugins2() {
+        File pluginsDir = new File("plugins/");
+        try {
+            URLClassLoader cls;
+            URL[] urls;
+            // list all jars in /plugins/
+            File[] fs = pluginsDir.listFiles(new JARorFolderFileFilter());
+            if (fs != null) {
+                urls = new URL[fs.length];
+                for (int i = 0; i < urls.length; i++) {
+                    urls[i] = fs[i].toURL();
+                }
+            } else {
+                urls = new URL[0];
+            }
+            boolean foundMain = false;
+            // look on all manifests
+            cls = new URLClassLoader(urls, PluginUtils.class.getClassLoader());
+            for (Enumeration<URL> mlist = cls
+                    .getResources("META-INF/MANIFEST.MF"); mlist
+                    .hasMoreElements();) {
+                URL mu = mlist.nextElement();
+                System.out.println(mu);
+                InputStream in = mu.openStream();
+                Manifest m;
+                try {
+                    m = new Manifest(in);
+                } finally {
+                    in.close();
+                }
+                if ("org.omegat.Main".equals(m.getMainAttributes().getValue(
+                        "Main-Class"))) {
+                    // found main manifest - not in development mode
+                    foundMain = true;
+                }
+                loadByManifest(m, cls);
+                System.out.println();
+            }
+            if (!foundMain) {
+                // development mode - load main menifest template
+                File mf = new File("manifest-template.mf");
+                if (mf.exists()) {
+                    InputStream in = new FileInputStream(mf);
+                    Manifest m;
+                    try {
+                        m = new Manifest(in);
+                    } finally {
+                        in.close();
+                    }
+                    loadByManifest(m, cls);
+                }
+            }
+        } catch (Exception ex) {
+            Log.log(ex);
+        }
+    }
+
+    public static List<Class<?>> getFilterClasses() {
+        return filterClasses;
+    }
+
+    protected static List<Class<?>> filterClasses = new ArrayList<Class<?>>();
+
+    /**
+     * Parse one manifest file.
+     * 
+     * @param m
+     *            manifest
+     * @param classLoader
+     *            classloader
+     * @throws ClassNotFoundException
+     */
+    protected static void loadByManifest(final Manifest m,
+            final ClassLoader classLoader) throws ClassNotFoundException {
+        if (m.getMainAttributes().getValue("OmegaT-Plugin") == null) {
+            return;
+        }
+
+        Map<String, Attributes> entries = m.getEntries();
+        for (String key : entries.keySet()) {
+            Attributes attrs = (Attributes) entries.get(key);
+            String isfilter = attrs.getValue("OmegaT-Filter");
+            if ("true".equals(isfilter)) {
+                filterClasses.add(classLoader.loadClass(key));
+            }
+        }
+    }
 }
 
 /** File filter that accepts JAR files or directories. */

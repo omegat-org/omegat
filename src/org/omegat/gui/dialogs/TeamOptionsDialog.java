0290e2cc6fc7943a33ecf955bdbd6685c9d3d886
Didier Briel <d.briel@free.fr>
2009-12-11 17:38:08 +0000

    Update contribution
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@2624 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/dialogs/TeamOptionsDialog.java
+++ src/org/omegat/gui/dialogs/TeamOptionsDialog.java
@@ -4,7 +4,7 @@
           glossaries, and translation leveraging into updated projects.
 
  Copyright (C) 2000-2006 Keith Godfrey and Maxym Mykhalchuk
-               2009 Didier Briel
+               2009 Didier Briel, Martin Fleurke
                Home page: http://www.omegat.org/
                Support center: http://groups.yahoo.com/group/OmegaT/
 

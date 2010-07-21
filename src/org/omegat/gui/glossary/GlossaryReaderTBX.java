ba6e4d0d656040f99ff30c2e042f2e327e84be8f
Didier Briel <d.briel@free.fr>
2010-07-21 15:51:33 +0000

    Prevent having blank lines between comments
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@3062 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/glossary/GlossaryReaderTBX.java
+++ src/org/omegat/gui/glossary/GlossaryReaderTBX.java
@@ -162,6 +162,8 @@ public class GlossaryReaderTBX {
     }
     
     protected static void appendLine(final StringBuilder str, String line) {
+        if (! (line.length() > 0)) // No need to append empty lines
+            return;
         if (str.length() > 0) {
             str.append('\n');
         }

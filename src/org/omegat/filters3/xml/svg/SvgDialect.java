961da409c4b4f67e084be60c6df55758519c547b
Didier Briel <d.briel@free.fr>
2010-08-23 08:47:50 +0000

    SVG image filter
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@3080 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ src/org/omegat/filters3/xml/svg/SvgDialect.java
@@ -0,0 +1,46 @@
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool 
+          with fuzzy matching, translation memory, keyword search, 
+          glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2000-2006 Keith Godfrey and Maxym Mykhalchuk
+               2010 Didier Briel
+
+               Home page: http://www.omegat.org/
+               Support center: http://groups.yahoo.com/group/OmegaT/
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ **************************************************************************/
+
+package org.omegat.filters3.xml.svg;
+
+import java.util.regex.Pattern;
+
+import org.omegat.filters3.xml.DefaultXMLDialect;
+
+/**
+ * XML dialect declaration for the SVG filter.
+ * 
+ * @author Didier Briel
+ */
+public class SvgDialect extends DefaultXMLDialect {
+    public static final Pattern ROOT_PATTERN =
+            Pattern.compile("svg");
+
+    public SvgDialect() {
+        defineConstraint(CONSTRAINT_ROOT, ROOT_PATTERN);
+        defineParagraphTags(new String[] { "text" });
+    }
+}

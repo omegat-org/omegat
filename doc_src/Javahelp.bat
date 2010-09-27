78e4fb5004450e8eb7fee0d70dc40a3b10bfb7e7
Didier Briel <d.briel@free.fr>
2010-09-27 21:05:08 +0000

    Handle target directories dynamically
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@3109 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- doc_src/Javahelp.bat
+++ doc_src/Javahelp.bat
@@ -1,4 +1,12 @@
 if %1.==. exit /b
 call Xincludes %1
+
+cd %1
+md html
+cd html
+md javahelp
+cd..
+cd..
+
 java com.icl.saxon.StyleSheet %1\index.xml c:\dbk\javahelp\javahelp.xsl use.id.as.filename=1 base.dir=%1/javahelp/ use.extensions=1 chapter.autolabel=I section.autolabel=1 tablecolumns.extension=0 toc.max.depth=2 generate.toc="book toc,title,figure,table chapter toc appendix toc" generate.index=1
 copy %1\images %1\javahelp\images

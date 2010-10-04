c33447d48cc7e23ae2c9cef309c5bcab14135105
Didier Briel <d.briel@free.fr>
2010-10-04 07:29:48 +0000

    Fix to be able to go back to where we started
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@3123 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- doc_src/PDF.bat
+++ doc_src/PDF.bat
@@ -4,4 +4,6 @@ call fo %1
 copy %1\images %1\pdf\images
 cd %1
 cd pdf
-fop -c ..\..\fop.xconf -fo OmegaT_documentation.fo -pdf OmegaT_documentation.pdf -dpi 1200 -r
+call fop -c ..\..\fop.xconf -fo OmegaT_documentation.fo -pdf OmegaT_documentation.pdf -dpi 1200 -r
+cd..
+cd..

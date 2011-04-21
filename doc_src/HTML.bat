ba685c2243ab785b882e090fa2d16851ef07b8a0
Didier Briel <d.briel@free.fr>
2011-04-21 20:18:55 +0000

    Correct generation of instantStartGuideNoTOC.html
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@3495 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- doc_src/HTML.bat
+++ doc_src/HTML.bat
@@ -7,6 +7,6 @@ md images
 cd..
 cd..
 java com.icl.saxon.StyleSheet %1\index.xml c:\dbk\html\chunk.xsl use.id.as.filename=1 base.dir=%1/html/  chunk.section.depth=0 chunk.first.sections=0 use.extensions=1 chapter.autolabel=1 section.autolabel=1 tablecolumns.extension=0 toc.max.depth=2 generate.toc="book toc,title,figure,table chapter toc appendix toc" generate.index=1
-java com.icl.saxon.StyleSheet -o %1\html\instantStartGuideNoTOC.html %1\02_InstantStartGuide.xml C:\dbk\html\docbook.xsl base.dir=%1/html/ chunk.section.depth=0 chunk.first.sections=0 use.extensions=1 chapter.autolabel=0 section.autolabel=1 tablecolumns.extension=0 toc.max.depth=0 generate.toc=0 generate.index=0
+java com.icl.saxon.StyleSheet -o %1\html\instantStartGuideNoTOC.html %1\InstantStartGuide.xml C:\dbk\html\docbook.xsl base.dir=%1/html/ chunk.section.depth=0 chunk.first.sections=0 use.extensions=1 chapter.autolabel=0 section.autolabel=1 tablecolumns.extension=0 toc.max.depth=0 generate.toc=0 generate.index=0
 
 copy %1\images %1\html\images

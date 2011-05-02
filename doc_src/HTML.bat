d8c21e705688f98e7c9b3cbbadda192c8e41b09d
Didier Briel <d.briel@free.fr>
2011-05-02 09:43:41 +0000

    Use real target for HTML
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@3537 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- doc_src/HTML.bat
+++ doc_src/HTML.bat
@@ -1,13 +1,19 @@
 if %1.==. exit /b
+
 call Xincludes %1
 call classpath
-cd %1
-md html
-cd html
-md images
-cd..
-cd..
-java com.icl.saxon.StyleSheet %1\index.xml c:\dbk\html\chunk.xsl use.id.as.filename=1 base.dir=%1/html/  chunk.section.depth=0 chunk.first.sections=0 use.extensions=1 chapter.autolabel=1 section.autolabel=1 tablecolumns.extension=0 toc.max.depth=2 generate.toc="book toc,title,figure,table chapter toc appendix toc" generate.index=1
-java com.icl.saxon.StyleSheet -o %1\html\instantStartGuideNoTOC.html %1\InstantStartGuide.xml C:\dbk\html\docbook.xsl base.dir=%1/html/ chunk.section.depth=0 chunk.first.sections=0 use.extensions=1 chapter.autolabel=0 section.autolabel=1 tablecolumns.extension=0 toc.max.depth=0 generate.toc=0 generate.index=0
 
-copy %1\images %1\html\images
+cd ..\docs\
+md %1
+cd %1
+md images
+cd ..
+cd ..\doc_src
+
+java com.icl.saxon.StyleSheet %1\index.xml c:\dbk\html\chunk.xsl use.id.as.filename=1 base.dir=../docs/%1/  chunk.section.depth=0 chunk.first.sections=0 use.extensions=1 chapter.autolabel=1 section.autolabel=1 tablecolumns.extension=0 toc.max.depth=2 generate.toc="book toc,title,figure,table chapter toc appendix toc" generate.index=1
+
+java com.icl.saxon.StyleSheet -o ..\docs\%1\instantStartGuideNoTOC.html %1\InstantStartGuide.xml C:\dbk\html\docbook.xsl base.dir=../docs/%1/ chunk.section.depth=0 chunk.first.sections=0 use.extensions=1 chapter.autolabel=0 section.autolabel=1 tablecolumns.extension=0 toc.max.depth=0 generate.toc=0 generate.index=0
+
+copy %1\images ..\docs\%1\images
+copy %1\license.txt ..\docs\%1
+copy %1\version.properties ..\docs\%1

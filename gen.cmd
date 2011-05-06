54c0a7dc8ecac326a4e4a01d271ddd25856eca4b
Alex Buloichik <alex73mail@gmail.com>
2011-05-06 07:52:40 +0000

    Convert tmx14.dtd into xsd (using Altova XMLSpy)
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@3562 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- gen.cmd
+++ gen.cmd
@@ -6,6 +6,6 @@ xjc src/schemas/project_properties.xsd -d src -p gen.core.project
 #xjc src/schemas/tmx11.xsd -dtd -d src -p gen.core.tmx11
 # do not forget to change xml:lang:
 #    @XmlAttribute(name = "lang", namespace="http://www.w3.org/XML/1998/namespace", required = true)
-xjc src/schemas/tmx14.xsd -dtd -d src -p gen.core.tmx14 
+xjc src/schemas/tmx14.xsd -b src/schemas/tmx14.xjb -d src -p gen.core.tmx14 
 
 pause

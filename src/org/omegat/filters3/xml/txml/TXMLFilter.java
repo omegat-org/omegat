b717867c588ddc368e8558b13342dc97e85f3812
Didier Briel <d.briel@free.fr>
2011-07-19 07:57:20 +0000

    Wordfast TXML filter
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@3715 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ src/org/omegat/filters3/xml/txml/TXMLFilter.java
@@ -0,0 +1,92 @@
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool 
+          with fuzzy matching, translation memory, keyword search, 
+          glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2000-2006 Keith Godfrey and Maxym Mykhalchuk
+               2011 Didier Briel
+               Home page: http://www.omegat.org/
+               Support center: http://groups.yahoo.com/group/OmegaT/
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ **************************************************************************/
+
+package org.omegat.filters3.xml.txml;
+
+import org.omegat.filters2.Instance;
+import org.omegat.filters3.xml.XMLFilter;
+import org.omegat.util.OStrings;
+
+/**
+ * Filter for TXML files.
+ * 
+ * @author Didier Briel
+ */
+public class TXMLFilter extends XMLFilter {
+
+    /**
+     * Creates a new instance of TXMLFilter
+     */
+    public TXMLFilter() {
+        super(new TXMLDialect());
+    }
+
+    /**
+     * Human-readable name of the File Format this filter supports.
+     * 
+     * @return File format name
+     */
+    public String getFileFormatName() {
+        return OStrings.getString("TXML_FILTER_NAME");
+    }
+
+    /**
+     * The default list of filter instances that this filter class has. One
+     * filter class may have different filter instances, different by source
+     * file mask, encoding of the source file etc.
+     * <p>
+     * Note that the user may change the instances freely.
+     * 
+     * @return Default filter instances
+     */
+    public Instance[] getDefaultInstances() {
+        return new Instance[] { new Instance("*.txml", null, null), };
+    }
+
+    /**
+     * Either the encoding can be read, or it is UTF-8.
+     * 
+     * @return <code>false</code>
+     */
+    @Override
+    public boolean isSourceEncodingVariable() {
+        return false;
+    }
+
+    /**
+     * Yes, TXML may be written out in a variety of encodings.
+     * 
+     * @return <code>true</code>
+     */
+    @Override
+    public boolean isTargetEncodingVariable() {
+        return true;
+    }
+    
+    @Override
+    protected boolean requirePrevNextFields() {
+        return true;
+    }
+}

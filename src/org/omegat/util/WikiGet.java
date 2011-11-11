2b1f9b561375b074184bab6a93c6fb71842a62a8
Martin Fleurke <martin.fleurke@portavita.nl>
2011-11-11 13:30:41 +0000

    little cleanup of comments
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@3905 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/util/WikiGet.java
+++ src/org/omegat/util/WikiGet.java
@@ -174,16 +174,12 @@ public class WikiGet {
      */
     public static String getURL(String target) throws IOException {
         StringBuffer page = new StringBuffer();
-//        try {
-            URL url = new URL(target);
-            InputStream in = url.openStream();
-            byte[] b = new byte[4096];
-            for (int n; (n = in.read(b)) != -1;) {
-                page.append(new String(b, 0, n, "UTF-8"));
-            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        URL url = new URL(target);
+        InputStream in = url.openStream();
+        byte[] b = new byte[4096];
+        for (int n; (n = in.read(b)) != -1;) {
+            page.append(new String(b, 0, n, "UTF-8"));
+        }
         return page.toString();
     }
 

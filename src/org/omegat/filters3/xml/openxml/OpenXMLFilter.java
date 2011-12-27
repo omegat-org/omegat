658709ac8ac2617b2992e7eddff60a37e4f72d14
Didier Briel <d.briel@free.fr>
2011-12-27 10:57:58 +0000

    Some Open XML files may use a backslash instead of a slash in the zip container
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@3995 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/filters3/xml/openxml/OpenXMLFilter.java
+++ src/org/omegat/filters3/xml/openxml/OpenXMLFilter.java
@@ -4,7 +4,7 @@
           glossaries, and translation leveraging into updated projects.
 
  Copyright (C) 2000-2006 Keith Godfrey and Maxym Mykhalchuk
-               2007-2010 Didier Briel
+               2007-2011 Didier Briel
                Home page: http://www.omegat.org/
                Support center: http://groups.yahoo.com/group/OmegaT/
 
@@ -163,8 +163,11 @@ public class OpenXMLFilter extends AbstractFilter {
      * Receives a filename with a path Returns a string without the path
      */
     private String removePath(String fileName) {
-        if (fileName.lastIndexOf('/') >= 0)
+        if (fileName.lastIndexOf('/') >= 0) {
             fileName = fileName.substring(fileName.lastIndexOf('/') + 1);
+        }  else if (fileName.lastIndexOf('\\') >= 0) { // Some weird files may use a backslash
+            fileName = fileName.substring(fileName.lastIndexOf('\\') + 1);
+        }
         return fileName;
     }
 

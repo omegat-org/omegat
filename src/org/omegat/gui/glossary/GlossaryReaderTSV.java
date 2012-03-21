791c680ff53d6d363f75a72f12f3719d0838983d
Didier Briel <d.briel@free.fr>
2012-03-21 14:21:15 +0000

    Move glossary file extensions to OConsts
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@4202 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/glossary/GlossaryReaderTSV.java
+++ src/org/omegat/gui/glossary/GlossaryReaderTSV.java
@@ -51,10 +51,10 @@ public class GlossaryReaderTSV {
     public static List<GlossaryEntry> read(final File file) throws IOException {
         InputStreamReader reader;
         String fname_lower = file.getName().toLowerCase();
-        if (fname_lower.endsWith(GlossaryManager.EXT_TSV_DEF)) {
+        if (fname_lower.endsWith(OConsts.EXT_TSV_DEF)) {
             reader = new InputStreamReader(new FileInputStream(file));
-        } else if (fname_lower.endsWith(GlossaryManager.EXT_TSV_UTF8)
-                || fname_lower.endsWith(GlossaryManager.EXT_TSV_TXT)) {
+        } else if (fname_lower.endsWith(OConsts.EXT_TSV_UTF8)
+                || fname_lower.endsWith(OConsts.EXT_TSV_TXT)) {
             InputStream fis = new FileInputStream(file);
             reader = new InputStreamReader(fis, OConsts.UTF8);
         } else {

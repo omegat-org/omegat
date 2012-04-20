dd4a7b15d32f6f768e33ad72bff640e2dcd350de
Alex Buloichik <alex73mail@gmail.com>
2012-04-20 20:24:53 +0000

    Add standard GPL header
    git-svn-id: https://omegat.svn.sourceforge.net/svnroot/omegat/trunk@4283 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/dialogs/NewTeamProject.java
+++ src/org/omegat/gui/dialogs/NewTeamProject.java
@@ -1,14 +1,34 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool
+          with fuzzy matching, translation memory, keyword search,
+          glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2012 Alex Buloichick
+               Home page: http://www.omegat.org/
+               Support center: http://groups.yahoo.com/group/OmegaT/
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ **************************************************************************/
+
 package org.omegat.gui.dialogs;
 
 import org.omegat.util.OStrings;
 
 /**
  *
- * @author alex
+ * @author Alex Buloichik (alex73mail@gmail.com)
  */
 public class NewTeamProject extends javax.swing.JDialog {
     /**

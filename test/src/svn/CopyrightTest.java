f073394f085232bc7aaccff31edc73d635d1bd7a
Alex Buloichik <alex73mail@gmail.com>
2013-04-09 04:24:17 +0000

    Move to GPLv3
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@5116 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- test/src/svn/CopyrightTest.java
+++ test/src/svn/CopyrightTest.java
@@ -14,7 +14,7 @@
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
+ OmegaT is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
@@ -45,7 +45,7 @@ public class CopyrightTest extends TestCase {
             "OmegaT is free software: you can redistribute it and/or modify",
             "it under the terms of the GNU General Public License as published by",
             "the Free Software Foundation, either version 3 of the License, or", "(at your option) any later version.",
-            "This program is distributed in the hope that it will be useful,",
+            "OmegaT is distributed in the hope that it will be useful,",
             "but WITHOUT ANY WARRANTY; without even the implied warranty of",
             "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
             "GNU General Public License for more details.",

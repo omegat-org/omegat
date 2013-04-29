c874ec6d739949d90c70ab382382c809c02cd91c
Didier Briel <d.briel@free.fr>
2013-04-29 18:25:22 +0000

    Merged revision(s) 5294 from branches/release-2-6/release/changes.txt: Merged revision(s) 5294 from branches/release-2-6/doc_src/gendoc.bat: Galician documpentation backported from 3.0
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@5295 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- doc_src/gendoc.bat
+++ doc_src/gendoc.bat
@@ -1,6 +1,7 @@
 call ant -Dlanguage=cs all
 call ant -Dlanguage=en all
 call ant -Dlanguage=es all
+call ant -Dlanguage=gl all
 call ant -Dlanguage=it all
 call ant -Dlanguage=ja html
 call ant -Dlanguage=nl all

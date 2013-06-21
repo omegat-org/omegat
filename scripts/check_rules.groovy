29b077205f0922277b9728f8dfabbce3319c90fc
Didier Briel <d.briel@free.fr>
2013-06-21 16:27:28 +0000

    The "check-rules" script now has a GUI
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@5515 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- scripts/check_rules.groovy
+++ scripts/check_rules.groovy
@@ -1,13 +1,33 @@
+import groovy.swing.SwingBuilder
+import java.awt.Component
+import javax.swing.JButton
+import javax.swing.JTable
+import javax.swing.table.*
+import javax.swing.event.*
+import java.awt.event.*
+import javax.swing.JOptionPane.*
+import org.omegat.util.Platform.*
+import java.awt.BorderLayout as BL
 /*
  The rules are based on the Checkmate Quality check 
  http://www.opentag.com/okapi/wiki/index.php?title=CheckMate_-_Quality_Check_Configuration
  Each rule is a block of groovy code, 'source' and 'target' are the two parameters of this block
  */
 
+def prop = project.projectProperties
+if (!prop) {
+  final def title = 'Check rules'
+  final def msg   = 'Please try again after you open a project.'
+  showMessageDialog null, msg, title, INFORMATION_MESSAGE
+  return
+}
+
+data=[]
 console.println("Check rules.\n");
 
 // Prefs
-maxCharLengthAbove=200
+maxCharLengthAbove=240
+minCharLengthAbove=40
 
 rules = [
             
@@ -16,9 +36,9 @@ rules = [
             targetTrailingWhiteSpaces: { s, t -> t =~ /\s+$/ },
             // Segment verification
             doubledWords: { s, t -> t =~ /(?i)(\b\w+)\s+\1\b/ },
-			// Length
-		    //targetShorter: { s, t -> t.length <  }
-			//targetLonger: { s, t -> (t.length() / s.length() * 100) > maxCharLengthAbove }
+           // Length
+           targetShorter: { s, t -> (t.length() / s.length() * 100) < minCharLengthAbove },
+           targetLonger: { s, t -> (t.length() / s.length() * 100) > maxCharLengthAbove }
         ];
 
 segment_count = 0;
@@ -41,6 +61,7 @@ for (i in 0 ..< files.size()) {
         rules.each { k, v ->
             if (rules[k](source, target)) {
                 console.println(ste.entryNum() + "\t" + k + /*"\t[" + source + "]" + */"\t[" + target + "]");
+                data.add([ seg: ste.entryNum(), rule: k, source: source, target: target ]);	
                 segment_count++;
             }
         }
@@ -50,4 +71,57 @@ for (i in 0 ..< files.size()) {
 console.println("Segments found : " + segment_count);
 
 
+swing = new SwingBuilder()
 
+frame = swing.frame(title:'Check rules', preferredSize: [800, 500]) {
+    scrollPane {
+        table() {
+            tableModel(list:data) {
+                propertyColumn(editable: true, header:'Segment', propertyName:'seg', minWidth: 80, maxWidth: 80, preferredWidth: 80,
+                        cellEditor: new TableCellEditor()
+                        {
+                            public void cancelCellEditing()                             {}
+                            public boolean stopCellEditing()                            {   return false;   }
+                            public Object getCellEditorValue()                          {   return value;   }
+                            public boolean isCellEditable(EventObject anEvent)          {   return true;    }
+                            public boolean shouldSelectCell(EventObject anEvent)        {   return true;   }
+                            public void addCellEditorListener(CellEditorListener l)     {}
+                            public void removeCellEditorListener(CellEditorListener l)  {}
+                            public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column)
+                            {
+                                println("value: " + value);
+                                org.omegat.core.Core.getEditor().gotoEntry(value);
+                            }
+                            
+                        },
+                        cellRenderer: new TableCellRenderer()
+                        {
+                            public Component getTableCellRendererComponent(JTable table,
+                            Object value,
+                            boolean isSelected,
+                            boolean hasFocus,
+                            int row,
+                            int column)
+                            {
+                                def btn = new JButton()
+                                btn.setText(value.toString())
+                                return btn
+                                
+                            }
+                        }
+                        )                
+                propertyColumn(editable: false, header:'Rule',propertyName:'rule', minWidth: 120, maxWidth: 200, preferredWidth: 150)
+                propertyColumn(editable: false, header:'Source',propertyName:'source', minWidth: 150, preferredWidth: 350)
+                propertyColumn(editable: false, header:'Target',propertyName:'target', minWidth: 150, preferredWidth: 350)
+            }
+        }
+        
+    }
+     panel(constraints: BL.SOUTH){
+            button('Quit', actionPerformed:{
+                frame.visible = false
+            })
+		}
+}
+frame.pack()
+frame.show()

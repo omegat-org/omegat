753928798e22948129e8eb3582e805ef891f7b20
Didier Briel <d.briel@free.fr>
2013-07-03 08:52:10 +0000

    Merged revision(s) 5536 from branches/release-2-6/src/org/omegat/gui/main/MainWindowUI.java: Restore default window when saved layout cannot be restored https://sourceforge.net/p/omegat/bugs/602/
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@5537 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/main/MainWindowUI.java
+++ src/org/omegat/gui/main/MainWindowUI.java
@@ -172,7 +172,8 @@ public class MainWindowUI {
                     in.close();
                 }
             } catch (Exception e) {
-                Log.log(e);
+                Log.log(e);                     // In case something wrong happened, it's better to have a default
+                resetDesktopLayout(mainWindow); // screen than a blank one
             }
         }
     }

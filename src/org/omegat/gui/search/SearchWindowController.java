ccb0684b73190e7b20da408ff8d940c4999dc6cc
Aaron Madlon-Kay <aaron@madlon-kay.com>
2013-11-18 06:31:05 +0000

    Make Ctrl+F (Cmd+F) in search window re-focus on search field
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@5756 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/search/SearchWindowController.java
+++ src/org/omegat/gui/search/SearchWindowController.java
@@ -32,6 +32,7 @@ package org.omegat.gui.search;
 
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -218,6 +219,18 @@ public class SearchWindowController {
         };
         form.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escape, "ESCAPE");
         form.getRootPane().getActionMap().put("ESCAPE", escapeAction);
+        
+        // Make Ctrl+F re-focus on search field
+        KeyStroke find = KeyStroke.getKeyStroke(KeyEvent.VK_F,
+                Toolkit.getDefaultToolkit().getMenuShortcutKeyMask(), false);
+        Action focusAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                form.m_searchField.requestFocus();
+            }
+        };
+        form.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(find, "CTRL+F");
+        form.getRootPane().getActionMap().put("CTRL+F", focusAction);
 
         // need to control check boxes and radio buttons manually
         //

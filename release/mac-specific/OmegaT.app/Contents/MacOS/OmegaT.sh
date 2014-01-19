9b424da20f3cb04d625a8ce04103b098931c4c72
Didier Briel <d.briel@free.fr>
2014-01-19 14:12:17 +0000

    Add "standard" parameters to Mac versions
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@5901 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- release/mac-specific/OmegaT.app/Contents/MacOS/OmegaT.sh
+++ release/mac-specific/OmegaT.app/Contents/MacOS/OmegaT.sh
@@ -2,5 +2,15 @@
 
 cd `dirname $0`/Java
 
-#GOOGLE_API_KEY="-Dgoogle.api.key=0123456789A0123456789B0123456789C0123456789D"
-../jre/bin/java -Xmx1024m ${GOOGLE_API_KEY} -jar OmegaT.jar
+#LANGUAGE="-Duser.language=en"
+#COUNTRY="-Duser.country=GB"
+# Settings to access the Internet behind a proxy
+#PROXY_HOST-Dhttp.proxyHost=192.168.1.1
+#PROXY_PORT=-Dhttp.proxyPort=3128
+# Google Translate v2 API key
+#GOOGLE_API_KEY=""-Dgoogle.api.key=0123456789A0123456789B0123456789C0123456789D"
+# Microsoft Translator credentials
+#MS_CLIENT_ID="-Dmicrosoft.api.client_id=xxxxx"
+#MS_CLIENT_SECRET="-Dmicrosoft.api.client_secret=xxxxx'
+
+../jre/bin/java -Xmx1024m ${LANGUAGE} ${COUNTRY} ${PROXY_HOST} ${PROXY_PORT} ${GOOGLE_API_KEY} ${MS_CLIENT_ID} ${MS_CLIENT_SECRET} -jar OmegaT.jar

26934594ef08ef807457643ed04d817d1c6bc315
Didier Briel <d.briel@free.fr>
2014-01-21 08:48:27 +0000

    Correct quote usage in parameters
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@5909 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- release/mac-specific/OmegaT.app/Contents/MacOS/OmegaT.sh
+++ release/mac-specific/OmegaT.app/Contents/MacOS/OmegaT.sh
@@ -5,10 +5,10 @@ cd `dirname $0`/Java
 #LANGUAGE="-Duser.language=en"
 #COUNTRY="-Duser.country=GB"
 # Settings to access the Internet behind a proxy
-#PROXY_HOST-Dhttp.proxyHost=192.168.1.1
-#PROXY_PORT=-Dhttp.proxyPort=3128
+#PROXY_HOST="-Dhttp.proxyHost=192.168.1.1"
+#PROXY_PORT="-Dhttp.proxyPort=3128"
 # Google Translate v2 API key
-#GOOGLE_API_KEY=""-Dgoogle.api.key=0123456789A0123456789B0123456789C0123456789D"
+#GOOGLE_API_KEY="-Dgoogle.api.key=0123456789A0123456789B0123456789C0123456789D"
 # Microsoft Translator credentials
 #MS_CLIENT_ID="-Dmicrosoft.api.client_id=xxxxx"
 #MS_CLIENT_SECRET="-Dmicrosoft.api.client_secret=xxxxx'

582ce960f57946a7eca0b583cf0036ae536bf45e
Didier Briel <d.briel@free.fr>
2014-02-24 10:54:44 +0000

    Merged revision(s) 6025 from trunk/release/mac-specific/OmegaT.app/Contents/MacOS/OmegaT.sh: Make it easier to use system Java 1.6 on OS X
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/branches/release-3-0@6032 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- release/mac-specific/OmegaT.app/Contents/MacOS/OmegaT.sh
+++ release/mac-specific/OmegaT.app/Contents/MacOS/OmegaT.sh
@@ -4,6 +4,11 @@ cd "$(dirname "$0")/Java"
 
 MACOS="-Xdock:name=OmegaT -Xdock:icon=../../Resources/OmegaT.icns"
 
+JAVA=../jre/bin/java
+
+# Un-comment the following line to use the system-supplied Java 1.6.
+#JAVA=$(/usr/libexec/java_home -v 1.6)/bin/java
+
 #LANGUAGE="-Duser.language=en"
 #COUNTRY="-Duser.country=GB"
 # Settings to access the Internet behind a proxy
@@ -15,4 +20,4 @@ MACOS="-Xdock:name=OmegaT -Xdock:icon=../../Resources/OmegaT.icns"
 #MS_CLIENT_ID="-Dmicrosoft.api.client_id=xxxxx"
 #MS_CLIENT_SECRET="-Dmicrosoft.api.client_secret=xxxxx'
 
-../jre/bin/java -Xmx1024m ${MACOS} ${LANGUAGE} ${COUNTRY} ${PROXY_HOST} ${PROXY_PORT} ${GOOGLE_API_KEY} ${MS_CLIENT_ID} ${MS_CLIENT_SECRET} -jar OmegaT.jar
+${JAVA} -Xmx1024m ${MACOS} ${LANGUAGE} ${COUNTRY} ${PROXY_HOST} ${PROXY_PORT} ${GOOGLE_API_KEY} ${MS_CLIENT_ID} ${MS_CLIENT_SECRET} -jar OmegaT.jar

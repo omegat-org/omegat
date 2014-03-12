12b7c64f6aee00952ace8878f2b8c24900e4ce81
Briac Pilpré <briacp@gmail.com>
2014-03-12 23:42:04 +0000

    - Added a GUI dialog for the search and replace script.
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@6097 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- scripts/search_replace.groovy
+++ scripts/search_replace.groovy
@@ -2,18 +2,50 @@
  *  Simple search and replace script
  *
  * @author  Didier Briel
- * @date    2012-02-17
- * @version 0.1
+ * @author  Briac Pilpre
+ * @date    2014-03-13
+ * @version 0.2
  */
+import groovy.swing.SwingBuilder
+import java.awt.BorderLayout as BL
 
-def search_string = "xxx"
-def replace_string = "yyy"
+def search_string  = ""
+def replace_string = ""
 
-def segment_count = 0
+new SwingBuilder().edt {
+  frame(title:"Search and Replace", size: [ 350, 200], show: true) {
+  borderLayout(vgap: 5)
 
-console.println("Do a search and replace.\n");
+  panel(constraints: BL.CENTER,
+    border: compoundBorder([emptyBorder(10), titledBorder('Simple search and replace')])) {
+      tableLayout {
+        tr {
+           td { label "Search: " }
+           td { textField search_string, columns: 20  }
+        }
+        tr {
+           td { label "Replace: " }
+           td { textField replace_string, columns: 20  }
+        }
+        tr {
+           td { label "" }
+           td {
+              button(text:'Search & Replace',
+                 actionPerformed: { doReplace(search_string, replace_string) },
+                 constraints:BL.SOUTH)
+           }
+        }
+      }
+    }
+  }
+}
 
-project.allEntries.each { ste ->
+def doReplace(search_string, replace_string) {
+  def segment_count = 0
+
+  console.println("Do a search and replace.\n");
+
+  project.allEntries.each { ste ->
     source = ste.getSrcText();
     target = project.getTranslationInfo(ste) ? project.getTranslationInfo(ste).translation : null;
     initial_target = target
@@ -24,11 +56,12 @@ project.allEntries.each { ste ->
     target = target.replaceAll(search_string, replace_string)
 
     if (initial_target != target) {
-    	segment_count++
-    	editor.gotoEntry(ste.entryNum())
-    	console.println(ste.entryNum() + "\t" + ste.srcText + "\t" + target )
-    	editor.replaceEditText(target)
+      segment_count++
+      editor.gotoEntry(ste.entryNum())
+      console.println(ste.entryNum() + "\t" + ste.srcText + "\t" + target )
+      editor.replaceEditText(target)
     }
-}
+  }
 
-console.println("Segments modified: " + segment_count);
+  console.println("Segments modified: " + segment_count);
+}

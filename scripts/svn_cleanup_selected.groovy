64c84614a080c7ce98d8db00dc588b5b4b841e7a
Kos Ivantsov <k_ivantsov@yahoo.com>
2014-06-06 16:32:48 +0000

    added en, ru and uk localizations to svn_cleanup_selected.groovy added more verbosity when the script is run in a non-team project
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@6302 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- scripts/svn_cleanup_selected.groovy
+++ scripts/svn_cleanup_selected.groovy
@@ -1,11 +1,10 @@
-/* :name=SVN - Cleanup :description=Perform SVN cleanup on any local SVN repository
- *
+/*
  * Perform SVN cleanup on any local SVN repository
  *
  * @author	Yu Tang
  * @author	Kos Ivatsov
- * @date	2014-01-17
- * @version	0.2
+ * @date	2014-06-04
+ * @version	0.3
  */
 
 import javax.swing.JFileChooser
@@ -13,18 +12,19 @@ import org.omegat.core.team.SVNRemoteRepository
 import org.tmatesoft.svn.core.wc.*
 
 def folder
-
+console.clear()
+console.println(res.getString("name")+"\n${"-"*15}")
 if (project.isProjectLoaded()) {
 	def prop = project.getProjectProperties()
 	folder = new File(prop.getProjectRoot())
 	}else{
 	JFileChooser fc = new JFileChooser(
-		dialogTitle: "Choose SVN repository to perform cleanup",
+		dialogTitle: res.getString("title"),
 		fileSelectionMode: JFileChooser.DIRECTORIES_ONLY, 
 		multiSelectionEnabled: false
 		)
 	if(fc.showOpenDialog() != JFileChooser.APPROVE_OPTION) {
-		console.println "Canceled"
+		console.println(res.getString("canceled"))
 		return
 		}
 	folder = new File(fc.getSelectedFile().toString())
@@ -33,6 +33,7 @@ if (project.isProjectLoaded()) {
 if (SVNRemoteRepository.isSVNDirectory(folder)) { 
 	def clientManager = SVNClientManager.newInstance()
 	clientManager.getWCClient().doCleanup(folder)
-	console.println("Cleanup done!!")
-	}
+	console.println(res.getString("done"))
+	}else
+	console.println(res.getString("notrepo"))
 return

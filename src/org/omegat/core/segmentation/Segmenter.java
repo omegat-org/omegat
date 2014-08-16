1bee71ed3519c80f047c22193c7eff0ed97cbd5a
Piotr Kulik <cienislaw@users.sourceforge.net>
2014-08-16 15:09:09 +0000

    Bug fix: gluing segmented parts was skipping leading and trailing spaces from source text.
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@6565 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/core/segmentation/Segmenter.java
+++ src/org/omegat/core/segmentation/Segmenter.java
@@ -284,6 +284,7 @@ public final class Segmenter {
             return "";
 
         StringBuffer res = new StringBuffer();
+        res.append(spaces.get(0));
         res.append(sentences.get(0));
 
         for (int i = 1; i < sentences.size(); i++) {
@@ -307,6 +308,7 @@ public final class Segmenter {
             res.append(sp);
             res.append(sentences.get(i));
         }
+        res.append(spaces.get(spaces.size() - 1));
         return res.toString();
     }
 

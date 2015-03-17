f5713579b11be44be0a6e0dde7cd7fe0498d151b
Aaron Madlon-Kay <aaron@madlon-kay.com>
2015-03-17 05:04:47 +0000

    Actually we really can't use Ctrl+Space on Mac
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@7070 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/editor/autocompleter/AutoCompleter.java
+++ src/org/omegat/gui/editor/autocompleter/AutoCompleter.java
@@ -127,7 +127,7 @@ public class AutoCompleter {
      */
     public boolean processKeys(KeyEvent e) {
         
-        if (!isVisible() && (StaticUtils.isKey(e, KeyEvent.VK_SPACE, KeyEvent.CTRL_MASK)
+        if (!isVisible() && ((!onMac && StaticUtils.isKey(e, KeyEvent.VK_SPACE, KeyEvent.CTRL_MASK))
                 || (onMac && StaticUtils.isKey(e, KeyEvent.VK_ESCAPE, 0)))) {
 
             if (!editor.isInActiveTranslation(editor.getCaretPosition())) {
@@ -169,7 +169,7 @@ public class AutoCompleter {
                 return true;
             }
             
-            if (StaticUtils.isKey(e, KeyEvent.VK_SPACE, KeyEvent.CTRL_MASK)
+            if ((!onMac && StaticUtils.isKey(e, KeyEvent.VK_SPACE, KeyEvent.CTRL_MASK))
                     || StaticUtils.isKey(e, GO_NEXT_KEY, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())) {
                 if (popup.isVisible()) {
                     selectNextView();

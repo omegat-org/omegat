46dc45e5cd4c2e3fe12f50806589c6a4b7e4b4da
Aaron Madlon-Kay <aaron@madlon-kay.com>
2015-04-07 02:52:13 +0000

    Improve LuceneSmartChineseTokenizer
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@7102 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/tokenizer/LuceneSmartChineseTokenizer.java
+++ src/org/omegat/tokenizer/LuceneSmartChineseTokenizer.java
@@ -4,7 +4,7 @@
           glossaries, and translation leveraging into updated projects.
  
  Copyright (C) 2009 Alex Buloichik (alex73mail@gmail.com)
-               2013 Aaron Madlon-Kay
+               2013, 2015 Aaron Madlon-Kay
                Home page: http://www.omegat.org/
                Support center: http://groups.yahoo.com/group/OmegaT/
 
@@ -30,6 +30,7 @@ import java.io.StringReader;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.cn.smart.SentenceTokenizer;
 import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
+import org.apache.lucene.analysis.cn.smart.WordTokenFilter;
 
 /**
  * @author Alex Buloichik (alex73mail@gmail.com)
@@ -42,13 +43,10 @@ public class LuceneSmartChineseTokenizer extends BaseTokenizer {
     protected TokenStream getTokenStream(final String strOrig,
             final boolean stemsAllowed, final boolean stopWordsAllowed) {
         if (stemsAllowed) {
-            SmartChineseAnalyzer analyzer = stopWordsAllowed ?
-                        new SmartChineseAnalyzer(getBehavior(), true) :
-                            new SmartChineseAnalyzer(getBehavior(), false);
+            SmartChineseAnalyzer analyzer = new SmartChineseAnalyzer(getBehavior(), stopWordsAllowed);
             return analyzer.tokenStream("", new StringReader(strOrig));
         } else {
-            return new SentenceTokenizer(
-                    new StringReader(strOrig.toLowerCase()));
+            return new WordTokenFilter(new SentenceTokenizer(new StringReader(strOrig.toLowerCase())));
         }
     }
 }

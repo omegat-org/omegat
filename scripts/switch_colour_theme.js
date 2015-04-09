81840d71a2d3c707f7f41e4d03d7e5c9ed265d49
Aaron Madlon-Kay <aaron@madlon-kay.com>
2015-04-09 02:44:05 +0000

    Add Trafficlight theme
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@7126 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- scripts/switch_colour_theme.js
+++ scripts/switch_colour_theme.js
@@ -32,7 +32,11 @@ var themes = {
         'COLOR_REPLACE': '#0000ff',
         'COLOR_LANGUAGE_TOOLS': '#0000ff',
         'COLOR_TRANSTIPS': '#0000ff',
-        'COLOR_SPELLCHECK': '#ff0000'
+        'COLOR_SPELLCHECK': '#ff0000',
+        'COLOR_MOD_INFO_FG': '__DEFAULT__',
+        'COLOR_ACTIVE_SOURCE': '__DEFAULT__',
+        'COLOR_MATCHES_INS_ACTIVE': '__DEFAULT__',
+        'COLOR_MATCHES_DEL_ACTIVE': '__DEFAULT__'
     },
     'Dark': {
         'COLOR_BACKGROUND': '#000000',
@@ -54,7 +58,37 @@ var themes = {
         'COLOR_REPLACE': '#3333ff',
         'COLOR_LANGUAGE_TOOLS': '#3333ff',
         'COLOR_TRANSTIPS': '#3333f',
-        'COLOR_SPELLCHECK': '#ff3333'
+        'COLOR_SPELLCHECK': '#ff3333',
+        'COLOR_MOD_INFO_FG': '__DEFAULT__',
+        'COLOR_ACTIVE_SOURCE': '__DEFAULT__',
+        'COLOR_MATCHES_INS_ACTIVE': '__DEFAULT__',
+        'COLOR_MATCHES_DEL_ACTIVE': '__DEFAULT__'
+    },
+    'Trafficlight': {
+        'COLOR_BACKGROUND': '#ffffff',
+        'COLOR_FOREGROUND': '#000000',
+        'COLOR_SOURCE': '#c0ffc0',
+        'COLOR_NOTED': '#c0ffff',
+        'COLOR_UNTRANSLATED': '#ffcccc',
+        'COLOR_TRANSLATED': '#e5e5e5',
+        'COLOR_NON_UNIQUE': '#000000',
+        'COLOR_PLACEHOLDER': '#969696',
+        'COLOR_REMOVETEXT_TARGET': '#ff0000',
+        'COLOR_NBSP': '#c8c8c8',
+        'COLOR_WHITESPACE': '#808080',
+        'COLOR_BIDIMARKERS': '#c80000',
+        'COLOR_MARK_COMES_FROM_TM': '#fa8072',
+        'COLOR_MARK_COMES_FROM_TM_XICE': '#af76df',
+        'COLOR_MARK_COMES_FROM_TM_X100PC': '#ff9408',
+        'COLOR_MARK_COMES_FROM_TM_XAUTO': '#ffd596',
+        'COLOR_REPLACE': '#0000ff',
+        'COLOR_LANGUAGE_TOOLS': '#0000ff',
+        'COLOR_TRANSTIPS': '#0000ff',
+        'COLOR_SPELLCHECK': '#ff0000',
+        'COLOR_MOD_INFO_FG': '#999999',
+        'COLOR_ACTIVE_SOURCE': '#ffff99',
+        'COLOR_MATCHES_INS_ACTIVE': '#00ff00',
+        'COLOR_MATCHES_DEL_ACTIVE': '#ff0000'
     }
 };
 

8dfdc9178b14e4eff284ce7a5d55e0acb6158268
Aaron Madlon-Kay <aaron@madlon-kay.com>
2015-05-12 06:35:52 +0000

    Update script for creating OS X icons
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@7249 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- images/make_osx_icon.sh
+++ images/make_osx_icon.sh
@@ -1,4 +1,35 @@
 #!/bin/sh
 
-iconutil --convert icns images/OmegaT.iconset
-cp images/OmegaT.icns release/mac-specific/OmegaT.app/Contents/Resources/
+set -e
+
+SCRIPT_ROOT="$(ruby -e "puts File.expand_path(\"$(dirname "$0")\")")"
+
+INKSCAPE="$(which inkscape)"
+PNGCRUSH="$(which pngcrush)"
+
+[ -z "$INKSCAPE" ] && echo "Make sure inkscape is on your path." && exit 1
+[ -z "$PNGCRUSH" ] && echo "Make sure pngcrush is on your path." && exit 1
+
+SVG_FILE="${SCRIPT_ROOT}/OmegaT.svg"
+[ ! -f "$SVG_FILE" ] && echo "Can't find SVG to convert." && exit 1
+
+SIZES="32 64 128 256"
+# 512px version is taken from website:
+# http://briac.net/omegat-logo/
+
+for SIZE in $SIZES; do
+    OUTFILE="${SCRIPT_ROOT}/OmegaT.iconset/icon_${SIZE}x${SIZE}.png"
+    inkscape --export-area-page \
+             --export-width=$SIZE \
+             --export-height=$SIZE \
+             --export-png="$OUTFILE" \
+             --file="$SVG_FILE"
+    pngcrush -ow "$OUTFILE"
+done
+
+# The system likes 64x64 to be named 32x32@2x for some reason.
+mv "${SCRIPT_ROOT}/OmegaT.iconset/icon_64x64.png" \
+   "${SCRIPT_ROOT}/OmegaT.iconset/icon_32x32@2x.png"
+
+iconutil --convert icns "${SCRIPT_ROOT}/OmegaT.iconset"
+cp "${SCRIPT_ROOT}/OmegaT.icns" "${SCRIPT_ROOT}/../release/mac-specific/OmegaT.app/Contents/Resources/"

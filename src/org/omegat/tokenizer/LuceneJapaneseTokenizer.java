a78ab5b9693325a0206e5543d148b65f0a8c068e
Aaron Madlon-Kay <aaron@madlon-kay.com>
2015-06-17 06:06:53 +0000

    Concatenate tags broken up by LuceneJapaneseTokenizer
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@7489 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/tokenizer/LuceneJapaneseTokenizer.java
+++ src/org/omegat/tokenizer/LuceneJapaneseTokenizer.java
@@ -24,16 +24,22 @@
  **************************************************************************/
 package org.omegat.tokenizer;
 
+import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
 import java.util.regex.Matcher;
 
 import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.TokenFilter;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.ja.JapaneseAnalyzer;
 import org.apache.lucene.analysis.ja.JapaneseTokenizer;
 import org.apache.lucene.analysis.ja.JapaneseTokenizer.Mode;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
 import org.omegat.util.PatternConsts;
 
 /**
@@ -59,7 +65,7 @@ public class LuceneJapaneseTokenizer extends BaseTokenizer {
             return new JapaneseAnalyzer(getBehavior(), null, Mode.SEARCH, stopWords, stopTags)
                     .tokenStream("", new StringReader(strOrig));
         } else {
-            return new JapaneseTokenizer(new StringReader(strOrig), null, false, Mode.NORMAL);
+            return new TagJoiningFilter(new JapaneseTokenizer(new StringReader(strOrig), null, false, Mode.NORMAL));
         }
     }
     
@@ -81,5 +87,145 @@ public class LuceneJapaneseTokenizer extends BaseTokenizer {
             }
         }
         return buffer.toString();
+    }
+    
+    /**
+     * This filter will reassemble OmegaT-style tags (&lt;x0>, etc.) that this tokenizer
+     * has broken apart. It is only meant to recognize "OmegaT-style tags". It has limited
+     * recovery capability when encountering false positives.
+     */
+    private static class TagJoiningFilter extends TokenFilter {
+
+        private static final int BUFFER_INITIAL_SIZE = 5;
+        
+        private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+        private final OffsetAttribute offsetAtt = addAttribute(OffsetAttribute.class);
+        
+        private StringBuilder buffer = new StringBuilder(BUFFER_INITIAL_SIZE);
+        
+        private int startOffset = -1;
+        
+        private boolean buffering = false;
+        private boolean doReplay = false;
+
+        private final ArrayDeque<CachedToken> stack = new ArrayDeque<CachedToken>();
+        
+        protected TagJoiningFilter(TokenStream input) {
+            super(input);
+        }
+
+        @Override
+        public boolean incrementToken() throws IOException {
+            if (doReplay) {
+                replayCachedToken();
+                doReplay = !stack.isEmpty();
+                return true;
+            }
+            while (input.incrementToken()) {
+                char[] chars = termAtt.buffer();
+                int len = termAtt.length();
+                if (buffering) {
+                    if (finishBuffering(chars, len)) {
+                        return true;
+                    }
+                    if (cancelBuffering(chars, len)) {
+                        return true;
+                    }
+                    cacheToken(chars, len);
+                    buffer.append(chars, 0, len);
+                    continue;
+                }
+                if (startBuffering(chars, len)) {
+                    continue;
+                }
+                return true;
+            }
+            return finishToken();
+        }
+        
+        private boolean startBuffering(char[] chars, int len) {
+            if (len > 2 || !isTagOpen(chars[0])) {
+                return false;
+            }
+            buffer.append(chars, 0, len);
+            startOffset = offsetAtt.startOffset();
+            cacheToken(chars, len);
+            buffering = true;
+            return true;
+        }
+        
+        private boolean isTagOpen(char c) {
+            return c == '<' || c == '{';
+        }
+        
+        private boolean cancelBuffering(char[] chars, int len) {
+            for (int i = 0; i < len; i++) {
+                if (!isTagContent(chars[i])) {
+                    cacheToken(chars, len);
+                    replayCachedToken();
+                    clearBuffer();
+                    doReplay = true;
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        private boolean isTagContent(char c) {
+            return c == '/' || Character.isLetterOrDigit(c);
+        }
+        
+        private void replayCachedToken() {
+            CachedToken t = stack.poll();
+            termAtt.copyBuffer(t.chars, 0, t.chars.length);
+            termAtt.setLength(t.chars.length);
+            offsetAtt.setOffset(t.startOffset, t.startOffset + t.chars.length);
+        }
+        
+        private boolean finishBuffering(char[] chars, int len) {
+            if (len > 2 || !isTagClose(chars[len - 1])) {
+                return false;
+            }
+            buffer.append(chars, 0, len);
+            return finishToken();
+        }
+        
+        private boolean isTagClose(char c) {
+            char open = buffer.charAt(0);
+            return (open == '<' && c == '>')
+                    || (open == '{' && c == '}');
+        }
+        
+        private boolean finishToken() {
+            if (buffer.length() == 0) {
+                return false;
+            }
+            String token = buffer.toString();
+            termAtt.copyBuffer(token.toCharArray(), 0, token.length());
+            termAtt.setLength(token.length());
+            offsetAtt.setOffset(startOffset, offsetAtt.endOffset());
+            clearBuffer();
+            stack.clear();
+            return true;
+        }
+        
+        private void clearBuffer() {
+            buffer = new StringBuilder(BUFFER_INITIAL_SIZE);
+            buffering = false;
+        }
+        
+        private void cacheToken(char[] chars, int len) {
+            stack.add(new CachedToken(Arrays.copyOf(chars, len), offsetAtt.startOffset()));
+        }
+        
+        private static class CachedToken {
+            public final char[] chars;
+            public final int startOffset;
+            
+            public CachedToken(char[] chars, int startOffset) {
+                this.chars = chars;
+                this.startOffset = startOffset;
+            }
+        }
     }
 }

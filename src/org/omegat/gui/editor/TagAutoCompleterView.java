abdfb43c0de96d95f82d6f9bd00b422702909a99
Aaron Madlon-Kay <aaron@madlon-kay.com>
2015-08-17 06:36:33 +0000

    Fix tag comparison in TagAutoCompleterView
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@7655 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/editor/TagAutoCompleterView.java
+++ src/org/omegat/gui/editor/TagAutoCompleterView.java
@@ -40,6 +40,7 @@ import org.omegat.gui.editor.autocompleter.AutoCompleterItem;
 import org.omegat.gui.editor.autocompleter.AutoCompleterListView;
 import org.omegat.util.OStrings;
 import org.omegat.util.TagUtil;
+import org.omegat.util.TagUtil.Tag;
 import org.omegat.util.Token;
 
 /**
@@ -62,8 +63,11 @@ public class TagAutoCompleterView extends AutoCompleterListView {
         List<String> missingGroups = TagUtil.getGroupedMissingTagsFromTarget();
         
         // If wordChunk is a tag, pretend we have a blank wordChunk.
-        if (TagUtil.getAllTagsInSource().contains(wordChunk)) {
-            wordChunk = "";
+        for (Tag tag : TagUtil.getAllTagsInSource()) {
+            if (tag.tag.equals(wordChunk)) {
+                wordChunk = "";
+                break;
+            }
         }
 
         List<String> matchGroups = new ArrayList<String>();

be920cd9f724bc876741e8e0e138585f56f2e048
Aaron Madlon-Kay <aaron@madlon-kay.com>
2015-09-30 13:55:29 +0000

    Add tokenization tests for Turkish and Chinese
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@7762 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- test/src/org/omegat/tokenizer/TokenizerTest.java
+++ test/src/org/omegat/tokenizer/TokenizerTest.java
@@ -96,6 +96,84 @@ public class TokenizerTest extends TestCase {
         assertResult(new String[] { "\u3042", "foo", "bar" },
                 tok.tokenizeWordsToStrings(orig, StemmingMode.MATCHING));
     }
+    
+    /**
+     * Turkish warrants special testing because it has the letter \u0130
+     * (LATIN CAPITAL LETTER I WITH DOT ABOVE); the result (both content
+     * and length) of performing <code>"\u0130".toLowerCase()</code> depends
+     * on the default Locale, and in the past there were issues with improper
+     * lowercasing during tokenization leading to OOB exceptions.
+     * <p>
+     * Text from https://tr.wikipedia.org/wiki/T%C3%BCrk%C3%A7e
+     */
+    public void testTurkish() {
+        ITokenizer tok = new LuceneTurkishTokenizer();
+        String orig = "\u201C\u0130stanbul a\u011Fz\u0131\u201D, T\u00FCrkiye T\u00FCrk\u00E7esi"
+                + "yaz\u0131 dilinin kayna\u011F\u0131 olarak kabul edilir; yaz\u0131 dili bu"
+                + "a\u011F\u0131z temelinde olu\u015Fmu\u015Ftur.";
+        assertVerbatim(new String[] { "\u201C", "\u0130stanbul", " ", "a\u011Fz\u0131", "\u201D",
+                ",", " ", "T\u00FCrkiye", " ", "T\u00FCrk\u00E7esiyaz\u0131", " ", "dilinin", " ",
+                "kayna\u011F\u0131", " ", "olarak", " ", "kabul", " ", "edilir", ";", " ", "yaz\u0131",
+                " ", "dili", " ", "bua\u011F\u0131z", " ", "temelinde", " ", "olu\u015Fmu\u015Ftur", "." },
+                tok.tokenizeVerbatimToStrings(orig),
+                tok.tokenizeVerbatim(orig),
+                orig);
+        assertResult(new String[] { "\u0130stanbul", "a\u011Fz\u0131", "T\u00FCrkiye",
+                "T\u00FCrk\u00E7esiyaz\u0131", "dilinin", "kayna\u011F\u0131", "olarak",
+                "kabul", "edilir", "yaz\u0131", "dili", "bua\u011F\u0131z", "temelinde",
+                "olu\u015Fmu\u015Ftur" },
+                tok.tokenizeWordsToStrings(orig, StemmingMode.NONE));
+        assertResult(new String[] { "istanbul", "a\u011Fz\u0131", "t\u00FCrki", "T\u00FCrkiye",
+                "t\u00FCrk\u00E7esiyaz", "T\u00FCrk\u00E7esiyaz\u0131", "dil", "dilinin",
+                "kaynak", "kayna\u011F\u0131", "olarak", "kabul", "edilir", "yaz", "yaz\u0131",
+                "dil", "dili", "buak", "bua\u011F\u0131z", "temel", "temelinde", "olu\u015F",
+                "olu\u015Fmu\u015Ftur" },
+                tok.tokenizeWordsToStrings(orig, StemmingMode.GLOSSARY));
+        assertResult(new String[] { "istanbul", "a\u011Fz\u0131", "t\u00FCrki", "T\u00FCrkiye",
+                "t\u00FCrk\u00E7esiyaz", "T\u00FCrk\u00E7esiyaz\u0131", "dil", "dilinin",
+                "kaynak", "kayna\u011F\u0131", "kabul", "edilir", "yaz", "yaz\u0131",
+                "dil", "dili", "buak", "bua\u011F\u0131z", "temel", "temelinde", "olu\u015F",
+                "olu\u015Fmu\u015Ftur" },
+                tok.tokenizeWordsToStrings(orig, StemmingMode.MATCHING));
+    }
+    
+    /**
+     * Chinese tends to have very few character boundaries breakable by BreakIterator,
+     * so LuceneSmartChineseTokenizer tokenizes by code point for verbatim tokenizing.
+     * <p>
+     * Text from https://zh.wikipedia.org/wiki/%E6%B1%89%E8%AF%AD
+     */
+    public void testChinese() {
+        ITokenizer tok = new LuceneSmartChineseTokenizer();
+        String orig = "\u6F22\u8A9E\u7684\u6587\u5B57\u7CFB\u7D71\u2014\u2014\u6F22\u5B57\u662F"
+                + "\u4E00\u7A2E\u610F\u97F3\u8A9E\u8A00\uFF0C\u8868\u610F\u7684\u540C\u6642\u4E5F"
+                + "\u5177\u4E00\u5B9A\u7684\u8868\u97F3\u529F\u80FD\u3002";
+        assertVerbatim(new String[] { "\u6F22", "\u8A9E", "\u7684", "\u6587", "\u5B57", "\u7CFB",
+                "\u7D71", "\u2014", "\u2014", "\u6F22", "\u5B57", "\u662F", "\u4E00", "\u7A2E",
+                "\u610F", "\u97F3", "\u8A9E", "\u8A00", "\uFF0C", "\u8868", "\u610F", "\u7684",
+                "\u540C", "\u6642", "\u4E5F", "\u5177", "\u4E00", "\u5B9A", "\u7684", "\u8868",
+                "\u97F3", "\u529F", "\u80FD", "\u3002" },
+                tok.tokenizeVerbatimToStrings(orig),
+                tok.tokenizeVerbatim(orig),
+                orig);
+        assertResult(new String[] { "\u6F22", "\u8A9E", "\u7684", "\u6587\u5B57", "\u7CFB",
+                "\u7D71", ",", ",", "\u6F22", "\u5B57", "\u662F", "\u4E00", "\u7A2E",
+                "\u610F", "\u97F3", "\u8A9E", "\u8A00", ",", "\u8868\u610F", "\u7684",
+                "\u540C", "\u6642", "\u4E5F", "\u5177", "\u4E00\u5B9A", "\u7684", "\u8868\u97F3",
+                "\u529F\u80FD", "," },
+                tok.tokenizeWordsToStrings(orig, StemmingMode.NONE));
+        assertResult(new String[] { "\u6F22", "\u8A9E", "\u7684", "\u6587\u5B57", "\u7CFB",
+                "\u7D71", ",", "\u2014", ",", "\u2014", "\u6F22", "\u5B57", "\u662F", "\u4E00",
+                "\u7A2E", "\u610F", "\u97F3", "\u8A9E", "\u8A00", ",", "\uFF0C", "\u8868\u610F",
+                "\u7684", "\u540C", "\u6642", "\u4E5F", "\u5177", "\u4E00\u5B9A", "\u7684",
+                "\u8868\u97F3", "\u529F\u80FD", ",", "\u3002" },
+                tok.tokenizeWordsToStrings(orig, StemmingMode.GLOSSARY));
+        assertResult(new String[] { "\u6F22", "\u8A9E", "\u7684", "\u6587\u5B57", "\u7CFB",
+                "\u7D71", "\u6F22", "\u5B57", "\u662F", "\u4E00", "\u7A2E", "\u610F", "\u97F3",
+                "\u8A9E", "\u8A00", "\u8868\u610F", "\u7684", "\u540C", "\u6642", "\u4E5F",
+                "\u5177", "\u4E00\u5B9A", "\u7684", "\u8868\u97F3", "\u529F\u80FD" },
+                tok.tokenizeWordsToStrings(orig, StemmingMode.MATCHING));
+    }
 
     private void assertVerbatim(String[] expected, String[] test, Token[] testTok, String origString) {
         assertResult(expected, test);

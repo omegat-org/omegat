ee2f64aa16c4235103129dfe2c9f05b6796a7d06
Piotr Kulik <cienislaw@users.sourceforge.net>
2015-10-03 09:48:22 +0000

    Updated date and version for check_rules.groovy
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@7780 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- scripts/check_rules.groovy
+++ scripts/check_rules.groovy
@@ -5,8 +5,8 @@
  * @author  Piotr Kulik
  * @author  Kos Ivantsov
  * @author  Didier Briel
- * @date    2014-06-05
- * @version 0.6
+ * @date    2015-10-03
+ * @version 0.7
  */
 
 // if FALSE only current file will be checked

5450a303a0cd1fc468987b09e6df9b027b5c3508
Vincent Bidaux <vbidaux@diptik.fr>
2016-01-25 17:37:20 +0000

    POC publications in their temporary pub folder
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@8165 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ docs2/en/js/modcodes.js
@@ -0,0 +1,3 @@
+
+      var modcodes = new Object();
+      modcodes['index.html']='Généralités';modcodes['NewProject.html']='[en]Nouveau...';modcodes['OpenProject.html']='[en]Ouvrir...';modcodes['CommentsPane.html']='[en]Volet Commentaires';modcodes['DictionaryPane.html']='[en]Volet Dictionnaires';modcodes['EditorPane.html']='[en]Volet Editeur';modcodes['NotesPane.html']='[en]Volet Notes';

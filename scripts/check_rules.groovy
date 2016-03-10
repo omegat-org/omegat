707529b126f7eea4ae622ccf5740d881d10536dd
Kos Ivantsov <k_ivantsov@yahoo.com>
2016-03-10 14:51:58 +0000

    Updated QA script version and date.
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@8275 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- scripts/check_rules.groovy
+++ scripts/check_rules.groovy
@@ -5,8 +5,8 @@
  * @author  Piotr Kulik
  * @author  Kos Ivantsov
  * @author  Didier Briel
- * @date    2015-10-03
- * @version 0.7
+ * @date    2016-03-10
+ * @version 0.7.1
  */
 
 // if FALSE only current file will be checked

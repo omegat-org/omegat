58ee5ff8e3378da3a4ff0685f30865f273b99781
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-03-11 07:44:43 +0000

    Add prefs loading, storing test
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@8357 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- test/src/org/omegat/util/PreferencesTest.java
+++ test/src/org/omegat/util/PreferencesTest.java
@@ -3,7 +3,7 @@
           with fuzzy matching, translation memory, keyword search, 
           glossaries, and translation leveraging into updated projects.
 
- Copyright (C) 2015 Aaron Madlon-Kay
+ Copyright (C) 2015-2016 Aaron Madlon-Kay
                Home page: http://www.omegat.org/
                Support center: http://groups.yahoo.com/group/OmegaT/
 
@@ -29,6 +29,7 @@ import java.io.File;
 import java.io.PrintWriter;
 
 import org.omegat.filters.TestFilterBase;
+import org.omegat.util.Preferences.IPreferences;
 
 import junit.framework.TestCase;
 
@@ -37,51 +38,141 @@ import junit.framework.TestCase;
  */
 public class PreferencesTest extends TestCase {
 
+    private File tmpDir;
+
+    @Override
+    protected void setUp() throws Exception {
+        tmpDir = FileUtil.createTempDir();
+        assertTrue(tmpDir.isDirectory());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        assertTrue(FileUtil.deleteTree(tmpDir));
+    }
+
     /**
      * Test that if an error is encountered when loading the
      * preferences file, the original file is backed up.
      */
     public void testPreferencesBackup() throws Exception {
-        File tmpDir = FileUtil.createTempDir();
-        try {
-            assertTrue(tmpDir.isDirectory());
-            
-            // Initialize the log first because if we don't then the log
-            // file will be put under our temp config dir, and on Windows
-            // the log file will be locked, so when we try to delete the
-            // temp dir at the end of the test it will fail.
-            Log.log("Dummy log line");
-            
-            File prefsFile = new File(tmpDir, Preferences.FILE_PREFERENCES);
-            
-            // Write anything that is malformed XML, to force a parsing error.
-            PrintWriter out = new PrintWriter(prefsFile, "UTF-8");
-            out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
-            out.println("<omegat>");
-            out.println("<preference version=\"1.0\">");
-            out.close();
-            assertFalse(out.checkError());
-            
-            // Load bad prefs file.
-            new PreferencesImpl(new PreferencesXML(prefsFile, null));
-            
-            // The actual backup file will have a timestamp in the filename,
-            // so we have to loop through looking for it.
-            File backup = null;
-            for (File f : tmpDir.listFiles()) {
-                String name = f.getName();
-                if (name.startsWith("omegat.prefs") && name.endsWith(".bak")) {
-                    backup = f;
-                    break;
-                }
+        File prefsFile = new File(tmpDir, Preferences.FILE_PREFERENCES);
+
+        // Write anything that is malformed XML, to force a parsing error.
+        PrintWriter out = new PrintWriter(prefsFile, "UTF-8");
+        out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
+        out.println("<omegat>");
+        out.println("<preference version=\"1.0\">");
+        out.close();
+        assertFalse(out.checkError());
+
+        // Load bad prefs file.
+        new PreferencesImpl(new PreferencesXML(prefsFile, null));
+
+        // The actual backup file will have a timestamp in the filename,
+        // so we have to loop through looking for it.
+        File backup = null;
+        for (File f : tmpDir.listFiles()) {
+            String name = f.getName();
+            if (name.startsWith("omegat.prefs") && name.endsWith(".bak")) {
+                backup = f;
+                break;
             }
-            
-            assertNotNull(backup);
-            assertTrue(backup.isFile());
-            
-            TestFilterBase.compareBinary(prefsFile, backup);
-        } finally {
-            assertTrue(FileUtil.deleteTree(tmpDir));
         }
+
+        assertNotNull(backup);
+        assertTrue(backup.isFile());
+
+        TestFilterBase.compareBinary(prefsFile, backup);
+    }
+
+    enum TestEnum {
+        BAR, BAZ, BAZINGA;
+
+        public String toString() {
+            if (this == BAZINGA) {
+                return "!";
+            } else {
+                return super.toString();
+            }
+        };
+    }
+
+    public void testPreferencesLoadStore() throws Exception {
+        File prefsFile = new File(tmpDir, Preferences.FILE_PREFERENCES);
+
+        IPreferences prefs = new PreferencesImpl(new PreferencesXML(null, prefsFile));
+
+        // Set values normally
+        assertEquals(null, prefs.setPreference("MyString", "foo"));
+        assertEquals(null, prefs.setPreference("MyBoolean", true));
+        assertEquals(null, prefs.setPreference("MyInt", 5));
+        assertEquals(null, prefs.setPreference("MyEnum", TestEnum.BAR));
+        assertEquals(null, prefs.setPreference("MyEmptyString", ""));
+
+        // Check values normally
+        assertEquals("foo", prefs.getPreference("MyString"));
+        assertEquals(true, prefs.isPreference("MyBoolean"));
+        assertEquals(TestEnum.BAR, TestEnum.valueOf(prefs.getPreference("MyEnum")));
+        assertEquals(5, Integer.parseInt(prefs.getPreference("MyInt")));
+        // Check empty string behavior
+        assertEquals("", prefs.getPreference("MyEmptyString"));
+        assertTrue(prefs.existsPreference("MyEmptyString"));
+        assertEquals("", prefs.getPreference("MyNonexistentString"));
+        assertFalse(prefs.existsPreference("MyNonexistentString"));
+        // Check that defaults are not used for already-set values
+        assertEquals("foo", prefs.getPreferenceDefault("MyString", "bar"));
+        assertEquals(true, prefs.isPreferenceDefault("MyBoolean", false));
+        assertEquals(5, prefs.getPreferenceDefault("MyInt", 0));
+        assertEquals(TestEnum.BAR, prefs.getPreferenceEnumDefault("MyEnum", TestEnum.BAZ));
+
+        // Persist prefs
+        prefs.save();
+
+        // Load prefs from persisted version
+        prefs = new PreferencesImpl(new PreferencesXML(prefsFile, null));
+
+        // Check values
+        assertEquals("foo", prefs.getPreference("MyString"));
+        assertEquals(true, prefs.isPreference("MyBoolean"));
+        assertEquals(5, Integer.parseInt(prefs.getPreference("MyInt")));
+        assertEquals(TestEnum.BAR, TestEnum.valueOf(prefs.getPreference("MyEnum")));
+        assertEquals("", prefs.getPreference("MyEmptyString"));
+        assertTrue(prefs.existsPreference("MyEmptyString"));
+        assertEquals("", prefs.getPreference("MyNonexistentString"));
+        assertFalse(prefs.existsPreference("MyNonexistentString"));
+        assertEquals("foo", prefs.getPreferenceDefault("MyString", "bar"));
+        assertEquals(true, prefs.isPreferenceDefault("MyBoolean", false));
+        assertEquals(5, prefs.getPreferenceDefault("MyInt", 0));
+        assertEquals(TestEnum.BAR, prefs.getPreferenceEnumDefault("MyEnum", TestEnum.BAZ));
+
+        // Check setting values via *Default methods
+        assertEquals("bar", prefs.getPreferenceDefault("MyStringDefault", "bar"));
+        assertEquals("bar", prefs.getPreference("MyStringDefault"));
+        assertEquals(false, prefs.isPreferenceDefault("MyBoolDefault", false));
+        assertEquals(false, prefs.isPreference("MyBoolDefault"));
+        assertEquals(77, prefs.getPreferenceDefault("MyIntDefault", 77));
+        assertEquals(77, Integer.parseInt(prefs.getPreference("MyIntDefault")));
+        assertEquals(TestEnum.BAZ, prefs.getPreferenceEnumDefault("MyEnumDefault", TestEnum.BAZ));
+        assertEquals(TestEnum.BAZ, TestEnum.valueOf(prefs.getPreference("MyEnumDefault")));
+
+        // Check writing new value
+        assertEquals("bar", prefs.setPreference("MyStringDefault", "baz"));
+
+        // Check bad enum
+        try {
+            prefs.setPreference("Bad!", TestEnum.BAZINGA);
+            fail();
+        } catch (IllegalArgumentException ex) {
+            // Can't store enum where toString() gives a different result than
+            // name()
+        }
+
+        // Check null behvaior
+        assertEquals(null, prefs.setPreference(null, null));
+        assertEquals(null, prefs.setPreference("", null));
+        assertEquals(null, prefs.setPreference("blah", null));
+        assertEquals("", prefs.getPreference(null));
+        assertEquals("", prefs.getPreference(""));
     }
 }

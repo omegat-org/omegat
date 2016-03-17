50701eed7534c4a24851ea45a67a0f43968cf71f
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-03-17 08:46:24 +0000

    Reduce excessive indentation
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@8452 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/main/ProjectUICommands.java
+++ src/org/omegat/gui/main/ProjectUICommands.java
@@ -311,43 +311,43 @@ public class ProjectUICommands {
                     return null;
                 }
 
-                    try {
-                        boolean needToSaveProperties = false;
-                        if (props.hasRepositories()) {
-                            // team project - non-exist directories could be created from repo
-                            props.autocreateDirectories();
-                        } else {
-                            // not a team project - ask for non-exist directories
-                            while (!props.isProjectValid()) {
-                                needToSaveProperties = true;
-                                // something wrong with the project - display open dialog
-                                // to fix it
-                                ProjectPropertiesDialog prj = new ProjectPropertiesDialog(props, new File(
-                                        projectRootFolder, OConsts.FILE_PROJECT).getAbsolutePath(),
-                                        ProjectPropertiesDialog.Mode.RESOLVE_DIRS);
-                                prj.setVisible(true);
-                                props = prj.getResult();
-                                prj.dispose();
-                                if (props == null) {
-                                    // user clicks on 'Cancel'
-                                    mainWindow.setCursor(oldCursor);
-                                    return null;
-                                }
+                try {
+                    boolean needToSaveProperties = false;
+                    if (props.hasRepositories()) {
+                        // team project - non-exist directories could be created from repo
+                        props.autocreateDirectories();
+                    } else {
+                        // not a team project - ask for non-exist directories
+                        while (!props.isProjectValid()) {
+                            needToSaveProperties = true;
+                            // something wrong with the project - display open dialog
+                            // to fix it
+                            ProjectPropertiesDialog prj = new ProjectPropertiesDialog(props,
+                                    new File(projectRootFolder, OConsts.FILE_PROJECT).getAbsolutePath(),
+                                    ProjectPropertiesDialog.Mode.RESOLVE_DIRS);
+                            prj.setVisible(true);
+                            props = prj.getResult();
+                            prj.dispose();
+                            if (props == null) {
+                                // user clicks on 'Cancel'
+                                mainWindow.setCursor(oldCursor);
+                                return null;
                             }
                         }
-
-                        ProjectFactory.loadProject(props, true);
-                        if (needToSaveProperties) {
-                            Core.getProject().saveProjectProperties();
-                        }
-                    } catch (Exception ex) {
-                        Log.logErrorRB(ex, "PP_ERROR_UNABLE_TO_READ_PROJECT_FILE");
-                        Core.getMainWindow().displayErrorRB(ex, "PP_ERROR_UNABLE_TO_READ_PROJECT_FILE");
-                        mainWindow.setCursor(oldCursor);
-                        return null;
                     }
 
-				RecentProjects.add(projectRootFolder.getAbsolutePath());
+                    ProjectFactory.loadProject(props, true);
+                    if (needToSaveProperties) {
+                        Core.getProject().saveProjectProperties();
+                    }
+                } catch (Exception ex) {
+                    Log.logErrorRB(ex, "PP_ERROR_UNABLE_TO_READ_PROJECT_FILE");
+                    Core.getMainWindow().displayErrorRB(ex, "PP_ERROR_UNABLE_TO_READ_PROJECT_FILE");
+                    mainWindow.setCursor(oldCursor);
+                    return null;
+                }
+
+                RecentProjects.add(projectRootFolder.getAbsolutePath());
 
                 mainWindow.setCursor(oldCursor);
                 return null;

bedffb413561655439b87f43e0e1c8c5d991a613
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-03-21 14:21:27 +0000

    Add PdfFilter unit test
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@8506 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ test/src/org/omegat/filters/PdfFilterTest.java
@@ -0,0 +1,54 @@
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool 
+          with fuzzy matching, translation memory, keyword search, 
+          glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2016 Aaron Madlon-Kay
+               Home page: http://www.omegat.org/
+               Support center: http://groups.yahoo.com/group/OmegaT/
+
+ This file is part of OmegaT.
+
+ OmegaT is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OmegaT is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **************************************************************************/
+package org.omegat.filters;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import org.omegat.core.data.IProject;
+import org.omegat.filters2.pdf.PdfFilter;
+
+public class PdfFilterTest extends TestFilterBase {
+    public void testParse() throws Exception {
+        List<ParsedEntry> lines = parse3(new PdfFilter(),
+                "test/data/filters/pdf/file-PdfFilter.pdf", null);
+        assertEquals("This is some text. This is also some text. ", lines.get(0).source);
+    }
+
+    public void testTranslate() throws Exception {
+        translate(new PdfFilter(), "test/data/filters/pdf/file-PdfFilter.pdf", Collections.emptyMap());
+        compareBinary(new File("test/data/filters/pdf/file-PdfFilter-gold.txt"), outFile);
+    }
+
+    public void testLoad() throws Exception {
+        String f = "test/data/filters/pdf/file-PdfFilter.pdf";
+        IProject.FileInfo fi = loadSourceFiles(new PdfFilter(), f);
+
+        checkMultiStart(fi, f);
+        checkMulti("This is some text. This is also some text.", null, null, null, null, null);
+        checkMultiEnd();
+    }
+}

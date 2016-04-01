9a0f2f11f37523d3028eb433c90f146ef0ab1043
Piotr Kulik <cienislaw@users.sourceforge.net>
2016-04-01 18:09:48 +0000

    Added Lucene Polish Tokenizer
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@8607 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ src/org/omegat/tokenizer/LucenePolishTokenizer.java
@@ -0,0 +1,55 @@
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool 
+          with fuzzy matching, translation memory, keyword search, 
+          glossaries, and translation leveraging into updated projects.
+ 
+ Copyright (C) 2009 Alex Buloichik (alex73mail@gmail.com)
+               2013 Aaron Madlon-Kay
+	       2016 Piotr Kulik
+               Home page: http://www.omegat.org/
+               Support center: http://groups.yahoo.com/group/OmegaT/
+
+ This file is part of OmegaT.
+
+ OmegaT is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OmegaT is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **************************************************************************/
+package org.omegat.tokenizer;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.pl.PolishAnalyzer;
+import org.apache.lucene.analysis.util.CharArraySet;
+
+/**
+ * @author Alex Buloichik (alex73mail@gmail.com)
+ * @author Aaron Madlon-Kay
+ */
+@Tokenizer(languages = { "pl" })
+public class LucenePolishTokenizer extends BaseTokenizer {
+
+    @SuppressWarnings("resource")
+    @Override
+    protected TokenStream getTokenStream(final String strOrig, final boolean stemsAllowed,
+            final boolean stopWordsAllowed) throws IOException {
+        if (stemsAllowed) {
+            CharArraySet stopWords = stopWordsAllowed ? PolishAnalyzer.getDefaultStopSet() : CharArraySet.EMPTY_SET;
+            PolishAnalyzer analyzer = new PolishAnalyzer(stopWords);
+            return analyzer.tokenStream("", new StringReader(strOrig));
+        } else {
+            return super.getStandardTokenStream(strOrig);
+        }
+    }
+}

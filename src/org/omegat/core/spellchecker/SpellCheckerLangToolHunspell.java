d3465fc1adfeb550aedf391f5e7aacb6841f7bad
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-04-09 08:10:06 +0000

    Fix spelling dictionary loading from non-ASCII paths
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@8708 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/core/spellchecker/SpellCheckerLangToolHunspell.java
+++ src/org/omegat/core/spellchecker/SpellCheckerLangToolHunspell.java
@@ -27,6 +27,7 @@ package org.omegat.core.spellchecker;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.List;
 
@@ -46,7 +47,29 @@ public class SpellCheckerLangToolHunspell implements ISpellCheckerProvider {
 
     public SpellCheckerLangToolHunspell(String dictBasename) throws Throwable {
         this.dictBasename = dictBasename;
+
+        // Here we are going to obtain the dictionary by ultimately calling
+        // Hunspell_create(String, String).
+        //
+        // When we do so, the Java strings for the dictionary file paths will be
+        // encoded according to JNA's Native.getDefaultEncoding(), which is
+        // either the property jna.encoding (if set) or UTF-8. To handle
+        // non-ASCII paths on Windows we need this encoding to be the system's
+        // native encoding (Cp1252, etc.) and the only way to set this appears
+        // to be globally via jna.encoding.
+        //
+        // We use other libraries that use JNA under the hood, and it's not
+        // clear that setting jna.encoding won't have some sort of adverse
+        // effect on them. Hunspell only takes Java strings for this call, so
+        // we set the encoding, get our dictionary, then set it back and hope
+        // that nothing bad happens.
+        //
+        // Discussion here:
+        // https://groups.google.com/d/msg/jna-users/FdcRn_F6Qts/aG-o8AZkBgAJ
+        String origEncoding = System.getProperty("jna.encoding");
+        System.setProperty("jna.encoding", Charset.defaultCharset().name());
         this.dict = Hunspell.getInstance().getDictionary(dictBasename);
+        System.setProperty("jna.encoding", origEncoding);
     }
 
     @Override

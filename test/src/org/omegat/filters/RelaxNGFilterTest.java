cd2d06dc3f613e96f33186bb248d43a19660debf
Tony Graham <tgraham@antenna.co.jp>
2016-10-26 12:25:26 +0100

    Updated copyright.

--- test/src/org/omegat/filters/RelaxNGFilterTest.java
+++ test/src/org/omegat/filters/RelaxNGFilterTest.java
@@ -4,6 +4,7 @@
           glossaries, and translation leveraging into updated projects.
 
  Copyright (C) 2008 Alex Buloichik
+               2016 Tony Graham
                Home page: http://www.omegat.org/
                Support center: http://groups.yahoo.com/group/OmegaT/
 

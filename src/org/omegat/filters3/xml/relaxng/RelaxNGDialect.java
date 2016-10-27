a8cd00d7d40f9d2a63dd3d5f5b62950a14ac3b1e
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-10-27 01:55:02 +0000

    Add file filter for RELAX NG schema for XML (*.rng)
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@9353 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ src/org/omegat/filters3/xml/relaxng/RelaxNGDialect.java
@@ -0,0 +1,57 @@
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool 
+          with fuzzy matching, translation memory, keyword search, 
+          glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2000-2006 Keith Godfrey and Maxym Mykhalchuk
+               2009 Didier Briel, Guido Leenders
+               2012 Guido Leenders
+               2015 Tony Graham
+ 
+               Home page: http://www.omegat.org/
+               Support center: http://groups.yahoo.com/group/OmegaT/
+
+ This file is part of OmegaT.
+
+ OmegaT is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OmegaT is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **************************************************************************/
+
+package org.omegat.filters3.xml.relaxng;
+
+import java.util.regex.Pattern;
+import java.util.HashMap;
+
+import org.omegat.filters3.xml.DefaultXMLDialect;
+
+/**
+ * This class specifies the RELAX NG XML Dialect.
+ *
+ * RELAX NG is a schema language for XML.  See http://relaxng.org/
+ * 
+ * @author Tony Graham
+ */
+public class RelaxNGDialect extends DefaultXMLDialect {
+    public static final Pattern RELAXNG_ROOT_TAG = Pattern.compile("grammar");
+    public static final Pattern RELAXNG_XMLNS = Pattern.compile("xmlns(:\\w+)?=\"http://relaxng.org/ns/structure/1.0\"");
+
+    public RelaxNGDialect() {
+        defineConstraint(CONSTRAINT_ROOT, RELAXNG_ROOT_TAG);
+        defineConstraint(CONSTRAINT_XMLNS, RELAXNG_XMLNS);
+
+        defineParagraphTags(new String[] { "documentation", "a:documentation", });
+
+	defineIntactTags(new String[] { "value", "name", "nsName", });
+    }
+
+}

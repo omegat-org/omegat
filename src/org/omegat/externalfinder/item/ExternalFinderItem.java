821e8f40894542ae38f71a18766fcae2dfa304d0
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-01-04 02:59:59 +0000

    Replace ExternalFinder license header with OmegaT header
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@9475 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/externalfinder/item/ExternalFinderItem.java
+++ src/org/omegat/externalfinder/item/ExternalFinderItem.java
@@ -1,19 +1,28 @@
-/*
- Copyright (C) 2016 Chihiro Hio
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool 
+          with fuzzy matching, translation memory, keyword search, 
+          glossaries, and translation leveraging into updated projects.
 
- This program is free software: you can redistribute it and/or modify
+ Copyright (C) 2016 Chihiro Hio
+               Home page: http://www.omegat.org/
+               Support center: http://groups.yahoo.com/group/OmegaT/
+
+ This file is part of OmegaT.
+
+ OmegaT is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
+ OmegaT is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ **************************************************************************/
+
 package org.omegat.externalfinder.item;
 
 import java.io.UnsupportedEncodingException;
--- src/org/omegat/externalfinder/item/ExternalFinderXMLItemLoader.java
+++ src/org/omegat/externalfinder/item/ExternalFinderXMLItemLoader.java
@@ -1,19 +1,28 @@
-/*
- Copyright (C) 2016 Chihiro Hio
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool 
+          with fuzzy matching, translation memory, keyword search, 
+          glossaries, and translation leveraging into updated projects.
 
- This program is free software: you can redistribute it and/or modify
+ Copyright (C) 2016 Chihiro Hio
+               Home page: http://www.omegat.org/
+               Support center: http://groups.yahoo.com/group/OmegaT/
+
+ This file is part of OmegaT.
+
+ OmegaT is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
+ OmegaT is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ **************************************************************************/
+
 package org.omegat.externalfinder.item;
 
 import java.io.File;
@@ -22,10 +31,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.swing.KeyStroke;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;

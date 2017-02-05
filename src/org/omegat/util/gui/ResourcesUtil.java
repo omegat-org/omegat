972b21efc56e5a3022835572d6da7103a7ebaf67
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-02-05 13:09:29 +0000

    Use less magical method for loading window icons
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@9573 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/util/gui/ResourcesUtil.java
+++ src/org/omegat/util/gui/ResourcesUtil.java
@@ -45,8 +45,8 @@ public class ResourcesUtil {
      * @see <a href="http://iconhandbook.co.uk/reference/chart/">Icon Reference
      *      Chart</a>
      */
-    public static final Image APP_ICON_32X32 = getImage(RESOURCES + "OmegaT.gif");
-    public static final Image APP_ICON_16X16 = getImage(RESOURCES + "OmegaT_small.gif");
+    public static final Image APP_ICON_32X32 = getBundledImage("OmegaT.gif");
+    public static final Image APP_ICON_16X16 = getBundledImage("OmegaT_small.gif");
 
     /**
      * Load icon.

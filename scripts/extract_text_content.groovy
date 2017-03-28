ab3cae23fba1f8b09eb8971ebbe1349559361b6e
Briac Pilpré <briacp@gmail.com>
2017-03-28 07:01:32 +0000

    Extracts the content of a project in a single text file (one line per segment). (FR#1282 exporting the strings only - https://sourceforge.net/p/omegat/feature-requests/182/ )
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@9745 b0d8beef-cb45-0410-a8e4-c0d495c3b779

---
+++ scripts/extract_text_content.groovy
@@ -0,0 +1,54 @@
+/* :name=Extract Text Content :description=Extracts the content of the projects in a single text file (one line per segment).
+ * see FR#1282 exporting the strings only - https://sourceforge.net/p/omegat/feature-requests/182/
+ * 
+ * @author  Briac Pilpré
+ * @date    2017-03-22
+ * @version 0.1
+ */
+import org.omegat.core.Core;
+import org.omegat.util.StringUtil;
+import static javax.swing.JOptionPane.*;
+
+def gui() {
+
+  // abort if a project is not opened yet
+  def prop = project.projectProperties
+  if (!prop) {
+    showMessageDialog null, res.getString("noProjectMsg"), res.getString("noProject"), INFORMATION_MESSAGE
+    return
+  }
+
+  def root = prop.projectRoot;
+
+  def srcTextFile = new File(root, 'project_source_content.txt');
+  def tgtTextFile = new File(root, 'project_target_content.txt');
+  def timeStamp   = new Date();
+
+  console.println(StringUtil.format(res.getString("sourceTextFile"), srcTextFile.absolutePath));
+  console.println(StringUtil.format(res.getString("targetTextFile"), tgtTextFile.absolutePath));
+
+  srcTextFile.write("## OmegaT Source Text Export (" + timeStamp + ")\n");
+  tgtTextFile.write("## OmegaT Target Text Export (" + timeStamp + ")\n");
+
+  def files = project.projectFiles;
+  for (i in 0 ..< files.size()) {
+    fi = files[i]
+
+    srcTextFile << "\n## OmegaT File:" + fi.filePath.toString() + "\n";
+    tgtTextFile << "\n## OmegaT File:" + fi.filePath.toString() + "\n";
+
+    for (j in 0 ..< fi.entries.size()) {
+      ste = fi.entries[j]
+      source = ste.getSrcText()
+      srcTextFile << source + "\n";
+
+      target = project.getTranslationInfo(ste) ? project.getTranslationInfo(ste).translation : null;
+      if (target) {
+        tgtTextFile << target + "\n";
+      }
+    }
+    
+    //console.println(source + "\t" + target);
+  }
+}
+

72dc038a20fd3c9f21b030b8cdd348f5b86a6c0c
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-04-03 01:44:31 +0000

    Fix jump in editor when double-clicking whitespace on last segment line
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@9784 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/gui/editor/EditorController.java
+++ src/org/omegat/gui/editor/EditorController.java
@@ -1041,8 +1041,10 @@ public class EditorController implements IEditor {
     protected int getSegmentIndexAtLocation(int location) {
         for (int i = 0; i < m_docSegList.length; i++) {
             SegmentBuilder builder = m_docSegList[i];
+            // Treat start and end positions inclusively to give
+            // intuitive results when double-clicking to jump in editor
             if (builder.hasBeenCreated() && location >= builder.getStartPosition()
-                    && location < builder.getEndPosition()) {
+                    && location <= builder.getEndPosition()) {
                 return i;
             }
         }

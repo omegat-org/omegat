2b30962c4688e36b8afe1f8ec6db342c4fc85393
Kos Ivantsov <k_ivantsov@yahoo.com>
2017-09-20 14:20:19 +0000

    svn_cleanup_selected.groovy updated, old (conflicting) localised *.properties removed
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@10093 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- scripts/svn_cleanup_selected.groovy
+++ scripts/svn_cleanup_selected.groovy
@@ -1,47 +1,78 @@
-/*
- * Perform SVN cleanup on any local SVN repository
+/* Perform SVN cleanup on current project or any folder (recursively)
  *
  * @author	Yu Tang
  * @author	Kos Ivatsov
- * @date	2016-04-28
- * @version	0.4
+ * @date		2017-09-20
+ * @version	0.6
  */
 
-// For 4.0 team project, this script has to be used with the project closed, otherwise the
-// SVN repository will not be found.
 
 import javax.swing.JFileChooser
+import org.omegat.core.Core
+import org.omegat.util.OStrings
+import org.omegat.util.StaticUtils
+import org.omegat.util.StringUtil
 import org.tmatesoft.svn.core.wc.*
 
-def folder
-console.clear()
-console.println(res.getString("name")+"\n${"-"*15}")
-if (project.isProjectLoaded()) {
-	def prop = project.getProjectProperties()
-	folder = new File(prop.getProjectRoot())
-	}else{
-	JFileChooser fc = new JFileChooser(
-		dialogTitle: res.getString("title"),
-		fileSelectionMode: JFileChooser.DIRECTORIES_ONLY, 
-		multiSelectionEnabled: false
-		)
-	if(fc.showOpenDialog() != JFileChooser.APPROVE_OPTION) {
-		console.println(res.getString("canceled"))
-		return
-		}
-	folder = new File(fc.getSelectedFile().toString())
-	}
+utils = (StringUtil.getMethods().toString().findAll("format")) ? StringUtil : StaticUtils
+if  (OStrings.VERSION < '3.5.2') {
+	console.clear()
+	console.println(utils.format(res.getString("oldver"), OStrings.VERSION))
+	return	
+}
 
+def directory
+def dir_list = []
+if (project.isProjectLoaded()) {
+    def prop = project.projectProperties
+    directory = new File(prop.getProjectRoot())
+    } else {
+        JFileChooser fc = new JFileChooser(
+          dialogTitle: res.getString("title"),
+          fileSelectionMode: JFileChooser.DIRECTORIES_ONLY,
+          multiSelectionEnabled: false
+          )
+        fc.setFileHidingEnabled(false)
+
+        if (fc.showOpenDialog() != JFileChooser.APPROVE_OPTION) {
+          console.println(res.getString("canceled"))
+          return
+        }
+        directory = new File(fc.getSelectedFile().toString())
+    }
+
+directory.eachDirRecurse () {
+   dir -> if (dir =~ /\.svn/) {dir_list << dir.toString().replaceAll(/\.svn.*$/, '')}
+   dir_list.unique()
+}
 
 public static boolean isSVNDirectory(File localDirectory) {
-	File svnDir = new File(localDirectory, ".svn");
-	return svnDir.exists() && svnDir.isDirectory();
-	}
-
-if (isSVNDirectory(folder)) { 
-	def clientManager = SVNClientManager.newInstance()
-	clientManager.getWCClient().doCleanup(folder)
-	console.println(res.getString("done"))
-	}else
-	console.println(res.getString("notrepo"))
+    File svnDir = new File(localDirectory, ".svn");
+    return svnDir.exists() && svnDir.isDirectory();
+}
+def count=0
+if (dir_list.size() < 1) {
+   console.println(utils.format(res.getString("norepos"), directory))
+   mainWindow.statusLabel.setText(utils.format(res.getString("count"), count))
+   Timer timer = new Timer().schedule({mainWindow.statusLabel.setText(null)} as TimerTask, 10000)
+   return 
+}
+org.omegat.util.gui.UIThreadsUtil.executeInSwingThread {
+    dir_list.each(){
+        folder = new File(it)
+        if (isSVNDirectory(folder)) { 
+            def clientManager = SVNClientManager.newInstance()
+            try {
+                clientManager.getWCClient().doCleanup(folder)
+            } catch (Exception e) {
+                console.println(utils.format(res.getString("notrepo"), folder))
+                return
+            }
+        console.println(utils.format(res.getString("done"), folder))
+        }
+        count++   
+     }
+mainWindow.statusLabel.setText(utils.format(res.getString("count"), count))
+Timer timer = new Timer().schedule({mainWindow.statusLabel.setText(null)} as TimerTask, 10000)
+} as Runnable
 return

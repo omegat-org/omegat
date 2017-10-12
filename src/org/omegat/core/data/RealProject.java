62cb8d1a8d2a554baf8e7cf18bf07d20ae4fd569
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-10-12 13:19:27 +0000

    Matches from tmx2source appear as fuzzy matches on Windows
    https://sourceforge.net/p/omegat/bugs/884/
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@10112 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- src/org/omegat/core/data/RealProject.java
+++ src/org/omegat/core/data/RealProject.java
@@ -1240,7 +1240,7 @@ public class RealProject implements IProject {
                 // not a TMX file
                 return;
             }
-            if (file.getPath().startsWith(config.getTMOtherLangRoot())) {
+            if (file.getPath().replace('\\', '/').startsWith(config.getTMOtherLangRoot())) {
                 // tmx in other language, which is already shown in editor. Skip it.
                 return;
             }

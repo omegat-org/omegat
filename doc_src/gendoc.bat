fbfd7f0642922aea3ecd2d054a0f213d887be1e2
Didier Briel <d.briel@free.fr>
2017-12-03 11:07:31 +0000

    Because of the "error" in xx.properties copying, it doesn't work anymore to launch two tasks on the same command line
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@10212 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- doc_src/gendoc.bat
+++ doc_src/gendoc.bat
@@ -12,11 +12,14 @@ call ant -Dlanguage=gl all
 call ant -Dlanguage=hu instant-start
 call ant -Dlanguage=ia all
 call ant -Dlanguage=it all
-call ant -Dlanguage=ja html pdf-cjk
+call ant -Dlanguage=ja html
+call ant -Dlanguage=ja pdf-cjk
 call ant -Dlanguage=ko instant-start
 call ant -Dlanguage=nl all
 call ant -Dlanguage=no instant-start
 call ant -Dlanguage=pt_BR all
 call ant -Dlanguage=ru all
 call ant -Dlanguage=uk instant-start
-call ant -Dlanguage=zh_CN html pdf-cjk
+call ant -Dlanguage=zh_CN html
+call ant -Dlanguage=zh_CN pdf-cjk
+

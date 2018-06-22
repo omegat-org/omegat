ce138edecc8afa712e20844891bf73c025f53527
Didier Briel <d.briel@free.fr>
2018-06-22 14:59:48 +0000

    Extract Text Content script to export to UTF-8 https://sourceforge.net/p/omegat/feature-requests/1393/
    git-svn-id: svn+ssh://svn.code.sf.net/p/omegat/svn/trunk@10427 b0d8beef-cb45-0410-a8e4-c0d495c3b779

--- scripts/extract_text_content.groovy
+++ scripts/extract_text_content.groovy
@@ -2,8 +2,9 @@
  * see RFE#1282 Extracts the content of the projects to text file - https://sourceforge.net/p/omegat/feature-requests/182/
  * 
  * @author  Briac Pilpré
- * @date    2017-03-22
- * @version 0.1
+ * @author Didier Briel
+ * @date    2018-06-22
+ * @version 0.3
  */
 import org.omegat.core.Core;
 import org.omegat.util.StringUtil;
@@ -23,28 +24,29 @@ def gui() {
   def srcTextFile = new File(root, 'project_source_content.txt');
   def tgtTextFile = new File(root, 'project_target_content.txt');
   def timeStamp   = new Date();
+  def nl = System.getProperty('line.separator')
 
   console.println(StringUtil.format(res.getString("sourceTextFile"), srcTextFile.absolutePath));
   console.println(StringUtil.format(res.getString("targetTextFile"), tgtTextFile.absolutePath));
 
-  srcTextFile.write("## OmegaT Source Text Export (" + timeStamp + ")\n");
-  tgtTextFile.write("## OmegaT Target Text Export (" + timeStamp + ")\n");
+  srcTextFile.write("## OmegaT Source Text Export (" + timeStamp + ")" + nl, "UTF-8");
+  tgtTextFile.write("## OmegaT Target Text Export (" + timeStamp + ")" + nl, "UTF-8");
 
   def files = project.projectFiles;
   for (i in 0 ..< files.size()) {
     fi = files[i]
 
-    srcTextFile << "\n## OmegaT File:" + fi.filePath.toString() + "\n";
-    tgtTextFile << "\n## OmegaT File:" + fi.filePath.toString() + "\n";
+    srcTextFile.append(nl + "## OmegaT File:" + fi.filePath.toString() + nl, "UTF-8");
+    tgtTextFile.append(nl + "## OmegaT File:" + fi.filePath.toString() + nl, "UTF-8");
 
     for (j in 0 ..< fi.entries.size()) {
       ste = fi.entries[j]
       source = ste.getSrcText()
-      srcTextFile << source + "\n";
+      srcTextFile.append(source + nl, "UTF-8");
 
       target = project.getTranslationInfo(ste) ? project.getTranslationInfo(ste).translation : null;
       if (target) {
-        tgtTextFile << target + "\n";
+        tgtTextFile.append(target + nl, "UTF-8");
       }
     }
     

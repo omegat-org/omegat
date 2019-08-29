e5f4dac19c5f781f5cfc42a568211d3610ec344e
Briac Pilpré <briacp@gmail.com>
2019-08-29 15:53:59 +0200

    Add default ParsedTuv to make sure all the tests in TMXReaderTest still pass.

--- src/org/omegat/util/TMXReader2.java
+++ src/org/omegat/util/TMXReader2.java
@@ -160,9 +160,12 @@ public class TMXReader2 {
                     if ("tu".equals(eStart.getName().getLocalPart())) {
                         parseTu(eStart);
                         ParsedTuv origTuv = getTuvByLang(sourceLanguage);
+                        // This ParsedTuv for the target translation is not used, but is there to avoid
+                        // any NPE during the callback.onEntry below.
+                        ParsedTuv targetTuv = getTuvByLang(targetLanguage);
                         // The filtering of the target Tuvs is done in
-                        // TMXLoader.loadImpl.LoadCallback.onEntry,
-                        allFound = callback.onEntry(currentTu, origTuv, null, isParagraphSegtype) && allFound;
+                        // ExternalTMFactory.TMXLoader.loadImpl => LoadCallback.onEntry,
+                        allFound = callback.onEntry(currentTu, origTuv, targetTuv, isParagraphSegtype) && allFound;
                     } else if ("header".equals(eStart.getName().getLocalPart())) {
                         parseHeader(eStart, sourceLanguage);
                     }

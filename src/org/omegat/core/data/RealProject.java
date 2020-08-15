b42897fa93b7844c2b4c3e7b6c635ac9607565a1
Vladimir V. Bychkov <github@bychkov.name>
2020-08-15 15:00:17 +0200

    Refactoring in statistics: calculation separated from presentation. Calculation statistics method returns Bean with results. Bean contains methods for presentation. This approach is very helpful for expansibility with plugins.

--- src/org/omegat/core/data/RealProject.java
+++ src/org/omegat/core/data/RealProject.java
@@ -73,6 +73,7 @@ import org.omegat.core.segmentation.Segmenter;
 import org.omegat.core.statistics.CalcStandardStatistics;
 import org.omegat.core.statistics.Statistics;
 import org.omegat.core.statistics.StatisticsInfo;
+import org.omegat.core.statistics.StatsResult;
 import org.omegat.core.team2.IRemoteRepository2;
 import org.omegat.core.team2.RebaseAndCommit;
 import org.omegat.core.team2.RemoteRepositoryProvider;
@@ -155,7 +156,7 @@ public class RealProject implements IProject {
 
     protected ImportFromAutoTMX importHandler;
 
-    private final StatisticsInfo hotStat = new StatisticsInfo();
+    private StatisticsInfo hotStat = new StatisticsInfo();
 
     private final ITokenizer sourceTokenizer, targetTokenizer;
 
@@ -372,9 +373,10 @@ public class RealProject implements IProject {
             loadOtherLanguages();
 
             // build word count
-            String stat = CalcStandardStatistics.buildProjectStats(this, hotStat);
+            StatsResult stat = CalcStandardStatistics.buildProjectStats(this);
+            hotStat = stat.toStatisticsInfo();
             String fn = config.getProjectInternal() + OConsts.STATS_FILENAME;
-            Statistics.writeStat(fn, stat);
+            Statistics.writeStat(fn, stat.getTextData());
 
             loaded = true;
 
@@ -790,9 +792,10 @@ public class RealProject implements IProject {
                 LastSegmentManager.saveLastSegment();
 
                 // update statistics
-                String stat = CalcStandardStatistics.buildProjectStats(this, hotStat);
+                StatsResult stat = CalcStandardStatistics.buildProjectStats(this);
+                hotStat = stat.toStatisticsInfo();
                 String fn = config.getProjectInternal() + OConsts.STATS_FILENAME;
-                Statistics.writeStat(fn, stat);
+                Statistics.writeStat(fn, stat.getTextData());
             } finally {
                 Core.getMainWindow().getMainMenu().getProjectMenu().setEnabled(true);
             }

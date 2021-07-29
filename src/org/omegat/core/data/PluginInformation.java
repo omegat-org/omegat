91c1b06d9d28c48d642ca68cc00c575bff249a69
Hiroshi Miura <miurahr@linux.com>
2021-07-29 23:08:00 +0900

    Improve Plugin handlings
    - docs_devel: update plugin development howto
    - PluginType: Append additional manifest parameters
    - Introduce PluginInformation$Builder class
      * Build POJO from Builder#fromManifest or Builder#fromProperties
    - Detect bundled plugins
    - Add PluginInformation#getUrl
      - getUrl is a replacement of getJarFile method.
      - When user want to know jar filename, the responsibility
        of manipurating URL is on user function.
    - Add unit tests
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/org/omegat/core/data/PluginInformation.java
+++ src/org/omegat/core/data/PluginInformation.java
@@ -4,6 +4,7 @@
           glossaries, and translation leveraging into updated projects.
 
  Copyright (C) 2020 Briac Pilpre
+               2021,2022 Hiroshi Miura
                Home page: http://www.omegat.org/
                Support center: https://omegat.org/support
 
@@ -25,102 +26,115 @@
 
 package org.omegat.core.data;
 
+import java.net.URL;
 import java.util.Properties;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
-public class PluginInformation {
-    private static final String PLUGIN_NAME = "Plugin-Name";
-    private static final String PLUGIN_VERSION = "Plugin-Version";
-    private static final String PLUGIN_AUTHOR = "Plugin-Author";
-    private static final String PLUGIN_DESCRIPTION = "Plugin-Description";
+import org.omegat.filters2.master.PluginUtils;
 
-    private static final String IMPLEMENTATION_VENDOR = "Implementation-Vendor";
-    private static final String IMPLEMENTATION_TITLE = "Implementation-Title";
-    private static final String IMPLEMENTATION_VERSION = "Implementation-Version";
-    private static final String BUNDLE_VERSION = "Bundle-Version";
-    private static final String BUNDLE_NAME = "Bundle-Name";
-    private static final String BUILT_BY = "Built-By";
+/**
+ * Plugin information POJO data class.
+ * @author Briac Pilpre
+ * @author Hiroshi Miura
+ */
+public class PluginInformation {
+
+    public enum Status {
+        INSTALLED,
+        BUNDLED,
+    }
 
     private final String className;
     private final String name;
     private final String version;
     private final String author;
     private final String description;
+    private final PluginUtils.PluginType category;
+    private final String link;
+    private final URL url;
+    private final Status status;
 
-    public PluginInformation(String className, Manifest manifest) {
+    /* The class is recommend to build from builder. */
+    private PluginInformation(String className, String name, String version, String author, String description,
+                             PluginUtils.PluginType category, String link, URL url, Status status) {
         this.className = className;
-        Attributes attrs = manifest.getMainAttributes();
-        name = findName(manifest);
-        version = findVersion(manifest);
-        author = findAuthor(manifest);
-        description = attrs.getValue(PLUGIN_DESCRIPTION);
+        this.name = name;
+        this.version = version;
+        this.author = author;
+        this.description = description;
+        this.category = category;
+        this.link = link;
+        this.url = url;
+        this.status = status;
     }
 
-    public PluginInformation(String className, Properties props) {
-        this.className = className;
-        name = null;
-        version = null;
-        author = null;
-        description = null;
-    }
-
-    private String findName(Manifest m) {
-        Attributes attrs = m.getMainAttributes();
-        if (attrs.getValue(PLUGIN_NAME) != null) {
-            return attrs.getValue(PLUGIN_NAME);
-        } else if (attrs.getValue(BUNDLE_NAME) != null) {
-            return attrs.getValue(BUNDLE_NAME);
-        } else if (attrs.getValue(IMPLEMENTATION_TITLE) != null) {
-            return attrs.getValue(IMPLEMENTATION_TITLE);
-        }
-        return null;
-    }
-
-    private String findVersion(Manifest m) {
-        Attributes attrs = m.getMainAttributes();
-        if (attrs.getValue(PLUGIN_VERSION) != null) {
-            return attrs.getValue(PLUGIN_VERSION);
-        } else if (attrs.getValue(BUNDLE_VERSION) != null) {
-            return attrs.getValue(BUNDLE_VERSION);
-        } else if (attrs.getValue(IMPLEMENTATION_VERSION) != null) {
-            return attrs.getValue(IMPLEMENTATION_VERSION);
-        }
-        return null;
-    }
-
-    private String findAuthor(Manifest m) {
-        Attributes attrs = m.getMainAttributes();
-        if (attrs.getValue(PLUGIN_AUTHOR) != null) {
-            return attrs.getValue(PLUGIN_AUTHOR);
-        } else if (attrs.getValue(IMPLEMENTATION_VENDOR) != null) {
-            return attrs.getValue(IMPLEMENTATION_VENDOR);
-        } else if (attrs.getValue(BUILT_BY) != null) {
-            return attrs.getValue(BUILT_BY);
-        }
-        return null;
-    }
-
+    /**
+     * @return className of plugin entry point
+     */
     public String getClassName() {
         return className;
     }
 
+    /**
+     * @return name of plugin
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return version of plugin
+     */
     public String getVersion() {
         return version;
     }
 
+    /**
+     * @return description of plugin features
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * @return author(s) of plugin
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * @return category type of plugin as PluginType enum
+     */
+    public final PluginUtils.PluginType getCategory() {
+        return category;
+    }
+
+    /**
+     * @return link URL of plugin homepage
+     */
+    public final String getLink() {
+        return link;
+    }
+
+    /**
+     * @return manifest URL of plugin jar
+     */
+    public URL getUrl() {
+        return url;
+    }
+
+    /**
+     * @return true if plugin is bundled with OmegaT distribution, otherwise false when 3rd party plugin
+     */
+    public final boolean isBundled() {
+        return status == Status.BUNDLED;
+    }
+
+    /**
+     * @return string expression of PluginInformation class.
+     */
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
@@ -130,6 +144,10 @@ public class PluginInformation {
         return builder.toString();
     }
 
+    /**
+     * It is identical if status is differed.
+     * @return hashCode of plugin
+     */
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -173,4 +191,109 @@ public class PluginInformation {
         return true;
     }
 
+
+    /**
+     * Builder class.
+     */
+    public static final class Builder {
+        private static final String PLUGIN_NAME = "Plugin-Name";
+        private static final String PLUGIN_VERSION = "Plugin-Version";
+        private static final String PLUGIN_AUTHOR = "Plugin-Author";
+        private static final String PLUGIN_DESCRIPTION = "Plugin-Description";
+        private static final String PLUGIN_CATEGORY = "Plugin-Category";
+        private static final String PLUGIN_LINK = "Plugin-Link";
+        private static final String PLUGIN_TYPE = "OmegaT-Plugin";
+        private static final String IMPLEMENTATION_VENDOR = "Implementation-Vendor";
+        private static final String IMPLEMENTATION_TITLE = "Implementation-Title";
+        private static final String IMPLEMENTATION_VERSION = "Implementation-Version";
+        private static final String BUNDLE_VERSION = "Bundle-Version";
+        private static final String BUNDLE_NAME = "Bundle-Name";
+        private static final String BUILT_BY = "Built-By";
+
+        /**
+         * Disable default constructor.
+         */
+        private Builder() {
+        }
+
+        /**
+         * Build PluginInformation from Manifest attributes.
+         * @param className Plugin class name.
+         * @param manifest metadata of plugin.
+         * @param mu URL of manifest
+         * @param status Plugin status, bundled or installed
+         * @return PluginInformation object.
+         */
+        public static PluginInformation fromManifest(final String className, final Manifest manifest, final URL mu,
+                                                     final Status status) {
+            Attributes mainAttrs = manifest.getMainAttributes();
+            Attributes attrs = manifest.getEntries().get(className);
+            if (attrs == null) {
+                  attrs = manifest.getMainAttributes();
+            }
+            return new PluginInformation(className, findName(className, attrs),
+                    findVersion(attrs, mainAttrs), findAuthor(mainAttrs), attrs.getValue(PLUGIN_DESCRIPTION),
+                    PluginUtils.PluginType.getTypeByValue(findCategoryKey(attrs)),
+                    attrs.getValue(PLUGIN_LINK), mu, status);
+        }
+
+        /**
+         * Build PluginInformation from properties.
+         * @param className Plugin class name.
+         * @param props plugin properties bundled with OmegaT.
+         * @param key plugin type string.
+         * @param mu URL of property
+         * @param status Plugin status, bundled or installed
+         * @return PluginInformation object.
+         */
+        public static PluginInformation fromProperties(String className, Properties props, final String key,
+                                                       final URL mu, final Status status) {
+            return new PluginInformation(className, findName(className, null),
+                    null, null, null,
+                    PluginUtils.PluginType.getTypeByValue(key),
+                    null, mu, status);
+        }
+
+        private static String findCategoryKey(Attributes attrs) {
+            return lookupAttribute(attrs, PLUGIN_CATEGORY, PLUGIN_TYPE);
+        }
+
+        private static String findName(String className, Attributes attrs) {
+            if (attrs != null) {
+                String name = lookupAttribute(attrs, PLUGIN_NAME, BUNDLE_NAME, IMPLEMENTATION_TITLE);
+                if (name != null) {
+                    return name;
+                }
+            }
+            return className.substring(className.lastIndexOf(".") + 1);
+        }
+
+        private static String findVersion(Attributes attrs, Attributes mainAttrs) {
+            String version = lookupAttribute(attrs, PLUGIN_VERSION, BUNDLE_VERSION, IMPLEMENTATION_VERSION);
+            if (version != null) {
+                return version;
+            }
+            version = lookupAttribute(mainAttrs, PLUGIN_VERSION, BUNDLE_VERSION, IMPLEMENTATION_VERSION);
+            if (version != null) {
+                return version;
+            }
+            return "unknown";
+        }
+
+        private static String findAuthor(Attributes attrs) {
+            if ("org.omegat.Main".equals(attrs.getValue("Main-Class"))) {
+                return "OmegaT team";
+            }
+            return lookupAttribute(attrs, PLUGIN_AUTHOR, IMPLEMENTATION_VENDOR, BUILT_BY);
+        }
+
+        private static String lookupAttribute(Attributes attrs, String... candidates) {
+            for (String key : candidates) {
+                if (attrs.getValue(key) != null) {
+                    return attrs.getValue(key);
+                }
+            }
+            return null;
+        }
+    }
 }

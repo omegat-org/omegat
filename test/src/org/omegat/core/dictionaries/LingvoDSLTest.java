994f1a8db8f9597984d36f7f024992958a0fd1f7
Hiroshi Miura <miurahr@linux.com>
2021-09-16 22:04:03 +0900

    LingvoDSL: adjust indent formatting
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- test/src/org/omegat/core/dictionaries/LingvoDSLTest.java
+++ test/src/org/omegat/core/dictionaries/LingvoDSLTest.java
@@ -61,7 +61,7 @@ public class LingvoDSLTest {
         List<DictionaryEntry> result = dict.readArticles(word);
         assertFalse(result.isEmpty());
         assertEquals(word, result.get(0).getWord());
-        assertEquals("Only a single white space on first character\n", result.get(0).getArticle());
+        assertEquals("<p style=\"text-indent: 30px\">Only a single white space on first character</p>\n", result.get(0).getArticle());
     }
 
     @Test
@@ -71,7 +71,7 @@ public class LingvoDSLTest {
         List<DictionaryEntry> result = dict.readArticles(word);
         assertFalse(result.isEmpty());
         assertEquals(word, result.get(0).getWord());
-        assertEquals("Translation line also can have a single TAB char\n", result.get(0).getArticle());
+        assertEquals("<p style=\"text-indent: 30px\">Translation line also can have a single TAB char</p>\n", result.get(0).getArticle());
     }
 
     @Test
@@ -81,7 +81,7 @@ public class LingvoDSLTest {
         List<DictionaryEntry> result = dict.readArticles(word);
         assertFalse(result.isEmpty());
         assertEquals(word, result.get(0).getWord());
-        assertEquals("\u0441\u0442\u0430\u043d\u043e\u043a\n", result.get(0).getArticle());
+        assertEquals("<p style=\"text-indent: 30px\">\u0441\u0442\u0430\u043d\u043e\u043a</p>\n", result.get(0).getArticle());
     }
 
     @Test
@@ -112,19 +112,19 @@ public class LingvoDSLTest {
         List<DictionaryEntry> result = dict.readArticles(word);
         assertFalse(result.isEmpty());
         assertEquals(word, result.get(0).getWord());
-        assertEquals("<strong>1.</strong> \u043E\u0442\u043A\u0430\u0437\u044B\u0432\u0430\u0442\u044C\u0441\u044F" +
+        assertEquals("<p style=\"text-indent: 30px\"><strong>1.</strong> \u043E\u0442\u043A\u0430\u0437\u044B\u0432\u0430\u0442\u044C\u0441\u044F" +
                         " (<span style='font-style: italic'>\u043E\u0442 \u0447\u0435\u0433\u043E-\u043B.</span>)," +
                         " \u043F\u0440\u0435\u043A\u0440\u0430\u0449\u0430\u0442\u044C " +
                         "(<span style='font-style: italic'>\u043F\u043E\u043F\u044B\u0442\u043A\u0438 \u0438 \u0442." +
-                        " \u043F.</span>)\n" +
-                        "<strong>2.</strong> \u043F\u043E\u043A\u0438\u0434\u0430\u0442\u044C," +
-                        " \u043E\u0441\u0442\u0430\u0432\u043B\u044F\u0442\u044C\n" +
-                        "&nbsp;to abandon attempts\n" +
-                        "&nbsp;to abandon a claim\n" +
-                        "&nbsp;to abandon convertibility\n" +
-                        "&nbsp;to abandon the gold standard\n" +
-                        "&nbsp;to abandon price control\n" +
-                        "&nbsp;to abandon a right\n",
+                        " \u043F.</span>)</p>\n" +
+                        "<p style=\"text-indent: 30px\"><strong>2.</strong> \u043F\u043E\u043A\u0438\u0434\u0430\u0442\u044C," +
+                        " \u043E\u0441\u0442\u0430\u0432\u043B\u044F\u0442\u044C</p>\n" +
+                        "<p style=\"text-indent: 60px\">to abandon attempts</p>\n" +
+                        "<p style=\"text-indent: 60px\">to abandon a claim</p>\n" +
+                        "<p style=\"text-indent: 60px\">to abandon convertibility</p>\n" +
+                        "<p style=\"text-indent: 60px\">to abandon the gold standard</p>\n" +
+                        "<p style=\"text-indent: 60px\">to abandon price control</p>\n" +
+                        "<p style=\"text-indent: 60px\">to abandon a right</p>\n",
                 result.get(0).getArticle());
     }
 

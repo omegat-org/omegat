8dae279449bb27dc54e64198cbb48e8889c95d76
Hiroshi Miura <miurahr@linux.com>
2021-10-08 20:22:49 +0900

    Move HtmlMediaSupport class to util.gui package
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/org/omegat/util/gui/HtmlMediaSupport.java
+++ src/org/omegat/util/gui/HtmlMediaSupport.java
@@ -22,7 +22,7 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **************************************************************************/
-package org.omegat.gui;
+package org.omegat.util.gui;
 
 import tokyo.northside.protocol.URLProtocolHandler;
 

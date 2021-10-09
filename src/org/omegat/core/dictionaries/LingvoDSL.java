f54592fc834f207d3f9079a5b2f3b1eb77c70da4
Hiroshi Miura <miurahr@linux.com>
2021-10-09 13:29:01 +0900

    LingvoDSL: Adjust for comments
    - Update variable name
    - Update copyright holder
    - Add more comments and todos
    - update IGNORED_TAG rules
    - Use Pattern.quote() for complex escape
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/org/omegat/core/dictionaries/LingvoDSL.java
+++ src/org/omegat/core/dictionaries/LingvoDSL.java
@@ -5,6 +5,7 @@
 
  Copyright (C) 2010 Alex Buloichik
                2015 Aaron Madlon-Kay
+               2021 Aaron Madlon-Kay, Gabix, Hiroshi Miura
                Home page: http://www.omegat.org/
                Support center: https://omegat.org/support
 
@@ -53,6 +54,7 @@ import org.omegat.util.Language;
  * <p>
  * Lingvo DSL format described in Lingvo help. See also
  * http://www.dsleditor.narod.ru/art_03.htm(russian).
+ * https://documentation.help/ABBYY-Lingvo8/G_NewDicsDSL.htm (english).
  *
  * @author Alex Buloichik (alex73mail@gmail.com)
  * @author Aaron Madlon-Kay
@@ -61,7 +63,7 @@ import org.omegat.util.Language;
 public class LingvoDSL implements IDictionaryFactory {
 
     // An ordered list of Pair of Regex pattern and replacement string
-    private static final TreeMap<Pattern, String> TREE_MAP = new TreeMap<>(Comparator.comparing(Pattern::pattern));
+    private static final TreeMap<Pattern, String> TAG_REPLACEMENTS = new TreeMap<>(Comparator.comparing(Pattern::pattern));
 
     @Override
     public boolean isSupportedFile(File file) {
@@ -118,7 +120,7 @@ public class LingvoDSL implements IDictionaryFactory {
                             }
                             word.append(line);
                         }
-                  });
+                    });
             if (word.length() > 0) {
                 data.add(word.toString(), trans.toString());
             }
@@ -140,40 +142,51 @@ public class LingvoDSL implements IDictionaryFactory {
 
     private static String replaceTag(final String line) {
         String result = line;
-        for (Map.Entry<Pattern, String> entry : TREE_MAP.entrySet()) {
+        for (Map.Entry<Pattern, String> entry : TAG_REPLACEMENTS.entrySet()) {
             result = entry.getKey().matcher(result).replaceAll(entry.getValue());
         }
         return result;
     }
 
     static {
-        TREE_MAP.put(Pattern.compile("\\[\\[(.+?)]]"), "&#91;$1&#93;");
-        TREE_MAP.put(Pattern.compile("\\\\\\["), "&#91;");
-        TREE_MAP.put(Pattern.compile("\\\\]"), "&#93;");
-        TREE_MAP.put(Pattern.compile("\\[b](?<content>.+?)\\[/b]"), "<strong>${content}</strong>");
-        TREE_MAP.put(Pattern.compile("\\[i](?<content>.+?)\\[/i]"), "<span style='font-style: italic'>${content}</span>");
-        TREE_MAP.put(Pattern.compile("\\[t](?<content>.+?)\\[/t]"), "${content}&nbsp;");
-        TREE_MAP.put(Pattern.compile("\\[br]"), "<br/>");
-        TREE_MAP.put(Pattern.compile("\\[c](?<content>.+?)\\[/c]"), "<span style='color:green'>${content}</span>");
+        // TODO: real parser.
+        // DSL language is not context-free:
+        // it allows arbitrary nesting (and also the meaning of e.g. [/m] is context-dependent)
+        // so this approach of replacing regex patterns is doomed to fail at at least some cases.
+        // and may leave some [tag].
+        // The only way to handle this language robustly is to make a real parser.
+        // ---
+        // following 3 lines replaces "[[...]]" and "\[..\]" into "[...]"
+        // but using HTML body reference to avoid further match.
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[\\[(?<content>.+?)]]"), "&#91;${content}&#93;");
+        TAG_REPLACEMENTS.put(Pattern.compile(Pattern.quote("\\[")), "&#91;");
+        TAG_REPLACEMENTS.put(Pattern.compile(Pattern.quote("\\]")), "&#93;");
+        // styling tags
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[b](?<content>.+?)\\[/b]"), "<strong>${content}</strong>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[i](?<content>.+?)\\[/i]"), "<span style='font-style: italic'>${content}</span>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[t](?<content>.+?)\\[/t]"), "${content}&nbsp;");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[c](?<content>.+?)\\[/c]"), "<span style='color:green'>${content}</span>");
         // The following line tries to replace [c value]text[/c] with text colored as per the value.
         // Since the color names are plain words like 'red', or 'blue', or 'steelgray' etc.,
-        TREE_MAP.put(Pattern.compile("\\[c\\s(?<color>[a-z]+?)](?<content>.+?)\\[/c]"),
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[c\\s(?<color>[a-z]+?)](?<content>.+?)\\[/c]"),
                 "<span style='color:${color}'>${content}</span>");
-        TREE_MAP.put(Pattern.compile("\\[m1](?<content>.+?)\\[/m]"), "<p style=\"text-indent: 30px\">${content}</p>");
-        TREE_MAP.put(Pattern.compile("\\[m2](?<content>.+?)\\[/m]"), "<p style=\"text-indent: 60px\">${content}</p>");
-        TREE_MAP.put(Pattern.compile("\\[(m3|m4|m5|m6|m7|m8|m9)](?<content>.+?)\\[/m]"),
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[sub](?<content>.+?)\\[/sub]"), "<sub>${content}</sub>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[sup](?<content>.+?)\\[/sup]"), "<sup>${content}</sup>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[u](?<content>.+?)\\[/u]"), "<span style='text-decoration:underline'>${content}</span>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[br]"), "<br/>");
+        // ignore tag 'm" but "m1" to indent 1 level, "m2" to indent 2 level and "m3" and more to indent 3 level.
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[m1](?<content>.+?)\\[/m]"), "<p style=\"text-indent: 30px\">${content}</p>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[m2](?<content>.+?)\\[/m]"), "<p style=\"text-indent: 60px\">${content}</p>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[(m3|m4|m5|m6|m7|m8|m9)](?<content>.+?)\\[/m]"),
                 "<p style=\\\"text-indent: 90px\">${content}</p>");
-        TREE_MAP.put(Pattern.compile("\\[sub](?<content>.+?)\\[/sub]"), "<sub>${content}</sub>");
-        TREE_MAP.put(Pattern.compile("\\[sup](?<content>.+?)\\[/sup]"), "<sup>${content}</sup>");
-        TREE_MAP.put(Pattern.compile("\\[u](?<content>.+?)\\[/u]"), "<span style='text-decoration:underline'>${content}</span>");
-        TREE_MAP.put(Pattern.compile("\\[url](?<link>.+?)\\[/url]"), "<a href='${link}'>Link</a>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[url](?<link>.+?)\\[/url]"), "<a href='${link}'>Link</a>");
         // The following line tries to replace a letter surrounded by ['][/'] tags (indicating stress)
         // with a red letter (the default behavior in Lingvo).
-        TREE_MAP.put(Pattern.compile("\\['](?<content>.+?)\\[/']"), "<span style='color:red'>${content}</span>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\['](?<content>.+?)\\[/']"), "<span style='color:red'>${content}</span>");
         // silently ignored these tags that can be arbitrary nested.
-        String[] IGNORED_TAGS={"\\*", "m", "com", "ex", "lang", "p", "preview", "ref", "s", "trn", "trn1", "trs", "!trs", "video"};
-        for (String tag: IGNORED_TAGS) {
-            TREE_MAP.put(Pattern.compile("\\[(?<tag>" + tag + ")](?<content>.+?)\\[/\\k<tag>]"), "${content}");
+        String[] IGNORED_TAGS = {"\\*", "m", "com", "ex", "lang", "p", "preview", "ref", "s", "trn", "trn1", "trs", "!trs", "video"};
+        for (String tag : IGNORED_TAGS) {
+            TAG_REPLACEMENTS.put(Pattern.compile("\\[(?<tag>" + tag + ")](?<content>.+?)\\[/\\k<tag>]"), "${content}");
         }
     }
 }

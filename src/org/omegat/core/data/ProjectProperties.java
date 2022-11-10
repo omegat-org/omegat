omegat-org/omegat

commit f426ee1e7788888abdf24ca266dd8e28d55f5ada

Add ProjectProperties#isTeamProject method
isTeamProject method check it has a mapping to project root

Signed-off-by: Hiroshi Miura <miurahr@linux.com>

src/org/omegat/core/data/ProjectProperties.java
@@ -52,6 +52,7 @@ import org.omegat.util.StringUtil;
 
 import gen.core.filters.Filters;
 import gen.core.project.RepositoryDefinition;
+import gen.core.project.RepositoryMapping;
 
 /**
  * Storage for project properties. May read and write project from/to disk.
@@ -433,6 +434,17 @@ public class ProjectProperties {
 
     public List<RepositoryDefinition> getRepositories() {
         return repositories;
+    }
+
+    public boolean isTeamProject() {
+        for (RepositoryDefinition repositoryDefinition: repositories) {
+            for (RepositoryMapping repositoryMapping: repositoryDefinition.getMapping()) {
+                if ("".equals(repositoryMapping.getLocal()) || "/".equals(repositoryMapping.getLocal())) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public void setRepositories(List<RepositoryDefinition> repositories) {

commit 0c8e173eee6b3c22c210490c1dc85a247e6bf62b

Fix NPE when reload local project
The bug is introduced in f426ee1e Add ProjectProperties#isTeamProject method

Signed-off-by: Hiroshi Miura <miurahr@linux.com>

src/org/omegat/core/data/ProjectProperties.java
@@ -437,6 +437,9 @@ public class ProjectProperties {
     }
 
     public boolean isTeamProject() {
+        if (repositories == null) {
+            return false;
+        }
         for (RepositoryDefinition repositoryDefinition: repositories) {
             for (RepositoryMapping repositoryMapping: repositoryDefinition.getMapping()) {
                 if ("".equals(repositoryMapping.getLocal()) || "/".equals(repositoryMapping.getLocal())) {
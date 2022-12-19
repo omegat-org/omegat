d01d7c87b528231aac45097e53f9cff47f9994e9
Jean-Christophe Helary <jean.christophe.helary@traduction-libre.org>
2022-12-20 00:30:09 +0900

    mark glossary match by default

--- src/org/omegat/util/Preferences.java
+++ src/org/omegat/util/Preferences.java
@@ -192,6 +192,7 @@ public final class Preferences {
      * backwards-compatibility.
      */
     public static final String MARK_GLOSSARY_MATCHES = "transtips";
+    public static final boolean MARK_GLOSSARY_MATCHES_DEFAULT = true;
 
     /** Mark the segments with a note with a different color */
     public static final String MARK_NOTED_SEGMENTS = "mark_noted_segments";

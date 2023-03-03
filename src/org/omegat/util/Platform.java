916d57a42210a4a3c4a90ae937df2c496c68ac16
Hiroshi Miura <miurahr@linux.com>
2023-03-04 08:34:45 +0900

    fix: work around for JRE 8 l10n bug
    - JRE 8 has a localization bug in Chinese
      - Locale class has wrong display name for
        ZA, SD, SD_IN and SD_PK
    - The problem is reported at
      https://github.com/OmegaT-L10N/zh_CN/issues/4
      and
      https://github.com/OmegaT-L10N/zh_CN/issues/5
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/org/omegat/util/Platform.java
+++ src/org/omegat/util/Platform.java
@@ -103,4 +103,16 @@ public final class Platform {
         }
         return false;
     }
+
+    public static int getJavaVersion() {
+        String[] versionElements = System.getProperty("java.version").split("\\.");
+        int discard = Integer.parseInt(versionElements[0]);
+        int version;
+        if (discard == 1) {
+            version = Integer.parseInt(versionElements[1]);
+        } else {
+            version = discard;
+        }
+        return version;
+    }
 }

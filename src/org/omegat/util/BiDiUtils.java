39027e6f0fef34b225e027472296ca45d9fca889
damien-rembert <damien.rembert@gmail.com>
2023-03-12 02:03:16 +0000

    use spaces to indent
    Signed-off-by: damien-rembert <damien.rembert@gmail.com>

--- src/org/omegat/util/BiDiUtils.java
+++ src/org/omegat/util/BiDiUtils.java
@@ -27,154 +27,153 @@
 package org.omegat.util;
 
 import java.awt.ComponentOrientation;
-import java.util.Locale;
 
 import org.omegat.core.Core;
 
 public class BiDiUtils {
 
-	public enum ORIENTATION {
-		/** All text is left-to-right oriented. */
-		ALL_LTR,
-		/** All text is right-to-left oriented. */
-		ALL_RTL,
-		/**
-		 * different texts/segments have different orientation, depending on
-		 * language/locale.
-		 */
-		DIFFER
-	};
+    public enum ORIENTATION {
+        /** All text is left-to-right oriented. */
+        ALL_LTR,
+        /** All text is right-to-left oriented. */
+        ALL_RTL,
+        /**
+         * different texts/segments have different orientation, depending on
+         * language/locale.
+         */
+        DIFFER
+    };
 
-	public static final String BIDI_LRE = "\u202a";
-	public static final String BIDI_RLE = "\u202b";
-	public static final String BIDI_PDF = "\u202c";
-	public static final String BIDI_LRM = "\u200e";
-	public static final String BIDI_RLM = "\u200f";
-	public static final char BIDI_LRM_CHAR = '\u200e';
-	public static final char BIDI_RLM_CHAR = '\u200f';
-	// TODO: delete the dagger
-	public static final String UNICODE_DAGGER = "\u2020";
+    public static final String BIDI_LRE = "\u202a";
+    public static final String BIDI_RLE = "\u202b";
+    public static final String BIDI_PDF = "\u202c";
+    public static final String BIDI_LRM = "\u200e";
+    public static final String BIDI_RLM = "\u200f";
+    public static final char BIDI_LRM_CHAR = '\u200e';
+    public static final char BIDI_RLM_CHAR = '\u200f';
+    // TODO: delete the dagger
+    public static final String UNICODE_DAGGER = "\u2020";
 
-	public static ORIENTATION getOrientationType() {
-		ORIENTATION currentOrientation;
+    public static ORIENTATION getOrientationType() {
+        ORIENTATION currentOrientation;
 
-		if (Core.getProject().isProjectLoaded()) {
-			currentOrientation = getOrientationFromProject();
-		} else if (isLocaleRtl()) {
-			// project not loaded, use locale
-			currentOrientation = ORIENTATION.ALL_RTL;
-		} else {
-			// project not loaded, default to LTR
-			currentOrientation = ORIENTATION.ALL_LTR;
-		}
-		return currentOrientation;
-	}
+        if (Core.getProject().isProjectLoaded()) {
+            currentOrientation = getOrientationFromProject();
+        } else if (isLocaleRtl()) {
+            // project not loaded, use locale
+            currentOrientation = ORIENTATION.ALL_RTL;
+        } else {
+            // project not loaded, default to LTR
+            currentOrientation = ORIENTATION.ALL_LTR;
+        }
+        return currentOrientation;
+    }
 
-	/**
-	 * Decide what document orientation should be default for source/target
-	 * languages.
-	 */
-	private static ORIENTATION getOrientationFromProject() {
-		ORIENTATION orientation;
+    /**
+     * Decide what document orientation should be default for source/target
+     * languages.
+     */
+    private static ORIENTATION getOrientationFromProject() {
+        ORIENTATION orientation;
 
-		boolean sourceLangIsRTL = isSourceLangRtl();
-		boolean targetLangIsRTL = isTargetLangRtl();
+        boolean sourceLangIsRTL = isSourceLangRtl();
+        boolean targetLangIsRTL = isTargetLangRtl();
 
-		if (sourceLangIsRTL) {
-			orientation = ORIENTATION.ALL_RTL;
-		} else {
-			orientation = ORIENTATION.ALL_LTR;
-		}
-		if (sourceLangIsRTL != targetLangIsRTL || sourceLangIsRTL != isLocaleRtl()) {
-			orientation = ORIENTATION.DIFFER;
-		}
-		return orientation;
-	}
+        if (sourceLangIsRTL) {
+            orientation = ORIENTATION.ALL_RTL;
+        } else {
+            orientation = ORIENTATION.ALL_LTR;
+        }
+        if (sourceLangIsRTL != targetLangIsRTL || sourceLangIsRTL != isLocaleRtl()) {
+            orientation = ORIENTATION.DIFFER;
+        }
+        return orientation;
+    }
 
-	/**
-	 * Decide what document orientation should be default for source/target
-	 * languages.
-	 */
-	public static ComponentOrientation getInitialOrientation() {
-		return getOrientation(null);
-	}
+    /**
+     * Decide what document orientation should be default for source/target
+     * languages.
+     */
+    public static ComponentOrientation getInitialOrientation() {
+        return getOrientation(null);
+    }
 
-	/**
-	 * Decide what document orientation should be default for source/target
-	 * languages.
-	 */
-	public static ComponentOrientation getOrientation(ORIENTATION orientationType) {
+    /**
+     * Decide what document orientation should be default for source/target
+     * languages.
+     */
+    public static ComponentOrientation getOrientation(ORIENTATION orientationType) {
 
-		ComponentOrientation targetOrientation = null;
+        ComponentOrientation targetOrientation = null;
 
-		if (orientationType == null) {
-			orientationType = getOrientationType();
-		}
+        if (orientationType == null) {
+            orientationType = getOrientationType();
+        }
 
-		switch (orientationType) {
-		case ALL_LTR:
-			targetOrientation = ComponentOrientation.LEFT_TO_RIGHT;
-			break;
-		case ALL_RTL:
-			targetOrientation = ComponentOrientation.RIGHT_TO_LEFT;
-			break;
-		case DIFFER:
-			if (isTargetLangRtl()) {
-				// using target lang direction gives better result when user
-				// starts editing.
-				targetOrientation = ComponentOrientation.RIGHT_TO_LEFT;
-			} else {
-				targetOrientation = ComponentOrientation.LEFT_TO_RIGHT;
-			}
-		}
-		return targetOrientation;
-	}
+        switch (orientationType) {
+        case ALL_LTR:
+            targetOrientation = ComponentOrientation.LEFT_TO_RIGHT;
+            break;
+        case ALL_RTL:
+            targetOrientation = ComponentOrientation.RIGHT_TO_LEFT;
+            break;
+        case DIFFER:
+            if (isTargetLangRtl()) {
+                // using target lang direction gives better result when user
+                // starts editing.
+                targetOrientation = ComponentOrientation.RIGHT_TO_LEFT;
+            } else {
+                targetOrientation = ComponentOrientation.LEFT_TO_RIGHT;
+            }
+        }
+        return targetOrientation;
+    }
 
-	public static String addRtlBidiAround(String string) {
-		return BIDI_RLE + string + BIDI_PDF;
-	}
+    public static String addRtlBidiAround(String string) {
+        return BIDI_RLE + string + BIDI_PDF;
+    }
 
-	public static String addLtrBidiAround(String string) {
-		return BIDI_LRE + string + BIDI_PDF;
-	}
+    public static String addLtrBidiAround(String string) {
+        return BIDI_LRE + string + BIDI_PDF;
+    }
 
-	public static boolean isSourceLangRtl() {
-		return isRtl(getSourceLanguage());
-	}
+    public static boolean isSourceLangRtl() {
+        return isRtl(getSourceLanguage());
+    }
 
-	public static boolean isTargetLangRtl() {
-		return isRtl(getTargetLanguage());
-	}
+    public static boolean isTargetLangRtl() {
+        return isRtl(getTargetLanguage());
+    }
 
-	public static String getSourceLanguage() {
-		return Core.getProject().getProjectProperties().getSourceLanguage().getLanguageCode();
-	}
+    public static String getSourceLanguage() {
+        return Core.getProject().getProjectProperties().getSourceLanguage().getLanguageCode();
+    }
 
-	public static String getTargetLanguage() {
-		return Core.getProject().getProjectProperties().getTargetLanguage().getLanguageCode();
-	}
+    public static String getTargetLanguage() {
+        return Core.getProject().getProjectProperties().getTargetLanguage().getLanguageCode();
+    }
 
-	/**
-	 * Check if locale is Right-To-Left oriented.
-	 * 
-	 * @return true if locale is Right-To-Left oriented.
-	 */
-	public static boolean isLocaleRtl() {
-		String language = Language.getLowerCaseLanguageFromLocale();
-		return isRtl(language);
-	}
+    /**
+     * Check if locale is Right-To-Left oriented.
+     * 
+     * @return true if locale is Right-To-Left oriented.
+     */
+    public static boolean isLocaleRtl() {
+        String language = Language.getLowerCaseLanguageFromLocale();
+        return isRtl(language);
+    }
 
-	/**
-	 * Check if language is Right-To-Left oriented.
-	 *
-	 * @param language
-	 *            ISO-639-2 language code
-	 * @return true if language is RTL
-	 */
-	public static boolean isRtl(final String language) {
-		return "ar".equalsIgnoreCase(language) || "iw".equalsIgnoreCase(language)
-				|| "he".equalsIgnoreCase(language) || "fa".equalsIgnoreCase(language)
-				|| "ur".equalsIgnoreCase(language) || "ug".equalsIgnoreCase(language)
-				|| "ji".equalsIgnoreCase(language) || "yi".equalsIgnoreCase(language);
-	}
+    /**
+     * Check if language is Right-To-Left oriented.
+     *
+     * @param language
+     *            ISO-639-2 language code
+     * @return true if language is RTL
+     */
+    public static boolean isRtl(final String language) {
+        return "ar".equalsIgnoreCase(language) || "iw".equalsIgnoreCase(language)
+                || "he".equalsIgnoreCase(language) || "fa".equalsIgnoreCase(language)
+                || "ur".equalsIgnoreCase(language) || "ug".equalsIgnoreCase(language)
+                || "ji".equalsIgnoreCase(language) || "yi".equalsIgnoreCase(language);
+    }
 }

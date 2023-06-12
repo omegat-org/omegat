0c455e874a38eb2c9d61380801e8de5592a9a1d0
Hiroshi Miura <miurahr@linux.com>
2023-06-12 20:03:58 +0900

    refactor: Platform class
    - remove unused methods
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/org/omegat/util/Platform.java
+++ src/org/omegat/util/Platform.java
@@ -74,19 +74,10 @@ public final class Platform {
     public static final boolean isAARCH64;
     // Java versions
     public static final long javaVersion;
-    public static final boolean isJava_9_orLater;
     public static final boolean isJava_11_orLater;
-    public static final boolean isJava_12_orLater;
-    public static final boolean isJava_15_orLater;
     public static final boolean isJava_17_orLater;
-    public static final boolean isJava_18_orLater;
-    // Java VMs
-    public static final boolean isJetBrainsJVM;
-    public static final boolean isJetBrainsJVM_11_orLater;
     // UI toolkits
     public static final boolean isKDE;
-    // features
-    public static final boolean isMacFullWindowContentSupported;
 
     private static OsType osType = OsType.OTHER;
 
@@ -112,44 +103,16 @@ public final class Platform {
 
         // Java versions
         javaVersion = scanVersion(System.getProperty("java.version"));
-        isJava_9_orLater = (javaVersion >= toVersion(9, 0, 0, 0));
         isJava_11_orLater = (javaVersion >= toVersion(11, 0, 0, 0));
-        isJava_12_orLater = (javaVersion >= toVersion(12, 0, 0, 0));
-        isJava_15_orLater = (javaVersion >= toVersion(15, 0, 0, 0));
         isJava_17_orLater = (javaVersion >= toVersion(17, 0, 0, 0));
-        isJava_18_orLater = (javaVersion >= toVersion(18, 0, 0, 0));
-
-        // Java VMs
-        isJetBrainsJVM = System.getProperty("java.vm.vendor", "Unknown").toLowerCase(Locale.ENGLISH)
-                .contains("jetbrains");
-        isJetBrainsJVM_11_orLater = isJetBrainsJVM && isJava_11_orLater;
 
         // UI toolkits
         isKDE = (isLinux && System.getenv("KDE_FULL_SESSION") != null);
 
-        // features
-        // available since Java 12; backported to Java 11.0.8 and 8u292
-        isMacFullWindowContentSupported = isMacOS && (javaVersion >= toVersion(11, 0, 8, 0)
-                || (javaVersion >= toVersion(1, 8, 0, 292) && !isJava_9_orLater));
-
-        // Note: Keep following at the end of this block because (optional)
-        // loading of a native library uses fields of this class. E.g. isX86_64
-
         // Windows 11 detection is implemented in Java 8u321, 11.0.14, 17.0.2
         // and 18 (or later).
         // (see https://bugs.openjdk.java.net/browse/JDK-8274840)
-        // For older Java versions, need to use a native library to get OS build
-        // number.
-        // Now no place to detect Win11, so we skip a use of a native library.
-        boolean isWin_11_orLater = false;
-        try {
-            isWin_11_orLater = isWindows_10_orLater && scanWindowsVersion(osName) >= toVersion(11, 0, 0, 0);
-        } catch (Throwable ex) {
-            // catch to avoid that application can not start if a native library
-            // is not up-to-date
-            Log.logErrorRB(ex, null);
-        }
-        isWindows_11_orLater = isWin_11_orLater;
+        isWindows_11_orLater = isWindows_10_orLater && scanWindowsVersion(osName) >= toVersion(11, 0, 0, 0);
         //
         if (isLinux) {
             osType = is64Bit() ? OsType.LINUX64 : OsType.LINUX32;

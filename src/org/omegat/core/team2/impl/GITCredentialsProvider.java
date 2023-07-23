4b1d821cf81d904c84fd08464b345a1319f91e6e
Briac Pilpré <bpilpre@efalia.com>
2023-07-23 23:46:23 +0200

    Backport rfe#1670
    - omegat-org/omegat@6c7b8ff415b7d6a730cca99b6341f3c885f6beb6
    - omegat-org/omegat@1a15c45606883bc7394015a234ea5b6ac0117d38

--- src/org/omegat/core/team2/impl/GITCredentialsProvider.java
+++ src/org/omegat/core/team2/impl/GITCredentialsProvider.java
@@ -134,15 +134,20 @@ public class GITCredentialsProvider extends CredentialsProvider {
     }
 
     private Credentials loadCredentials(URIish uri) {
-        String url = uri.toString();
+        String url = uri.toString(); // now we use schema://server:port but we keep this for backward compatibility
         Credentials credentials = new Credentials();
         credentials.username = TeamSettings.get(url + "!" + KEY_USERNAME_SUFFIX);
         credentials.password = TeamUtils.decodePassword(TeamSettings.get(url + "!" + KEY_PASSWORD_SUFFIX));
+        if (credentials.username == null) {
+            url = "" + uri.getScheme() + "://" + uri.getHost() + ":" + uri.getPort();
+            credentials.username = TeamSettings.get(url + "!" + KEY_USERNAME_SUFFIX);
+            credentials.password = TeamUtils.decodePassword(TeamSettings.get(url + "!" + KEY_PASSWORD_SUFFIX));
+        }
         return credentials;
     }
 
     private void saveCredentials(URIish uri, Credentials credentials) {
-        String url = uri.toString();
+        String url = "" + uri.getScheme() + "://" + uri.getHost() + ":" + uri.getPort();
         try {
             TeamSettings.set(url + "!" + KEY_USERNAME_SUFFIX, credentials.username);
             TeamSettings.set(url + "!" + KEY_PASSWORD_SUFFIX, TeamUtils.encodePassword(credentials.password));

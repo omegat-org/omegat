aa2b35592715bd1d7b95110d3f07f2a3625f6fed
Hiroshi Miura <miurahr@linux.com>
2023-07-31 18:06:02 +0900

    refactor(test): introduce TestCoreWireMock class
    - Separate WireMock related class from TestCore for test performance
    - Test cases which need WireMock facility, it should inherit from TestCoreWireMock
    - Add dependency for SLF4J for test-fixtures
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ test/fixtures/org/omegat/core/TestCoreWireMock.java
@@ -0,0 +1,37 @@
+/*
+ *  OmegaT - Computer Assisted Translation (CAT) tool
+ *           with fuzzy matching, translation memory, keyword search,
+ *           glossaries, and translation leveraging into updated projects.
+ *
+ *  Copyright (C) 2023 Hiroshi Miura
+ *                Home page: https://www.omegat.org/
+ *                Support center: https://omegat.org/support
+ *
+ *  This file is part of OmegaT.
+ *
+ *  OmegaT is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OmegaT is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.omegat.core;
+
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
+
+public class TestCoreWireMock extends TestCore {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(
+            WireMockConfiguration.wireMockConfig().dynamicPort().dynamicHttpsPort());
+}

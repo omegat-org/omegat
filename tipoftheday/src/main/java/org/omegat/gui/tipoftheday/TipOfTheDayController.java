abff607a9675642a27456d04d530a42790e8d563
Hiroshi Miura <miurahr@linux.com>
2023-08-22 23:41:58 +0900

    [6.2] refactor(tipfotheday): add dialog class (#592)
    * refactor(tipfotheday): add dialog class
    
    - Enable default close button
    - set InputMap; left, right, escape
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>
    
    * feat: unload TipOfTheDay when shutdown
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>
    
    * fix: remove menu item only when added
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>
    
    ---------
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tipoftheday/src/main/java/org/omegat/gui/tipoftheday/TipOfTheDayController.java
+++ tipoftheday/src/main/java/org/omegat/gui/tipoftheday/TipOfTheDayController.java
@@ -25,13 +25,17 @@
 
 package org.omegat.gui.tipoftheday;
 
-import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.ResourceBundle;
 
+import javax.swing.AbstractAction;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -49,6 +53,8 @@ public final class TipOfTheDayController {
     static final String INDEX_YAML = "tips.yaml";
     private static final String TIPOFTHEDAY_SHOW_ON_STARTUP = "tipoftheday_show_on_start";
     private static final String TIPOFTHEDAY_CURRENT_TIP = "tipoftheday_current_tip";
+    private static boolean menuAdded = false;
+    private static JMenuItem totdMenu = new JMenuItem();
 
     @SuppressWarnings("unused")
     public static void loadPlugins() {
@@ -74,16 +80,20 @@ public final class TipOfTheDayController {
             }
 
             private void initMenu() {
-                JMenuItem totdMenu = new JMenuItem();
                 totdMenu.setText(UIManager.getDefaults().getString("TipOfTheDay.menuItemText"));
                 totdMenu.setToolTipText(UIManager.getDefaults().getString("TipOfTheDay.menuToolTipText"));
-                // force to show Tip of the Day dialog.
+                // show Tip of the Day dialog on startup.
                 totdMenu.addActionListener(actionEvent -> TipOfTheDayController.start(true));
                 MenuExtender.addMenuItem(MenuKey.HELP, totdMenu);
+                menuAdded = true;
             }
 
             @Override
             public void onApplicationShutdown() {
+                if (menuAdded) {
+                    MenuExtender.removeMenuItems(MenuKey.HELP, Collections.singletonList(totdMenu));
+                    menuAdded = false;
+                }
             }
         });
     }
@@ -116,23 +126,46 @@ public final class TipOfTheDayController {
         }
         totd.setCurrentTip(currentTip);
 
-        TipOfTheDay.ShowOnStartupChoice choice = new TipOfTheDay.ShowOnStartupChoice() {
-            @Override
-            public void setShowingOnStartup(boolean showOnStartup) {
-                Preferences.setPreference(TIPOFTHEDAY_SHOW_ON_STARTUP, showOnStartup);
-            }
 
-            @Override
-            public boolean isShowingOnStartup() {
-                return Preferences.isPreferenceDefault(TIPOFTHEDAY_SHOW_ON_STARTUP, true);
-            }
-        };
         JFrame mainFrame = Core.getMainWindow().getApplicationFrame();
-        JDialog dialog = totd.getUI().createDialog(mainFrame, choice);
-        dialog.setPreferredSize(new Dimension(900, 450));
-        dialog.pack();
+        TipOfTheDayDialog dialog = new TipOfTheDayDialog(mainFrame,
+                UIManager.getString("TipOfTheDay.dialogTitle"), totd);
+
+        dialog.showOnCB.setSelected(Preferences.isPreferenceDefault(TIPOFTHEDAY_SHOW_ON_STARTUP, true));
+        dialog.previousTipButton.addActionListener(e -> totd.previousTip());
+        dialog.nextTipButton.addActionListener(e -> totd.nextTip());
+        dialog.closeButton.addActionListener(actionEvent -> close(dialog));
+
+        totd.getActionMap().put("close", new CloseAction(dialog));
+
+        // set keymap for actions of TipOfTheDay
+        totd.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "previousTip");
+        totd.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "nextTip");
+        totd.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "close");
+
         dialog.setVisible(true);
-        dialog.dispose();
         Preferences.setPreference(TIPOFTHEDAY_CURRENT_TIP, totd.getCurrentTip());
+        Preferences.setPreference(TIPOFTHEDAY_SHOW_ON_STARTUP, dialog.showOnCB.isSelected());
+        close(dialog);
+    }
+
+    private static void close(JDialog dialog) {
+        if (dialog.isDisplayable()) {
+            dialog.dispose();
+        }
+    }
+
+    @SuppressWarnings("serial")
+    private static class CloseAction extends AbstractAction {
+        JDialog dialog;
+
+        CloseAction(JDialog dialog) {
+            this.dialog = dialog;
+        }
+
+        @Override
+        public void actionPerformed(final ActionEvent e) {
+            close(dialog);
+        }
     }
 }

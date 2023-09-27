fdb4a382532e27ce4e0ab274f8572ec508ae303c
Thomas CORDONNIER <t_cordonnier@yahoo.fr>
2023-09-27 11:41:58 +0200

    Revert "[BUGS#1164] Fix reloading TMX after team synchronization"
    This reverts commit 99ed84e859a10966be9c5b5a44354e2317604707.

--- src/org/omegat/core/data/RealProject.java
+++ src/org/omegat/core/data/RealProject.java
@@ -992,11 +992,6 @@ public class RealProject implements IProject {
                         public void rebaseAndSave(File out) throws Exception {
                             mergeTMX(baseTMX, headTMX, commitDetails);
                             projectTMX.exportTMX(config, out, false, false, true);
-                            
-                            ProjectTMX newTMX = new ProjectTMX(config.getSourceLanguage(),
-                                    config.getTargetLanguage(), config.isSentenceSegmentingEnabled(), new File(
-                                            config.getProjectInternalDir(), OConsts.STATUS_EXTENSION), null);
-                            projectTMX.replaceContent(newTMX);
                         }
 
                         @Override
@@ -1009,6 +1004,13 @@ public class RealProject implements IProject {
                             return TMXReader2.detectCharset(file);
                         }
                     });
+            if (projectTMX != null) {
+                // it can be not loaded yet
+                ProjectTMX newTMX = new ProjectTMX(config.getSourceLanguage(),
+                        config.getTargetLanguage(), config.isSentenceSegmentingEnabled(), new File(
+                                config.getProjectInternalDir(), OConsts.STATUS_EXTENSION), null);
+                projectTMX.replaceContent(newTMX);
+            }
         }
 
         if (processGlossary) {

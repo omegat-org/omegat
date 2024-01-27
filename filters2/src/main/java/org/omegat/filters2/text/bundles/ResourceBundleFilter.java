81d868dab60da854dd678de5393d326a1562e77a
Hiroshi Miura <miurahr@linux.com>
2024-01-27 15:54:41 +0900

    chore: tweak unexpected copyright header change
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- filters2/src/main/java/org/omegat/filters2/text/bundles/ResourceBundleFilter.java
+++ filters2/src/main/java/org/omegat/filters2/text/bundles/ResourceBundleFilter.java
@@ -1,27 +1,33 @@
-/*******************************************************************************
-  OmegaT - Computer Assisted Translation (CAT) tool
-           with fuzzy matching, translation memory, keyword search,
-           glossaries, and translation leveraging into updated projects.
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool
+          with fuzzy matching, translation memory, keyword search,
+          glossaries, and translation leveraging into updated projects.
 
-  Copyright (C) 2000-2024 Hiroshi Miura
-                Home page: https://www.omegat.org/
-                Support center: https://omegat.org/support
+ Copyright (C) 2000-2006 Keith Godfrey and Maxym Mykhalchuk
+               2009 Alex Buloichik
+               2011 Martin Fleurke
+               2013-2014 Enrique Estevez, Didier Briel
+               2015 Aaron Madlon-Kay, Enrique Estevez
+               2016 Aaron Madlon-Kay
+               2023-2024 Hiroshi Miura
+               Home page: https://www.omegat.org/
+               Support center: https://omegat.org/support
 
-  This file is part of OmegaT.
+ This file is part of OmegaT.
 
-  OmegaT is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+ OmegaT is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-  OmegaT is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+ OmegaT is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- ******************************************************************************/
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **************************************************************************/
 
 package org.omegat.filters2.text.bundles;
 

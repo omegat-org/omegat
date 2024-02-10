f59958cb1bdfd722828da906054254971c6aabbb
Hiroshi Miura <miurahr@linux.com>
2024-02-10 15:42:54 +0900

    [BUGS#1247] fix: filters4: create event reader when entering event mode (#945)
    * [BUGS#1247] fix: filters4: create event reader when entering event mode
    
    - Complete solution to avoid StaX implementation difference.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>
    
    * style: improve filters4 styles
    
    - AbstractXmlFilter: variable name, case-default
    - OpenXmlFilter: variable name, suppression update
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>
    
    ---------
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/org/omegat/filters4/xml/xliff/Xliff1Filter.java
+++ src/org/omegat/filters4/xml/xliff/Xliff1Filter.java
@@ -92,7 +92,7 @@ public class Xliff1Filter extends AbstractXliffFilter {
     }
 
     @Override // start events on body
-    protected void checkCurrentCursorPosition(XMLStreamReader reader, boolean doWrite) {
+    protected boolean checkCurrentCursorPosition(XMLStreamReader reader, boolean doWrite) {
         if (reader.getEventType() == StartElement.START_ELEMENT) {
             if (reader.getLocalName().equals("body")) {
                 this.isEventMode = true;
@@ -115,6 +115,7 @@ public class Xliff1Filter extends AbstractXliffFilter {
                 }
             }
         }
+        return isEventMode;
     }
 
     @Override

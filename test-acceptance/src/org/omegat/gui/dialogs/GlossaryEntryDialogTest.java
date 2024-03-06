a5614b66c097d61b89380414197c38720a5ec08d
Hiroshi Miura <miurahr@linux.com>
2024-03-06 17:39:51 +0900

    test: explain expected behavior in comment
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- test-acceptance/src/org/omegat/gui/dialogs/GlossaryEntryDialogTest.java
+++ test-acceptance/src/org/omegat/gui/dialogs/GlossaryEntryDialogTest.java
@@ -22,34 +22,49 @@ public class GlossaryEntryDialogTest extends TestCoreGUI {
 
     private File tmpDir;
 
+    /**
+     * Test case to create glossary entry and see a result.
+     * @throws Exception
+     */
     @Test
     public void testOpenGlossaryEntryDialog() throws Exception {
-        // Prepare project folder
+        // 0. Prepare project folder
         tmpDir = Files.createTempDirectory("omegat-sample-project-").toFile();
         File projSrc = new File("test-acceptance/data/project/");
         FileUtils.copyDirectory(projSrc, tmpDir);
         FileUtils.forceDeleteOnExit(tmpDir);
+        // 1. Prepare preference for the test;
+        // * OmegaT doesn't show a file list dialog on a project load event.
+        // * Glossary sort order preference
         Preferences.setPreference(Preferences.PROJECT_FILES_SHOW_ON_LOAD, false);
         Preferences.setPreference(Preferences.GLOSSARY_SORT_BY_LENGTH, false);
         Preferences.setPreference(Preferences.GLOSSARY_SORT_BY_SRC_LENGTH, false);
         assertFalse(Preferences.isPreferenceDefault(Preferences.PROJECT_FILES_SHOW_ON_LOAD, true));
-        //
+        // 2. Open a sample project.
         SwingUtilities.invokeAndWait(() -> ProjectUICommands.projectOpen(tmpDir));
+        // Enforce "Edit" menu enabled.
         final JMenuItem menuItem = window.menuItem(BaseMainWindowMenu.EDIT_MENU).target();
         SwingUtilities.invokeAndWait(() -> menuItem.setEnabled(true));
-        //
+        // 3. Click menu Edit > Create glossary menu items.
         window.menuItem(BaseMainWindowMenu.EDIT_MENU).click();
         window.menuItem(BaseMainWindowMenu.EDIT_CREATE_GLOSSARY_MENUITEM).click();
+        // 4. Check Create glossary dialog visible
         window.dialog(CreateGlossaryEntry.DIALOG_NAME).requireVisible();
-        window.dialog(CreateGlossaryEntry.DIALOG_NAME).textBox(CreateGlossaryEntry.SOURCE_TEXT_FIELD).enterText("Apertium");
-        window.dialog(CreateGlossaryEntry.DIALOG_NAME).textBox(CreateGlossaryEntry.TARGET_TEXT_FIELD).enterText("Translation Engine");
+        // 5. Enter glossary keyword and translation in the field
+        window.dialog(CreateGlossaryEntry.DIALOG_NAME).textBox(CreateGlossaryEntry.SOURCE_TEXT_FIELD)
+                .enterText("Apertium");
+        window.dialog(CreateGlossaryEntry.DIALOG_NAME).textBox(CreateGlossaryEntry.TARGET_TEXT_FIELD)
+                .enterText("Translation Engine");
+        // 6. Click a OK button
         window.dialog(CreateGlossaryEntry.DIALOG_NAME).button(CreateGlossaryEntry.OK_BUTTON).click();
-        // check glossary pane
+        // 7. Enforce refresh glossary pane
         GlossaryTextArea glossaryTextArea =
                 ((GlossaryTextArea) (window.textBox(GlossaryTextArea.TEXTPANE_NAME).target()));
         glossaryTextArea.refresh();
+        // 8. Wait showing a search result during some operation
         window.textBox(GlossaryTextArea.TEXTPANE_NAME).rightClick();
         window.pressKey(KeyEvent.VK_ESCAPE);
+        // Check the glossary pane shown in the pane
         assertFalse(glossaryTextArea.getDisplayedEntries().isEmpty());
     }
 }

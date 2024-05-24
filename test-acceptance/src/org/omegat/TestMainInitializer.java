9d75cf41883b880dfeaf300056d03254e7aa2912
Hiroshi Miura <miurahr@linux.com>
2024-05-24 12:54:04 +0900

    fix: TestCoreGUI class loader config
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- test-acceptance/src/org/omegat/TestMainInitializer.java
+++ test-acceptance/src/org/omegat/TestMainInitializer.java
@@ -2,8 +2,6 @@ package org.omegat;
 
 import javax.swing.UIManager;
 
-import org.omegat.filters2.master.PluginUtils;
-
 public final class TestMainInitializer {
 
     private TestMainInitializer() {
@@ -13,7 +11,6 @@ public final class TestMainInitializer {
         ClassLoader cl = ClassLoader.getSystemClassLoader();
         MainClassLoader mainClassLoader = (cl instanceof MainClassLoader) ? (MainClassLoader) cl
                 : new MainClassLoader(cl);
-        PluginUtils.getThemePluginJars().forEach(mainClassLoader::add);
         UIManager.put("ClassLoader", mainClassLoader);
     }
 

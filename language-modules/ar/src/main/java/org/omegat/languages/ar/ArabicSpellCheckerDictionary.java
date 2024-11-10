afede7a5ca589a6c53ba7c1cb024fbd16d8327f8
Hiroshi Miura <miurahr@linux.com>
2024-11-10 18:06:58 +0900

    feat: SpellChecker Dictionary as a language module
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ language-modules/ar/src/main/java/org/omegat/languages/ar/ArabicSpellCheckerDictionary.java
@@ -0,0 +1,56 @@
+package org.omegat.languages.ar;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.util.Collections;
+
+import org.apache.lucene.analysis.ar.ArabicAnalyzer;
+import org.apache.lucene.analysis.hunspell.Dictionary;
+import org.apache.lucene.analysis.util.CharArraySet;
+
+import org.languagetool.JLanguageTool;
+
+import org.omegat.core.spellchecker.ISpellCheckerDictionary;
+
+public class ArabicSpellCheckerDictionary implements ISpellCheckerDictionary, AutoCloseable {
+
+    private static final String DICTIONARY_PATH = "/org/languagetool/resource/ar/hunspell/";
+
+    private InputStream affixInputStream;
+    private InputStream dictInputStream;
+
+    @Override
+    public Dictionary getHunspellDictionary() {
+        try {
+            affixInputStream = JLanguageTool.getDataBroker().getAsStream(DICTIONARY_PATH + "ar.aff");
+            dictInputStream = JLanguageTool.getDataBroker().getAsStream(DICTIONARY_PATH + "ar.dic");
+            return new Dictionary(affixInputStream, Collections.singletonList(dictInputStream), true);
+        } catch (IOException | ParseException ignored) {
+        }
+        return null;
+    }
+
+    @Override
+    public morfologik.stemming.Dictionary getMofologikDictionary() {
+        return null;
+    }
+
+    @Override
+    public CharArraySet getStopWordSet() {
+        return ArabicAnalyzer.getDefaultStopSet();
+    }
+
+    @Override
+    public DictionaryType getDictionaryType() {
+        return DictionaryType.HUNSPELL;
+    }
+
+    @Override
+    public void close() {
+        try {
+            affixInputStream.close();
+            dictInputStream.close();
+        } catch (IOException ignored) {}
+    }
+}
---
+++ language-modules/el/src/main/java/org/omegat/languages/el/GreekSpellCheckerDictionary.java
@@ -0,0 +1,56 @@
+package org.omegat.languages.el;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.util.Collections;
+
+import org.apache.lucene.analysis.ar.ArabicAnalyzer;
+import org.apache.lucene.analysis.hunspell.Dictionary;
+import org.apache.lucene.analysis.util.CharArraySet;
+import org.languagetool.JLanguageTool;
+
+import org.omegat.core.spellchecker.ISpellCheckerDictionary;
+
+public class GreekSpellCheckerDictionary implements ISpellCheckerDictionary, AutoCloseable {
+
+    private static final String DICTIONARY_PATH = "/org/languagetool/resource/el/hunspell/";
+
+    private InputStream affixInputStream;
+    private InputStream dictInputStream;
+
+    @Override
+    public Dictionary getHunspellDictionary() {
+        try {
+            affixInputStream = JLanguageTool.getDataBroker().getAsStream(DICTIONARY_PATH + "el_GR.aff");
+            dictInputStream = JLanguageTool.getDataBroker().getAsStream(DICTIONARY_PATH + "el_GRdict");
+            return new Dictionary(affixInputStream, Collections.singletonList(dictInputStream), true);
+        } catch (IOException | ParseException ignored) {
+        }
+        return null;
+    }
+
+    @Override
+    public morfologik.stemming.Dictionary getMofologikDictionary() {
+        return null;
+    }
+
+    @Override
+    public CharArraySet getStopWordSet() {
+        return ArabicAnalyzer.getDefaultStopSet();
+    }
+
+    @Override
+    public DictionaryType getDictionaryType() {
+        return DictionaryType.HUNSPELL;
+    }
+
+    @Override
+    public void close() {
+        try {
+            affixInputStream.close();
+            dictInputStream.close();
+        } catch (IOException ignored) {
+        }
+    }
+}

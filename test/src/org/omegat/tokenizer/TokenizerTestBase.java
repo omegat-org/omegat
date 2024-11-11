555a53baa0ae62dd3bb7501ae35788c3a90c3f7d
Hiroshi Miura <miurahr@linux.com>
2024-11-12 00:01:21 +0900

    feat: HunspellTokenizer handles stopWords (#1182)
    * feat: HunspellTokenizer handles stopWords
    
    User can use HunspellTokenizer when users have a corresponding dictionary for minor languages.
    OmegaT supports these languages by bundling stopwords.
    
    There are af, bn, et, gu, ha, he, ko, ku, mr, ms, sl, st, tl, uk, ur, yo, zh and zu stopwords.
    
    and further improvements as follows
    - Get StopWords from Lucene language analyzers or bundled one
    - Add private method getEffectiveStopWordSet
    - Refactor populateInstalledDicts to check early for `listFiles` nullity.
    - Add tests, change HunspellTokenizer#getDict visibility for the test
       - Add English test dictionary
       - Add Spanish test dictionary
    - style: apply spotless
    docs: update license description
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>
    
    * fix: rmeove stopwords-ja.txt
    
    - japanese stopwords are provided by Lucene Japanese Analyzer
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>
    
    ---------
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ test/src/org/omegat/tokenizer/TokenizerTestBase.java
@@ -0,0 +1,51 @@
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool
+          with fuzzy matching, translation memory, keyword search,
+          glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2015 Aaron Madlon-Kay
+               2024 Hiroshi Miura
+               Home page: https://www.omegat.org/
+               Support center: https://omegat.org/support
+
+ This file is part of OmegaT.
+
+ OmegaT is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OmegaT is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **************************************************************************/
+package org.omegat.tokenizer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.omegat.util.Token;
+
+public class TokenizerTestBase {
+
+    protected void assertVerbatim(String[] expected, String[] test, Token[] testTok, String origString) {
+        assertResult(expected, test);
+        assertEquals(StringUtils.join(expected), StringUtils.join(test));
+        assertEquals(expected.length, testTok.length);
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], testTok[i].getTextFromString(origString));
+        }
+    }
+
+    protected void assertResult(String[] expected, String[] test) {
+        assertEquals(expected.length, test.length);
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], test[i]);
+        }
+    }
+}

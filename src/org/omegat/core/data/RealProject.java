b676498789092ee2aad52f598f8f3f050275e8ef
Thomas CORDONNIER <t_cordonnier@yahoo.fr>
2025-01-16 17:42:29 +0100

    Also wait for events loop from GUI thread

--- src/org/omegat/core/data/RealProject.java
+++ src/org/omegat/core/data/RealProject.java
@@ -714,6 +714,9 @@ public class RealProject implements IProject {
         // them to
         // the repository (BUGS#1176)
         CoreEvents.fireProjectChange(IProjectEventListener.PROJECT_CHANGE_TYPE.COMPILE);
+        while (CoreEvents.isInProjectEventLoop) {
+            Thread.yield();
+        }
         while (org.omegat.gui.scripting.ScriptingWindow.window.inEventsLoop > 0) {
             Thread.yield();
         }

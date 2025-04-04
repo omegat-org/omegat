609c610386698a4ba8cf941e7d007c80feca4614
Hiroshi Miura <miurahr@linux.com>
2025-04-04 22:04:24 +0900

    Remove unused import from TestTeamIntegrationChild.java
    The `Collections` import was unused and has been removed to improve code cleanliness. This change helps maintain adherence to best practices by keeping imports concise and relevant.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- test-integration/src/org/omegat/core/data/TestTeamIntegrationChild.java
+++ test-integration/src/org/omegat/core/data/TestTeamIntegrationChild.java
@@ -34,7 +34,6 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;

6038e26e61bd9f3d11a256836d57e0c8694b27c0
Hiroshi Miura <miurahr@linux.com>
2025-04-19 23:14:21 +0900

    Remove unused Locale import from MainWindowMenuTest.java
    This import was not used anywhere in the file, so it has been removed to clean up the code. This helps improve readability and maintainability.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- test/src/org/omegat/gui/main/MainWindowMenuTest.java
+++ test/src/org/omegat/gui/main/MainWindowMenuTest.java
@@ -37,7 +37,6 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;

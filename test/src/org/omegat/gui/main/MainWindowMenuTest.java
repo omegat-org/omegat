d475fc43ce541b99e2cdac2061a7d3844f02a5fb
Hiroshi Miura <miurahr@linux.com>
2025-04-20 18:36:19 +0900

    [6.0] chore: check for the existence of the menu invocation action method
    - There are several places to call action method in MainMenuHandler class from GUI parts. It uses indirect invocation of class, so it does not detect undefined error when compile time.
    - This test case is to detect such a case.
    - It is a backport from 6.1 development.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- test/src/org/omegat/gui/main/MainWindowMenuTest.java
+++ test/src/org/omegat/gui/main/MainWindowMenuTest.java
@@ -28,16 +28,29 @@ package org.omegat.gui.main;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.nio.ByteBuffer;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CodingErrorAction;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
 import org.junit.Test;
+
 import org.omegat.core.TestCore;
 
 /**
@@ -53,7 +66,7 @@ public class MainWindowMenuTest extends TestCore {
     public void testMenuActions() throws Exception {
         int count = 0;
 
-        Map<String, Method> existsMethods = new HashMap<String, Method>();
+        Map<String, Method> existsMethods = new HashMap<>();
 
         for (Method m : MainWindowMenuHandler.class.getDeclaredMethods()) {
             if (Modifier.isPublic(m.getModifiers()) && !Modifier.isStatic(m.getModifiers())) {
@@ -84,7 +97,63 @@ public class MainWindowMenuTest extends TestCore {
             }
         }
         assertTrue("menu items not found", count > 30);
-        assertTrue("There is action handlers in MainWindow which doesn't used in menu: " + existsMethods.keySet(),
-                existsMethods.isEmpty());
+        assertTrue("There is action handlers in MainWindow which doesn't used in menu: "
+                + existsMethods.keySet(), existsMethods.isEmpty());
+    }
+
+    @Test
+    public void testMenuActions_invokeActions() throws Exception {
+        Pattern pattern = Pattern.compile("getMainMenu\\(\\).invokeAction\\(\\s*\"([^\"]+)\"\\s*,");
+        final String[] target = {"src"};
+        processSourceContent(target, (path, chars) -> {
+            Matcher matcher = pattern.matcher(chars);
+            while (matcher.find()) {
+                String actionMethodName = matcher.group(1) + "ActionPerformed";
+                Method method = null;
+                try {
+                    method = MainWindowMenuHandler.class.getMethod(actionMethodName);
+                } catch (NoSuchMethodException ignore) {
+                    // See if the method accepts a modifier key argument.
+                    try {
+                        method = MainWindowMenuHandler.class.getMethod(actionMethodName, Integer.TYPE);
+                    } catch (NoSuchMethodException ex) {
+                        assertNotNull("Action method \"" + actionMethodName + "\" not defined for invoked from " + path,
+                                method);
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * Process the text content of all .java files under /src. Also check
+     * DIRECTIONALITY_LEFT_TO_RIGHT_OVERRIDE for security reasons.
+     * And check invisible space character.
+     *
+     * @param consumer
+     *            A function that accepts the file path and content
+     * @throws IOException
+     *             from Files.find()
+     */
+    public void processSourceContent(String[] targets, BiConsumer<Path, CharSequence> consumer)
+            throws IOException {
+        CharsetDecoder decoder = StandardCharsets.UTF_8.newDecoder();
+        decoder.onMalformedInput(CodingErrorAction.REPORT);
+        decoder.onUnmappableCharacter(CodingErrorAction.REPORT);
+        for (String root : targets) {
+            Path rootPath = Paths.get(".", root);
+            assertTrue(rootPath.toFile().isDirectory());
+            try (Stream<Path> files = Files.find(rootPath, 100,
+                    (path, attrs) -> attrs.isRegularFile() && path.toString().endsWith(".java"))) {
+                files.forEach(p -> {
+                    try {
+                        byte[] bytes = Files.readAllBytes(p);
+                        consumer.accept(p, decoder.decode(ByteBuffer.wrap(bytes)));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+        }
     }
 }

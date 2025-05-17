5f78973a86a3867491b0be344e96970d260cd198
Hiroshi Miura <miurahr@linux.com>
2025-05-17 12:31:43 +0900

    style: Remove unused import of FileNotFoundException.
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/org/omegat/core/segmentation/SRX.java
+++ src/org/omegat/core/segmentation/SRX.java
@@ -29,7 +29,6 @@ package org.omegat.core.segmentation;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;

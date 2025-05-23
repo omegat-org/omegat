eeed1b9ad4077bf8531903727f9da18868dc4f77
Hiroshi Miura <miurahr@linux.com>
2025-05-23 16:41:45 +0900

    refactor: HTMLReader to move the encoding detection logic to util
    - Separated BOM detection, content-based encoding detection, and charset parsing into distinct methods for clarity and reusability.
    - Replaced redundant code with a streamlined structure, enhancing maintainability and readability.
    - Introduced BufferedFileReader as a base class for HTMLReader to simplify encoding and file handling.
    - Moved encoding detection logic to `org.omegat.util.EncodingDetector` for reusability and better separation of concerns.i
      This enhances code modularity and reduces duplication.
    - Added `EncodingDetectorTest` to validate HTML encoding detection functionality.
    - Introduced multiple test HTML files to validate encoding, Covers cases for UTF-8, UTF-16 (LE/BE) with BOM, and files with no headers.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ test/src/org/omegat/util/EncodingDetectorTest.java
@@ -0,0 +1,56 @@
+/*
+ *  OmegaT - Computer Assisted Translation (CAT) tool
+ *           with fuzzy matching, translation memory, keyword search,
+ *           glossaries, and translation leveraging into updated projects.
+ *
+ *  Copyright (C) 2025 Hiroshi Miura
+ *                Home page: https://www.omegat.org/
+ *                Support center: https://omegat.org/support
+ *
+ *  This file is part of OmegaT.
+ *
+ *  OmegaT is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OmegaT is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.omegat.util;
+
+import org.junit.Test;
+
+import java.nio.charset.Charset;
+
+import static org.junit.Assert.assertEquals;
+
+public class EncodingDetectorTest {
+
+    @Test
+    public void testDetectHTMLEncoding() {
+        // Test with UTF-8
+        String utf8File = "test/data/util/file-HTMLUtils-utf8.html";
+        assertEquals("UTF-8", EncodingDetector.detectHtmlEncoding(utf8File, null).name());
+
+        // Test with UTF-16 BE BOM
+        String utf16BEFile = "test/data/util/file-HTMLUtils-utf16_be_with_bom.html";
+        assertEquals("UTF-16BE", EncodingDetector.detectHtmlEncoding(utf16BEFile, null).name());
+
+        // Test with UTF-16 LE BOM
+        String utf16LEFile = "test/data/util/file-HTMLUtils-utf16_le_with_bom.html";
+        assertEquals("UTF-16LE", EncodingDetector.detectHtmlEncoding(utf16LEFile, null).name());
+
+        // Test with no BOM and default encoding provided
+        String noBomFile = "test/data/util/file-HTMLUtils-no_header_no_bom.html";
+        assertEquals("ISO-8859-1", EncodingDetector.detectHtmlEncoding(noBomFile, "ISO-8859-1").name());
+
+        // Test with no BOM and no default encoding
+        assertEquals(Charset.defaultCharset().name(), EncodingDetector.detectHtmlEncoding(noBomFile, null).name());
+    }
+}

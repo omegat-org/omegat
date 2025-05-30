bb0722eeedede4953ec5a99026dda8c6743957df
Hiroshi Miura <miurahr@linux.com>
2025-05-30 10:44:45 +0900

    feat(util): enhance `normalizeWidth` to handle various space characters
    - Added support for multiple space types, including non-breaking and thin spaces, to be normalized to standard spaces.
    - Updated `StringUtilTest` with new test cases for validating the normalization of different space characters.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/org/omegat/util/StringUtil.java
+++ src/org/omegat/util/StringUtil.java
@@ -664,7 +664,7 @@ public final class StringUtil {
                 sb.setCharAt(i, (char)(ch-0xFEE0));
                 continue;
             }
-            if ( ch == 0x3000 ) {
+            if (ch == 0x00A0 ||  ch == 0x2007 || ch == 0x2008 || ch == 0x2009 || ch == 0x202F || ch == 0x3000 ) {
                 sb.setCharAt(i, ' ');
             }
             switch ( ch ) {

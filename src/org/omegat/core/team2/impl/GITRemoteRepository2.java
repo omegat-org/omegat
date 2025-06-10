4fe7d56effdbeb98f94b37196da42fa0d34f9d6f
Hiroshi Miura <miurahr@linux.com>
2025-06-10 13:37:03 +0900

    chore: remove unused `Collectors` import from GITRemoteRepository2
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/org/omegat/core/team2/impl/GITRemoteRepository2.java
+++ src/org/omegat/core/team2/impl/GITRemoteRepository2.java
@@ -34,7 +34,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import javax.xml.namespace.QName;

26e3d273e6eca3058f0a3100cab770e576d834e8
Hiroshi Miura <miurahr@linux.com>
2025-06-20 17:54:20 +0900

    fix(logging): narrow catch block to `IOException` in `Log` utility
    - Replaced generic `Exception` catch block with specific `IOException` for improved exception specificity and clarity.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/org/omegat/util/Log.java
+++ src/org/omegat/util/Log.java
@@ -111,7 +111,7 @@ public final class Log {
                 try (InputStream in = new FileInputStream(usersLogSettings)) {
                     init(in);
                     loaded = true;
-                } catch (Exception ignored) {
+                } catch (IOException ignored) {
                 }
             }
         }

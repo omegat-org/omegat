741250818b8eb87b9ad66c554faaa0caee3ab8c2
Hiroshi Miura <miurahr@linux.com>
2025-07-14 08:16:18 +0900

    refactor(spellchecker): update Lucene module conventions, dependencies, and logging
    - Replaced `java-library` plugin with `org.omegat.module-conventions`.
    - Simplified dependency exclusions and updated directory references in `build.gradle`.
    - Replaced `ex.printStackTrace` with `Log.log` for unified exception handling.
    - Updated dictionary base path retrieval to use `SpellCheckerManager#getDefaultDictionaryDir()`.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- spellchecker/lucene/src/main/java/org/omegat/spellchecker/lucene/LuceneHunSpellChecker.java
+++ spellchecker/lucene/src/main/java/org/omegat/spellchecker/lucene/LuceneHunSpellChecker.java
@@ -1,27 +1,27 @@
 /*******************************************************************************
- * OmegaT - Computer Assisted Translation (CAT) tool
- *          with fuzzy matching, translation memory, keyword search,
- *          glossaries, and translation leveraging into updated projects.
- *
- * Copyright (C) 2024 Hiroshi Miura
- *          Home page: https://www.omegat.org/
- *          Support center: https://omegat.org/support
- *
- * This file is part of OmegaT.
- *
- * OmegaT is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * OmegaT is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- ******************************************************************************/
+ OmegaT - Computer Assisted Translation (CAT) tool
+          with fuzzy matching, translation memory, keyword search,
+          glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2024 Hiroshi Miura
+          Home page: https://www.omegat.org/
+          Support center: https://omegat.org/support
+
+ This file is part of OmegaT.
+
+ OmegaT is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OmegaT is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+******************************************************************************/
 
 package org.omegat.spellchecker.lucene;
 
@@ -44,6 +44,7 @@ import org.omegat.core.spellchecker.AbstractSpellChecker;
 import org.omegat.core.spellchecker.ISpellChecker;
 import org.omegat.core.spellchecker.ISpellCheckerProvider;
 import org.omegat.core.spellchecker.SpellCheckerManager;
+import org.omegat.util.Log;
 import org.omegat.util.Preferences;
 
 public class LuceneHunSpellChecker extends AbstractSpellChecker implements ISpellChecker {
@@ -61,22 +62,21 @@ public class LuceneHunSpellChecker extends AbstractSpellChecker implements ISpel
     /**
      * Register plugins into OmegaT.
      */
+    @SuppressWarnings("unused")
     public static void loadPlugins() {
         Core.registerSpellCheckClass(LuceneHunSpellChecker.class);
     }
 
+    @SuppressWarnings("unused")
     public static void unloadPlugins() {
-    }
-
-    public LuceneHunSpellChecker() {
-        super();
+        // there is no way to unload
     }
 
     @Override
     protected Optional<ISpellCheckerProvider> initializeWithLanguage(final String language) {
         // check that the dict exists
         String dictionaryDir = Preferences.getPreferenceDefault(Preferences.SPELLCHECKER_DICTIONARY_DIRECTORY,
-                SpellCheckerManager.DEFAULT_DICTIONARY_DIR.getPath());
+                SpellCheckerManager.getDefaultDictionaryDir().getPath());
 
         File affixName = Path.of(dictionaryDir).resolve(Path.of(language)).resolve(SC_AFFIX_FILENAME).toFile();
         File dictionaryName = Path.of(dictionaryDir).resolve(language).resolve(SC_DICTIONARY_FILENAME).toFile();
@@ -89,7 +89,7 @@ public class LuceneHunSpellChecker extends AbstractSpellChecker implements ISpel
             ISpellCheckerProvider result = new LuceneProvider(dictionaryName, affixName);
             return Optional.of(result);
         } catch (Exception ex) {
-            ex.printStackTrace();
+            Log.log(ex);
         }
         return Optional.empty();
     }

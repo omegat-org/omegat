1108aee92466d4db8097a8c92d7b776668bdc1f9
Hiroshi Miura <miurahr@linux.com>
2025-07-19 00:31:23 +0900

    refactor(core): enhance immutability and thread safety in `RealProject`
    - Made `projectTMX`, `transMemories`, and `otherTargetLangTMs` final for improved immutability.
    - Introduced `synchronized` blocks to ensure thread safety in TMX load/save operations.
    - Refactored map updates to use `clear()` and `putAll()` for more predictable behavior.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/org/omegat/core/data/RealProject.java
+++ src/org/omegat/core/data/RealProject.java
@@ -166,7 +166,8 @@ public class RealProject implements IProject {
 
     private final StatisticsInfo hotStat = new StatisticsInfo();
 
-    private final ITokenizer sourceTokenizer, targetTokenizer;
+    private final ITokenizer sourceTokenizer;
+    private final ITokenizer targetTokenizer;
 
     private DirectoryMonitor tmMonitor;
 
@@ -186,14 +187,14 @@ public class RealProject implements IProject {
      * This map recreated each time when files changed. So, you can free use it
      * without thinking about synchronization.
      */
-    private Map<String, ExternalTMX> transMemories = new TreeMap<>();
+    private final Map<String, ExternalTMX> transMemories = new TreeMap<>();
 
     /**
      * Storage for all translation memories of translations to other languages.
      */
-    private Map<Language, ProjectTMX> otherTargetLangTMs = new TreeMap<>();
+    private final Map<Language, ProjectTMX> otherTargetLangTMs = new TreeMap<>();
 
-    protected ProjectTMX projectTMX;
+    protected final ProjectTMX projectTMX;
 
     /**
      * True if project loaded successfully.
@@ -201,8 +202,8 @@ public class RealProject implements IProject {
     private boolean loaded = false;
 
     // Sets of exist entries for check orphaned
-    private Set<String> existSource = new HashSet<>();
-    private Set<EntryKey> existKeys = new HashSet<>();
+    private final Set<String> existSource = new HashSet<>();
+    private final Set<EntryKey> existKeys = new HashSet<>();
 
     /** Segments count in project files. */
     protected List<FileInfo> projectFilesList = new ArrayList<>();
@@ -247,6 +248,9 @@ public class RealProject implements IProject {
             remoteRepositoryProvider = null;
         }
 
+        projectTMX = new ProjectTMX(config.getSourceLanguage(), config.getTargetLanguage(),
+                config.isSentenceSegmentingEnabled(), config.getProjectRootDir(), checkOrphanedCallback,
+                Core.getSegmenter());
         sourceTokenizer = createTokenizer(Core.getParams().get(CLIParameters.TOKENIZER_SOURCE),
                 props.getSourceTokenizer());
         Log.logInfoRB("SOURCE_TOKENIZER", sourceTokenizer.getClass().getName());
@@ -842,10 +846,10 @@ public class RealProject implements IProject {
 
                 try {
                     saveProjectProperties();
-
-                    projectTMX.save(config, config.getProjectInternal() + OConsts.STATUS_EXTENSION,
-                            isProjectModified());
-
+                    synchronized (projectTMX) {
+                        projectTMX.save(config, config.getProjectInternal() + OConsts.STATUS_EXTENSION,
+                                isProjectModified());
+                    }
                     if (remoteRepositoryProvider != null && doTeamSync) {
                         tmxPrepared = null;
                         glossaryPrepared = null;
@@ -1216,9 +1220,10 @@ public class RealProject implements IProject {
         File file = new File(config.getProjectInternalDir(), OConsts.STATUS_EXTENSION);
         try {
             Core.getMainWindow().showStatusMessageRB("CT_LOAD_TMX");
-            projectTMX = new ProjectTMX(checkOrphanedCallback);
-            projectTMX.load(config.getSourceLanguage(), config.getTargetLanguage(),
-                    config.isSentenceSegmentingEnabled(), file, Core.getSegmenter());
+            synchronized (projectTMX) {
+                projectTMX.load(config.getSourceLanguage(), config.getTargetLanguage(),
+                        config.isSentenceSegmentingEnabled(), file, Core.getSegmenter());
+            }
         } catch (SAXParseException ex) {
             Log.logErrorRB(ex, "TMXR_FATAL_ERROR_WHILE_PARSING", ex.getLineNumber(), ex.getColumnNumber());
             throw ex;
@@ -1434,7 +1439,8 @@ public class RealProject implements IProject {
             } else {
                 newTransMemories.remove(file.getPath());
             }
-            transMemories = newTransMemories;
+            transMemories.clear();
+            transMemories.putAll(newTransMemories);
         });
         tmMonitor.checkChanges();
         tmMonitor.start();
@@ -1472,7 +1478,8 @@ public class RealProject implements IProject {
             } else {
                 newOtherTargetLangTMs.remove(targetLanguage);
             }
-            otherTargetLangTMs = newOtherTargetLangTMs;
+            otherTargetLangTMs.clear();
+            otherTargetLangTMs.putAll(newOtherTargetLangTMs);
         });
         tmOtherLanguagesMonitor.checkChanges();
         tmOtherLanguagesMonitor.start();

ab5d49afa71cd87e201eebfb4c398a1f9c54c552
Hiroshi Miura <miurahr@linux.com>
2025-08-21 14:36:51 +0900

    chore(docs): add class-level Javadoc for readability and maintainability
    - Added detailed descriptions to class-level Javadocs across multiple modules, including `DragTargetOverlay`, `ApertiumTranslate`, `MyMemoryTranslate`, and theme-related classes.
    - Improved clarity in author annotations and method-level documentation.
    - Removed redundant constructor and method comments in theme classes for brevity.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- machinetranslators/mymemory/src/main/java/org/omegat/machinetranslators/mymemory/MyMemoryHumanTranslate.java
+++ machinetranslators/mymemory/src/main/java/org/omegat/machinetranslators/mymemory/MyMemoryHumanTranslate.java
@@ -38,6 +38,8 @@ import org.omegat.core.machinetranslators.MachineTranslateError;
 import org.omegat.util.Language;
 
 /**
+ * MyMemory machine translation engine connector.
+ *
  * @author Ibai Lakunza Velasco
  * @author Didier Briel
  * @author Martin Wunderlich

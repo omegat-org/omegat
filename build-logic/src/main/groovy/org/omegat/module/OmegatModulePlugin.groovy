f05b5006bac4a628df26bd40333ef6c4106bbb8c
Hiroshi Miura <miurahr@linux.com>
2025-08-30 14:15:22 +0900

    chore(build): configure JavaCompile tasks with UTF-8 and lint options
    - Set UTF-8 as the default encoding for JavaCompile tasks.
    - Enabled compiler lint checks by adding `-Xlint` to compiler arguments.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build-logic/src/main/groovy/org/omegat/module/OmegatModulePlugin.groovy
+++ build-logic/src/main/groovy/org/omegat/module/OmegatModulePlugin.groovy
@@ -5,6 +5,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.DuplicatesStrategy
 import org.gradle.api.tasks.bundling.Jar
+import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.Test
 
 class OmegatModulePlugin implements Plugin<Project> {
@@ -53,6 +54,11 @@ class OmegatModulePlugin implements Plugin<Project> {
             targetCompatibility = JavaVersion.VERSION_11
             withSourcesJar()
             withJavadocJar()
+        }
+
+        project.tasks.withType(JavaCompile).configureEach {
+            it.options.encoding = "UTF-8"
+            it.options.compilerArgs.addAll '-Xlint'
         }
 
         configureTestEnvironment(project)

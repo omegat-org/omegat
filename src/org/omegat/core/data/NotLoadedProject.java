ecbc35ffa115e1d63e8cf1802a6831f70d2e21e5
Hiroshi Miura <miurahr@linux.com>
2025-10-19 12:46:53 +0900

    refactor(core): remove unused `TranslationException` import from `NotLoadedProject`

--- src/org/omegat/core/data/NotLoadedProject.java
+++ src/org/omegat/core/data/NotLoadedProject.java
@@ -32,7 +32,6 @@ import java.util.Map;
 
 import org.omegat.core.data.TMXEntry.ExternalLinked;
 import org.omegat.core.statistics.StatisticsInfo;
-import org.omegat.filters2.TranslationException;
 import org.omegat.tokenizer.ITokenizer;
 import org.omegat.util.Language;
 

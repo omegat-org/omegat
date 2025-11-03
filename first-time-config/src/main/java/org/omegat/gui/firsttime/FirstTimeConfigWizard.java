0db7e0dba9280efba7da8b709f03d03b30e6cc36
Hiroshi Miura <miurahr@linux.com>
2025-11-03 10:30:15 +0900

    feature(gui): add First Time Configuration Wizard
    - Introduced a new "First Time Configuration Wizard" to guide users through initial setup at application startup.
    - Added `FirstTimeConfigWizard` core class and related dialog (`FirstTimeConfigWizardDialog`).
    - Implemented key configuration steps: Theme selection, Font customization, General options, and Greeting/Guidance.
    - Registered the wizard module in `settings.gradle` and implemented Gradle build configuration for the feature.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ first-time-config/src/main/java/org/omegat/gui/firsttime/FirstTimeConfigWizard.java
@@ -0,0 +1,101 @@
+/**************************************************************************
+ OmegaT - Computer Assisted Translation (CAT) tool
+         with fuzzy matching, translation memory, keyword search,
+         glossaries, and translation leveraging into updated projects.
+
+ Copyright (C) 2025 Hiroshi Miura
+               Home page: https://www.omegat.org/
+               Support center: https://omegat.org/support
+
+ This file is part of OmegaT.
+
+ OmegaT is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OmegaT is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **************************************************************************/
+package org.omegat.gui.firsttime;
+
+import java.util.Collections;
+
+import javax.swing.JMenuItem;
+import javax.swing.SwingUtilities;
+
+import org.jetbrains.annotations.Nullable;
+import org.omegat.core.Core;
+import org.omegat.core.CoreEvents;
+import org.omegat.core.events.IApplicationEventListener;
+import org.omegat.gui.main.ProjectUICommands;
+import org.omegat.util.Preferences;
+import org.omegat.util.gui.MenuExtender;
+
+public final class FirstTimeConfigWizard {
+
+    private static final String PREF_NO_FIRST_TIME_WIZARD = "no_first_time_config_wizard_on_start";
+
+    private static final FirstTimeConfigWizardModuleListener LISTENER = new FirstTimeConfigWizardModuleListener();
+
+    private FirstTimeConfigWizard() {}
+
+    public static void loadPlugins() {
+        CoreEvents.registerApplicationEventListener(LISTENER);
+    }
+
+    public static void unloadPlugins() {
+        CoreEvents.unregisterApplicationEventListener(LISTENER);
+    }
+
+    public static class FirstTimeConfigWizardModuleListener implements IApplicationEventListener {
+
+        private @Nullable JMenuItem menuItem;
+
+        @Override
+        public void onApplicationStartup() {
+            initMenu();
+            // Show wizard once if not suppressed
+            if (!Preferences.isPreferenceDefault(PREF_NO_FIRST_TIME_WIZARD, false)) {
+                SwingUtilities.invokeLater(() -> {
+                    FirstTimeConfigWizardDialog dlg = new FirstTimeConfigWizardDialog(
+                            Core.getMainWindow().getApplicationFrame());
+                    dlg.setVisible(true);
+                    if (dlg.isFinished()) {
+                        Preferences.setPreference(PREF_NO_FIRST_TIME_WIZARD, true);
+                        // Restart OmegaT to apply theme and other changes
+                        ProjectUICommands.projectRestart(null);
+                    }
+                });
+            }
+        }
+
+        private void initMenu() {
+            menuItem = new JMenuItem("First-time configuration...");
+            menuItem.addActionListener(e -> {
+                FirstTimeConfigWizardDialog dlg = new FirstTimeConfigWizardDialog(
+                        Core.getMainWindow().getApplicationFrame());
+                dlg.setVisible(true);
+                if (dlg.isFinished()) {
+                    Preferences.setPreference(PREF_NO_FIRST_TIME_WIZARD, true);
+                    if (dlg.isRestartRequired()) {
+                        ProjectUICommands.projectRestart(null);
+                    }
+                }
+            });
+            MenuExtender.addMenuItem(MenuExtender.MenuKey.HELP, menuItem);
+        }
+
+        @Override
+        public void onApplicationShutdown() {
+            if (menuItem != null) {
+                MenuExtender.removeMenuItems(MenuExtender.MenuKey.HELP, Collections.singletonList(menuItem));
+            }
+        }
+    }
+}
---
+++ first-time-config/src/main/java/org/omegat/gui/firsttime/FirstTimeConfigWizardDialog.java
@@ -0,0 +1,273 @@
+/*
+ *  OmegaT - Computer Assisted Translation (CAT) tool
+ *           with fuzzy matching, translation memory, keyword search,
+ *           glossaries, and translation leveraging into updated projects.
+ *
+ *  Copyright (C) 2025 Hiroshi Miura.
+ *                Home page: https://www.omegat.org/
+ *                Support center: https://omegat.org/support
+ *
+ *  This file is part of OmegaT.
+ *
+ *  OmegaT is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OmegaT is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.omegat.gui.firsttime;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.util.ResourceBundle;
+
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.ListSelectionModel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import org.omegat.gui.preferences.IPreferencesController;
+import org.omegat.gui.preferences.view.AppearanceController;
+import org.omegat.gui.preferences.view.FontSelectionController;
+import org.omegat.gui.preferences.view.GeneralOptionsController;
+
+/**
+ * Simple wizard dialog for first-time configuration.
+ * Steps:
+ * 1) Theme (AppearanceController)
+ * 2) Font (FontSelectionController)
+ * 3) General options (GeneralOptionsController)
+ * 4) First Steps (Greeting)
+ */
+public class FirstTimeConfigWizardDialog extends JDialog {
+
+    private final CardLayout cardLayout = new CardLayout();
+    private final JPanel cardPanel = new JPanel(cardLayout);
+
+    private final JButton backButton = new JButton();
+    private final JButton nextButton = new JButton();
+    private final JButton finishButton = new JButton();
+    private final JButton cancelButton = new JButton();
+    private final JLabel statusLabel = new JLabel();
+
+    private final IPreferencesController[] steps;
+    private int index = 0;
+
+    private boolean finished = false;
+
+    // West: steps list
+    private final DefaultListModel<String> stepsModel = new DefaultListModel<>();
+    private final JList<String> stepsList = new JList<>(stepsModel);
+
+    // East: right side container (explanations or greeting manual)
+    private final CardLayout eastLayout = new CardLayout();
+    private final JPanel eastPanel = new JPanel(eastLayout);
+    private final JTextArea explanation = new JTextArea();
+
+    private final ResourceBundle bundle = ResourceBundle.getBundle("org.omegat.gui.firsttime.Bundle");
+
+    public FirstTimeConfigWizardDialog(Frame owner) {
+        super(owner, "Welcome to OmegaT", true);
+        // Now that the dialog is constructed, set localized title
+        setTitle(getString("wizard.title", "Welcome to OmegaT"));
+
+        // Instantiate controllers
+        AppearanceController appearance = new AppearanceController();
+        FontSelectionController font = new FontSelectionController();
+        GeneralOptionsController general = new GeneralOptionsController();
+        // Add a final step for Greetings (First Steps) as a named controller class.
+        IPreferencesController greetingStep = new GreetingStepController(bundle);
+        steps = new IPreferencesController[] { appearance, font, general, greetingStep };
+
+        setLayout(new BorderLayout());
+
+        // WEST: Steps list
+        for (int i = 0; i < steps.length; i++) {
+            String name = steps[i].toString();
+            stepsModel.addElement((i + 1) + ". " + name);
+        }
+        stepsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        stepsList.setEnabled(false); // display only
+        stepsList.setVisibleRowCount(steps.length);
+        JScrollPane westScroll = new JScrollPane(stepsList);
+        westScroll.setPreferredSize(new Dimension(220, 100));
+        westScroll.setBorder(BorderFactory.createTitledBorder(getString("steps.title", "Steps")));
+        add(westScroll, BorderLayout.WEST);
+
+        // CENTER: cards with actual step UIs
+        for (int i = 0; i < steps.length; i++) {
+            JComponent gui = (JComponent) steps[i].getGui();
+            JScrollPane scroll = new JScrollPane(gui);
+            scroll.setBorder(null);
+            cardPanel.add(scroll, "step" + i);
+        }
+        add(cardPanel, BorderLayout.CENTER);
+
+        // EAST: explanation area or greetings manual
+        explanation.setEditable(false);
+        explanation.setLineWrap(true);
+        explanation.setWrapStyleWord(true);
+        explanation.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+        JScrollPane explainScroll = new JScrollPane(explanation);
+        explainScroll.setPreferredSize(new Dimension(280, 100));
+        explainScroll.setBorder(BorderFactory.createTitledBorder(getString("explain.title", "Explanation")));
+        eastPanel.add(explainScroll, "explain");
+
+        add(eastPanel, BorderLayout.EAST);
+
+        // NAVIGATION
+        JPanel nav = new JPanel();
+        backButton.setAction(new AbstractAction(getString("button.back", "Back")) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (index > 0) {
+                    index--;
+                    updateState();
+                }
+            }
+        });
+        nextButton.setAction(new AbstractAction(getString("button.next", "Next")) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (index < steps.length - 1) {
+                    if (!steps[index].validate()) {
+                        return; // stay until valid
+                    }
+                    boolean restartRequired = steps[index].isRestartRequired();
+                    index++;
+                    updateState();
+                    if (restartRequired) {
+                        statusLabel.setText(getString("status.restartRequired", "Changes on the previous step require restarting OmegaT."));
+                    } else {
+                        statusLabel.setText("");
+                    }
+                }
+            }
+        });
+        finishButton.setAction(new AbstractAction(getString("button.finish", "Finish")) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Validate all steps
+                for (IPreferencesController c : steps) {
+                    if (!c.validate()) {
+                        index = indexOf(c);
+                        updateState();
+                        return;
+                    }
+                }
+                for (IPreferencesController c : steps) {
+                    c.persist();
+                }
+                finished = true;
+                dispose();
+            }
+        });
+        cancelButton.setAction(new AbstractAction(getString("button.cancel", "Cancel")) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                finished = false;
+                dispose();
+            }
+        });
+        nav.add(backButton);
+        nav.add(nextButton);
+        nav.add(finishButton);
+        nav.add(cancelButton);
+        // South container with buttons and status bar
+        JPanel southPanel = new JPanel(new BorderLayout());
+        southPanel.add(nav, BorderLayout.CENTER);
+        statusLabel.setBorder(BorderFactory.createEmptyBorder(4, 8, 6, 8));
+        southPanel.add(statusLabel, BorderLayout.SOUTH);
+        add(southPanel, BorderLayout.SOUTH);
+
+        // Header
+        JLabel header = new JLabel(getString("header.text", "Let's set up a few preferences"));
+        header.setBorder(javax.swing.BorderFactory.createEmptyBorder(8, 8, 8, 8));
+        add(header, BorderLayout.NORTH);
+
+        setPreferredSize(new Dimension(980, 560));
+        pack();
+        setLocationRelativeTo(owner);
+        updateState();
+    }
+
+    private int indexOf(IPreferencesController c) {
+        for (int i = 0; i < steps.length; i++) {
+            if (steps[i] == c)
+                return i;
+        }
+        return 0;
+    }
+
+    private void updateState() {
+        cardLayout.show(cardPanel, "step" + index);
+        stepsList.setSelectedIndex(index);
+        backButton.setEnabled(index > 0);
+        nextButton.setEnabled(index < steps.length - 1);
+        finishButton.setEnabled(index == steps.length - 1);
+        // Clear any previous status message when changing steps
+        statusLabel.setText("");
+        // Always show the explanation area on the east side
+        eastLayout.show(eastPanel, "explain");
+        // Update explanation text based on current step
+        String textKey;
+        switch (index) {
+        case 0:
+            textKey = "explain.appearance";
+            break;
+        case 1:
+            textKey = "explain.font";
+            break;
+        case 2:
+            textKey = "explain.general";
+            break;
+        case 3:
+            textKey = "explain.greeting";
+            break;
+        default:
+            textKey = ""; // No explanation for the First Steps page
+            break;
+        }
+        explanation.setText(textKey.isEmpty() ? "" : getString(textKey, ""));
+        explanation.setCaretPosition(0);
+    }
+
+    private String getString(String key, String deflt) {
+        try {
+            return bundle.getString(key);
+        } catch (Exception e) {
+            return deflt;
+        }
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+    
+    public boolean isRestartRequired() {
+        for (IPreferencesController c : steps) {
+            if (c.isRestartRequired()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

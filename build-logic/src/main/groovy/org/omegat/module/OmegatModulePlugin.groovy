9b1ac4cca3530becc1113d24bb176f7fccff9567
Hiroshi Miura <miurahr@linux.com>
2026-06-26 11:33:44 +0900

    chore: update Java compatibility to 21 in OmegatModulePlugin

--- build-logic/src/main/groovy/org/omegat/module/OmegatModulePlugin.groovy
+++ build-logic/src/main/groovy/org/omegat/module/OmegatModulePlugin.groovy
@@ -97,8 +97,8 @@ class OmegatModulePlugin implements Plugin<Project> {
 
         // Add sources and javadoc jars
         project.java {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
         }
 
         project.tasks.withType(JavaCompile).configureEach { javaCompile ->

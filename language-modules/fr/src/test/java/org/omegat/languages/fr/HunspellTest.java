0954594510a9f9055c378914885596c9fcb79ce7
Hiroshi Miura <miurahr@linux.com>
2026-07-16 17:59:52 +0900

    Revert "chore: skip flaky test"
    This reverts commit 8ac391fa772dc03f242ae83267f10bbfc277a359.

--- language-modules/fr/src/test/java/org/omegat/languages/fr/HunspellTest.java
+++ language-modules/fr/src/test/java/org/omegat/languages/fr/HunspellTest.java
@@ -44,5 +44,7 @@ public class HunspellTest extends LanguageModuleTestBase {
     public void testDictionary() throws Exception {
         LuceneHunSpellChecker checker = new LuceneHunSpellChecker(60_000);
         testDictionaryHelper(checker, LANGUAGE, GOOD, BAD);
+        List<String> suggestions = suggestWithRetry(checker, BAD, 1);
+        assertThat(suggestions).as("Get suggestion").contains("erreur");
     }
 }
